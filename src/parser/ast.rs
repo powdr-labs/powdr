76a8af74316698be27d143ba0b45c177923c4dea
chriseth <chris@ethereum.org>
2023-01-06 20:51:09 +0100

    Plookups and identities.

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -9,16 +9,34 @@ pub enum Statement {
     Namespace(String, Expression),
     PolynomialDefinition(String, Expression),
     PolynomialConstantDeclaration(String),
+    PolynomialCommitDeclaration(String),
+    PolynomialIdentity(Expression),
+    PlookupIdentity(SelectedExpressions, SelectedExpressions),
     ConstantDefinition(String, Expression),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct SelectedExpressions {
+    pub selector: Option<Expression>,
+    pub expressions: Vec<Expression>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Expression {
     /// Reference to a constant, "%ConstantName"
     Constant(String),
+    PolynomialReference(PolynomialReference),
     // TODO use bignum or something
     Number(u64),
     BinaryOperation(Box<Expression>, BinaryOperator, Box<Expression>),
+}
+
+#[derive(Debug, PartialEq, Eq, Default)]
+pub struct PolynomialReference {
+    pub namespace: Option<String>,
+    pub name: String,
+    pub index: Option<Box<Expression>>,
+    pub next: bool,
 }
 
 #[derive(Debug, PartialEq, Eq)]
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -26,6 +26,30 @@ mod test {
         assert_eq!(parsed, PILFile(vec![Statement::Include("x".to_string())]));
     }
 
+    #[test]
+    fn simple_plookup() {
+        let parsed = pil::PILFileParser::new().parse("f in g;").unwrap();
+        assert_eq!(
+            parsed,
+            PILFile(vec![Statement::PlookupIdentity(
+                SelectedExpressions {
+                    selector: None,
+                    expressions: vec![Expression::PolynomialReference(PolynomialReference {
+                        name: "f".to_string(),
+                        ..Default::default()
+                    })]
+                },
+                SelectedExpressions {
+                    selector: None,
+                    expressions: vec![Expression::PolynomialReference(PolynomialReference {
+                        name: "g".to_string(),
+                        ..Default::default()
+                    })]
+                }
+            )])
+        );
+    }
+
     fn parse_file(name: &str) -> PILFile {
         let input = fs::read_to_string(name).unwrap();
         parse(&input).unwrap()
@@ -33,7 +57,8 @@ mod test {
 
     #[test]
     fn parse_example_files() {
-        parse_file("test_files/global.pil");
         parse_file("test_files/config.pil");
+        parse_file("test_files/global.pil");
+        parse_file("test_files/byte4.pil");
     }
 }
