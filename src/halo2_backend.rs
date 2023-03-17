3bc1061ab77f0792489a1d91ea26d261b012a0a5
adria0 <nowhere@>
2023-03-17 16:06:27 +0700

    public inputs wip

--- src/halo2_backend.rs
+++ src/halo2_backend.rs
@@ -16,12 +16,45 @@ use polyexen::plaf::{
 };
 use prettytable::{Row, Table};
 
-use crate::analyzer::{BinaryOperator, Expression, Identity, IdentityKind};
+use crate::analyzer::{
+    BinaryOperator, Expression, FunctionValueDefinition, Identity, IdentityKind,
+};
 use crate::commit_evaluator::WitnessColumn;
 use crate::number::{abstract_to_degree, AbstractNumberType, DegreeType};
-use crate::parser::ast::PILFile;
+use crate::parser::ast::{PILFile};
 use crate::{analyzer, asm_compiler, commit_evaluator, constant_evaluator, json_exporter};
 use halo2_proofs::{dev::MockProver, halo2curves::bn256::Fr, plonk::Circuit};
+
+// Follow dependency installation instrucions from https://github.com/ed255/polyexen-demo
+
+fn print_table(data: &[(&str, Vec<BigInt>)]) {
+    use prettytable::Cell as PCell;
+
+    let mut table = Table::new();
+    let header_n = std::iter::once(PCell::new("n"));
+    let header_rest = data.iter().map(|(name, _)| PCell::new(&name.to_string()));
+    let headers = header_n.chain(header_rest).collect();
+    table.add_row(Row::new(headers));
+
+    let trim = |s: &str| {
+        if s.len() > 5 {
+            format!("{}…", &s[..5])
+        } else {
+            s.to_string()
+        }
+    };
+
+    for row in 0..data.get(0).unwrap().1.len() {
+        let value_n = std::iter::once(PCell::new(&row.to_string()));
+        let value_rest = data
+            .iter()
+            .map(|(_, d)| PCell::new(&trim(&d.get(row).unwrap().to_string())));
+        let values = value_n.chain(value_rest).collect();
+        table.add_row(Row::new(values));
+    }
+
+    table.printstd();
+}
 
 fn expression_2_expr(
     cols: &HashMap<String, (ColumnKind, usize)>,
@@ -63,6 +96,7 @@ fn analyzed_to_circuit(
     int_to_field: &dyn Fn(&BigInt) -> BigUint,
 ) -> PlafH2Circuit {
     // The stucture of the table is as following
+    //
     // | constant columns | __enable_cur | __enable_next |  witness columns | \
     // |  bla_bla_bla     |    1         |       1       |   bla_bla_bla    |  |
     // |  bla_bla_bla     |    1         |       1       |   bla_bla_bla    |  |>  witness + constants  2^(k-1)
@@ -74,35 +108,24 @@ fn analyzed_to_circuit(
     // |  0               |    0         |       0       |   <unusable>     |  |
     // |  0               |    0         |       0       |   <unusable>     | /
 
-    let print_table = |data: &Vec<(&str, Vec<BigInt>)>| {
-        use prettytable::Cell as PCell;
-
-        let mut table = Table::new();
-        let header_n = std::iter::once(PCell::new("n"));
-        let header_rest = data.iter().map(|(name, _)| PCell::new(&name.to_string()));
-        let headers = header_n.chain(header_rest).collect();
-        table.add_row(Row::new(headers));
-
-        for row in 0..data.get(0).unwrap().1.len() {
-            let value_n = std::iter::once(PCell::new(&row.to_string()));
-            let value_rest = data
-                .iter()
-                .map(|(_, d)| PCell::new(&d.get(row).unwrap().to_string()));
-            let values = value_n.chain(value_rest).collect();
-            table.add_row(Row::new(values));
-        }
-
-        table.printstd();
-    };
+    // generate constants and commits (witness).
 
     let (mut constants, degree) = constant_evaluator::generate(analyzed);
     let commits = commit_evaluator::generate(analyzed, degree, &constants, query_callback, verbose);
-
     assert_eq!(
         constants.get(0).unwrap().1.len(),
         commits.get(0).unwrap().1.len()
     );
+    // print_table(&constants);
+    // print_table(&commits);
+
+    // get number of rows, we assume that data is defined by all rows and all columns.
+
     let num_rows = constants.get(0).unwrap().1.len();
+
+    // append to fixed columns, one that enables constrains that does not have rotations
+    // and another that enables contraints that have a rotation ( not this is not activated )
+    // in last row.
 
     constants.push((
         "__enable_cur",
@@ -118,6 +141,8 @@ fn analyzed_to_circuit(
     ));
     let q_enable_next_col = constants.len() - 1;
 
+    // get columns names and offset. -------------
+
     let const_cols = constants
         .iter()
         .enumerate()
@@ -131,6 +156,8 @@ fn analyzed_to_circuit(
     let cols: HashMap<String, (ColumnKind, usize)> =
         const_cols.clone().chain(witness_cols.clone()).collect();
 
+    // build Plaf columns. -------------
+
     let columns = Columns {
         fixed: constants
             .iter()
@@ -143,11 +170,53 @@ fn analyzed_to_circuit(
         public: vec![],
     };
 
+    // build Plaf info. -------------
+ 
     let info = Info {
         p: polyexen::expr::get_field_p::<Fr>(),
         num_rows: constants.get(0).unwrap().1.len(),
         challenges: vec![],
     };
+
+    // analyze inputs my pc -------------
+
+    // collect all inputs by pc defined
+
+    let mut inputs_by_pc = vec![]; 
+
+    for (def_name, (poly, def)) in &analyzed.definitions {
+        let Some(FunctionValueDefinition::Query(Expression::Tuple(query))) = def else {
+            continue;
+        };
+        for expr in query {
+            let Expression::Tuple(query_args) = expr else {
+                continue;
+            };
+            let Some(Expression::Number(pc)) = query_args.get(0) else {
+                continue;
+            };
+            let Some(Expression::Tuple(expr_args)) = query_args.get(1) else {
+                continue;
+            };
+            if Some(&Expression::String(String::from("input"))) != expr_args.get(0) {
+                continue;
+            } 
+            let Some(exprs) = expr_args.get(1) else {
+                continue;
+            };
+            let (expr,_) = expression_2_expr(&cols, exprs, int_to_field);
+
+            inputs_by_pc.push((pc.clone(),expr));
+        }
+    }
+
+    // build an expression where, for each defined option, input_n == expression
+
+        
+
+    println!("====> {:#?}", inputs_by_pc);
+
+    // build Plaf polys. -------------
 
     let mut polys = vec![];
     let mut lookups = vec![];
@@ -167,11 +236,17 @@ fn analyzed_to_circuit(
     });
     for id in &analyzed.identities {
         if id.kind == IdentityKind::Polynomial {
+            // polinomial identities.
+
             assert_eq!(id.right.expressions.len(), 0);
             assert_eq!(id.right.selector, None);
             assert_eq!(id.left.expressions.len(), 0);
+
             let (exp, rotation) =
                 expression_2_expr(&cols, &id.left.selector.as_ref().unwrap(), int_to_field);
+
+            // depending if this polinomial contains a rotation, enable for all rows or all unless last one.
+
             let exp = if rotation == 0 {
                 Expr::Mul(vec![exp, q_enable_cur.clone()])
             } else {
@@ -182,6 +257,8 @@ fn analyzed_to_circuit(
                 exp,
             });
         } else if id.kind == IdentityKind::Plookup {
+            // lookups.
+
             assert_eq!(id.left.selector, None);
             assert_eq!(id.right.selector, None);
 
@@ -208,6 +285,8 @@ fn analyzed_to_circuit(
         }
     }
 
+    // build Plaf fixeds. -------------
+
     let fixed: Vec<Vec<_>> = constants
         .iter()
         .map(|(_, row)| {
@@ -217,6 +296,8 @@ fn analyzed_to_circuit(
         })
         .collect();
 
+    // build plaf. -------------
+
     let plaf = Plaf {
         info,
         columns,
@@ -225,6 +306,8 @@ fn analyzed_to_circuit(
         copys: vec![],
         fixed,
     };
+
+    // build witness. -------------
 
     let witness: Vec<Vec<_>> = commits
         .iter()
@@ -243,16 +326,23 @@ fn analyzed_to_circuit(
         witness,
     };
 
+    // return circuit description + witness. -------------
+
     PlafH2Circuit { plaf, wit }
 }
 
 pub fn prove_asm(file_name: &str, inputs: Vec<AbstractNumberType>, verbose: bool) {
+    // read and compile PIL.
+
     let contents = fs::read_to_string(file_name).unwrap();
     let pil = asm_compiler::compile(Some(file_name), &contents).unwrap_or_else(|err| {
         eprintln!("Error parsing .asm file:");
         err.output_to_stderr();
         panic!();
     });
+    let analyzed = &analyzer::analyze_string(&format!("{pil}"));
+
+    // define how query information is retrieved.
 
     let query_callback = |query: &str| -> Option<AbstractNumberType> {
         let items = query.split(',').map(|s| s.trim()).collect::<Vec<_>>();
@@ -269,8 +359,6 @@ pub fn prove_asm(file_name: &str, inputs: Vec<AbstractNumberType>, verbose: bool
         }
         None
     };
-
-    let analyzed = &analyzer::analyze_string(&format!("{pil}"));
 
     let modulus = polyexen::expr::get_field_p::<Fr>();
     let int_to_field = |n: &BigInt| {
