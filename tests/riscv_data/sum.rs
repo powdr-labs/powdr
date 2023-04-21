42061ba9d6af64d3413be3757b9b997f9b3c3dbc
chriseth <chris@ethereum.org>
2023-04-21 11:58:24 +0200

    further reduction.

--- tests/riscv_data/sum.rs
+++ tests/riscv_data/sum.rs
@@ -22,37 +22,24 @@ pub mod write_to {
     use core::cmp::min;
     use core::fmt;
 
-    pub struct WriteTo<'a> {
-        buffer: &'a mut [u8],
-        // on write error (i.e. not enough space in buffer) this grows beyond
-        // `buffer.len()`.
+    pub struct WriteTo {
         used: usize,
     }
 
-    impl<'a> WriteTo<'a> {
-        pub fn new(buffer: &'a mut [u8]) -> Self {
-            WriteTo { buffer, used: 0 }
-        }
-
-        pub fn as_str(self) -> Option<&'a str> {
-            if self.used <= self.buffer.len() {
-                // only successful concats of str - must be a valid str.
-                use core::str::from_utf8_unchecked;
-                Some(unsafe { from_utf8_unchecked(&self.buffer[..self.used]) })
-            } else {
-                None
-            }
+    impl WriteTo {
+        pub fn new() -> Self {
+            WriteTo { used: 0 }
         }
     }
 
-    impl<'a> fmt::Write for WriteTo<'a> {
+    impl fmt::Write for WriteTo {
         fn write_str(&mut self, s: &str) -> fmt::Result {
             Ok(())
         }
     }
 
     pub fn show<'a>(buffer: &'a mut [u8], args: fmt::Arguments) -> Result<&'a str, fmt::Error> {
-        let mut w = WriteTo::new(buffer);
+        let mut w = WriteTo::new();
         print_prover("Created");
         fmt::write(&mut w, args)?;
         Ok("")
