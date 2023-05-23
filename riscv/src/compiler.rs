3f93cb565bca08e50bdc9b0b430252b98d0f8e31
chriseth <chris@ethereum.org>
2023-05-23 17:02:53 +0200

    remove pc counter.

--- riscv/src/compiler.rs
+++ riscv/src/compiler.rs
@@ -45,7 +45,6 @@ pub fn compile_riscv_asm(mut assemblies: BTreeMap<String, String>) -> String {
         .collect::<Vec<_>>();
     let (data_code, data_positions) = store_data_objects(&sorted_objects, data_start);
 
-    // TODO debug directives increase the PC counter below - this is not what we want, right?
     preamble()
         + &file_ids
             .into_iter()
@@ -60,14 +59,6 @@ pub fn compile_riscv_asm(mut assemblies: BTreeMap<String, String>) -> String {
                     .into_iter()
                     .flat_map(process_statement),
             )
-            .enumerate()
-            .map(|(i, line)| {
-                if i % 10 == 0 {
-                    format!("// PC: {i}\n{line}")
-                } else {
-                    line
-                }
-            })
             .join("\n")
 }
 
