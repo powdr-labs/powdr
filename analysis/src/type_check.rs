0b4f67f6a20f840983f7f0aa3b007d93f67f2230
chriseth <chris@ethereum.org>
2023-07-03 14:57:11 +0200

    Some renaming.

--- analysis/src/type_check.rs
+++ analysis/src/type_check.rs
@@ -1,5 +1,5 @@
 use ast::{
-    analysis::{
+    asm_analysis::{
         AnalysisASMFile, AssignmentStatement, DegreeStatement, InstructionDefinitionStatement,
         InstructionStatement, LabelStatement, PilBlock, RegisterDeclarationStatement,
     },
--- ast/src/lib.rs
+++ ast/src/lib.rs
@@ -1,4 +1,4 @@
 /// A typed-checked ASM + PIL AST optimised for analysis
-pub mod analysis;
+pub mod asm_analysis;
 /// A parsed ASM + PIL AST
 pub mod parsed;
