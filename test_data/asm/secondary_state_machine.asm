2217eaf1d638501f21292d47015e8b2ddb98d6d3
Georg Wiese <georgwiese@gmail.com>
2023-07-05 13:26:41 +0000

    secondary_state_machine.asm: Use simple binary SM example

--- test_data/asm/secondary_state_machine.asm
+++ test_data/asm/secondary_state_machine.asm
@@ -1,56 +1,70 @@
 reg pc[@pc];
 reg X[<=];
-reg A0;
-reg A1;
-reg A2;
-reg A3;
-reg A4;
-reg A5;
-reg A6;
-reg A7;
-reg B0;
-reg B1;
-reg B2;
-reg B3;
+reg A;
+reg B;
+reg C;
 
-pil {
-    pol commit in0, in1, in2, in3, in4, in5, in6, in7, hashType, cap1, cap2, cap3;
-    pol commit hash0, hash1, hash2, hash3;
 
-    pol commit is_used;
+pil{
+    // Binary SM for 2 4-bit numbers
+    // 0: AND
+    // 1: OR
+    // 2: XOR
+	macro is_nonzero(X) { match X { 0 => 0, _ => 1, } };
+	macro is_zero(X) { 1 - is_nonzero(X) };
 
-    hash0 = in0;
-    hash1 = in1;
-    hash2 = in2;
-    hash3 = in3;
+	col fixed binary_RESET(i) { is_zero((i % 2) - 1) };
+	col fixed binary_FACTOR(i) { 1 << (((i + 1) % 2) * 4) };
+
+	col fixed binary_P_A(i) { i % 16 };
+	col fixed binary_P_B(i) { (i >> 4) % 16 };
+    col fixed binary_P_operation(i) { (i / (16 * 16)) % 3 };
+	col fixed binary_P_C(i) {
+        match binary_P_operation(i) {
+            0 => binary_P_A(i) & binary_P_B(i),
+            1 => binary_P_A(i) | binary_P_B(i),
+            2 => binary_P_A(i) ^ binary_P_B(i),
+        } & 0xf
+    };
+
+	col witness binary_A_byte;
+	col witness binary_B_byte;
+	col witness binary_C_byte;
+
+	col witness binary_A;
+	col witness binary_B;
+	col witness binary_C;
+    col witness binary_operation;
+
+	binary_A' = binary_A * (1 - binary_RESET) + binary_A_byte * binary_FACTOR;
+	binary_B' = binary_B * (1 - binary_RESET) + binary_B_byte * binary_FACTOR;
+	binary_C' = binary_C * (1 - binary_RESET) + binary_C_byte * binary_FACTOR;
+	(binary_operation' - binary_operation) * (1 - binary_RESET) = 0;
+
+	{binary_operation', binary_A_byte, binary_B_byte, binary_C_byte} in {binary_P_operation, binary_P_A, binary_P_B, binary_P_C};
 }
 
 
-instr poseidon {
-    {
-        A0, A1, A2, A3, A4, A5, A6, A7,
-        0, 0, 0, 0,
-        B0, B1, B2, B3
-    }
-    in is_used {
-        in0, in1, in2, in3, in4, in5, in6, in7,
-        hashType, cap1, cap2, cap3,
-        hash0, hash1, hash2, hash3
-    }
+instr and {
+    {A, B, C, 0} in binary_RESET { binary_A, binary_B, binary_C, binary_operation }
+}
+
+instr or {
+    {A, B, C, 1} in binary_RESET { binary_A, binary_B, binary_C, binary_operation }
+}
+
+instr xor {
+    {A, B, C, 2} in binary_RESET { binary_A, binary_B, binary_C, binary_operation }
 }
 instr loop { pc' = pc }
 
 // Set input
-A0 <=X= 0;
-A1 <=X= 0;
-A2 <=X= 0;
-A3 <=X= 0;
-A4 <=X= 0;
-A5 <=X= 0;
-A6 <=X= 0;
-A7 <=X= 0;
+A <=X= 0xf0;
+B <=X= 0x11;
+C <=X= 0;
 
-poseidon;
-
+and; // -> 0x10
+or;  // -> 0xf1
+xor; // -> 0xe1
 
 loop;
