0e668e6487e8c773afe003e71a501b5d3b57b0a4
Georg Wiese <georgwiese@gmail.com>
2023-07-06 05:36:06 +0000

    Adjust test scripts

--- test_add2.sh
+++ test_add2.sh
@@ -3,4 +3,4 @@ set -e
 cargo run pil -f test_data/asm/secondary_state_machine_add2.asm --field bn254
 cargo run export-csv secondary_state_machine_add2.pil --field bn254
 csvtool namedcol -u TAB A,B,add_two_RESET,add_two_state,add_two_input columns.csv | head
-#cargo run prove secondary_state_machine_add2.pil --field bn254 --backend halo2-mock
+cargo run prove secondary_state_machine_add2.pil --field bn254 --backend halo2-mock 2>&1 | head -n 30
