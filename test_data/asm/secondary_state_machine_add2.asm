03363bbc3a8e107fc9ab12cb7ab1c6c719c7c9aa
Georg Wiese <georgwiese@gmail.com>
2023-07-06 05:32:45 +0000

    Tabs -> Spaces

--- test_data/asm/secondary_state_machine_add2.asm
+++ test_data/asm/secondary_state_machine_add2.asm
@@ -7,21 +7,21 @@ reg B;
 
 pil{
     // Add 2 to a number by adding 1 two times
-	col fixed add_two_RESET(i) { match i % 3 { 2 => 1, _ => 0 } };
+    col fixed add_two_RESET(i) { match i % 3 { 2 => 1, _ => 0 } };
 
-	col witness add_two_state;
-	col witness add_two_input;
+    col witness add_two_state;
+    col witness add_two_input;
 
-	// Because constraints are not cyclic, we need to explicitly constrain the first state
-	first_step * (add_two_state - add_two_input) = 0;
+    // Because constraints are not cyclic, we need to explicitly constrain the first state
+    first_step * (add_two_state - add_two_input) = 0;
 
-	// If RESET is true, constrain the next state to be equal to the input
-	// if RESET is false, increment the current state
-	add_two_state' = (1 - add_two_RESET) * (add_two_state + 1) + add_two_RESET * add_two_input';
+    // If RESET is true, constrain the next state to be equal to the input
+    // if RESET is false, increment the current state
+    add_two_state' = (1 - add_two_RESET) * (add_two_state + 1) + add_two_RESET * add_two_input';
 
-	// If RESET is true, the next input is unconstrained
-	// If RESET is false, the next input is equal to the current input
-	add_two_input' = (1 - add_two_RESET) * add_two_input;
+    // If RESET is true, the next input is unconstrained
+    // If RESET is false, the next input is equal to the current input
+    add_two_input' = (1 - add_two_RESET) * add_two_input;
 }
 
 instr add2 Y -> X {
--- test_data/asm/secondary_state_machine_binary.asm
+++ test_data/asm/secondary_state_machine_binary.asm
@@ -6,24 +6,24 @@ reg B;
 
 
 pil{
-	// Decomposes a byte into 2 4-bit values, adds 1 to each of them (via a lookup), and puts it back together
-	col fixed binary_RESET(i) { i % 2 };
+    // Decomposes a byte into 2 4-bit values, adds 1 to each of them (via a lookup), and puts it back together
+    col fixed binary_RESET(i) { i % 2 };
 
-	col fixed binary_P_A(i) { i % 16 };
-	col fixed binary_P_B(i) {
+    col fixed binary_P_A(i) { i % 16 };
+    col fixed binary_P_B(i) {
         (binary_P_A(i) + 1) & 0xf
     };
 
-	col witness binary_A_byte;
-	col witness binary_B_byte;
+    col witness binary_A_byte;
+    col witness binary_B_byte;
 
-	{binary_A_byte, binary_B_byte} in {binary_P_A, binary_P_B};
+    {binary_A_byte, binary_B_byte} in {binary_P_A, binary_P_B};
 
-	col witness binary_A;
-	col witness binary_B;
+    col witness binary_A;
+    col witness binary_B;
 
-	binary_A' = 16 * binary_A * (1 - binary_RESET) + binary_A_byte;
-	binary_B' = 16 * binary_B * (1 - binary_RESET) + binary_B_byte;
+    binary_A' = 16 * binary_A * (1 - binary_RESET) + binary_A_byte;
+    binary_B' = 16 * binary_B * (1 - binary_RESET) + binary_B_byte;
 
 }
 
