2fdad7b4076be9062de50e962f2da0f146a26926
Georg Wiese <georgwiese@gmail.com>
2023-08-28 14:43:45 +0200

    Benchmark: Fix Keccak benchmark, remove all other

--- compiler/benches/executor_benchmark.rs
+++ compiler/benches/executor_benchmark.rs
@@ -60,39 +60,10 @@ fn run_witgen<T: FieldElement>(analyzed: &Analyzed<T>, input: Vec<T>) {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("executor-benchmarks");
-
-    // ASM test files
-    add_benchmark_asm(&mut group, "simple_sum", vec![16, 4, 1, 2, 8, 5]);
-    add_benchmark_asm(&mut group, "palindrome", vec![7, 1, 7, 3, 9, 3, 7, 1]);
-    add_benchmark_asm(&mut group, "vm_to_block_unique_interface", vec![]);
-    add_benchmark_asm(&mut group, "mem_read_write", vec![]);
-    add_benchmark_asm(&mut group, "multi_assign", vec![7]);
-    add_benchmark_asm(&mut group, "bit_access", vec![20]);
-    add_benchmark_asm(&mut group, "functional_instructions", vec![20]);
-    add_benchmark_asm(&mut group, "full_pil_constant", vec![]);
-
-    // PIL test files
-    add_benchmark_pil(&mut group, "fibonacci", vec![]);
-    add_benchmark_pil(&mut group, "constant_in_identity", vec![7, 8, 2, 3]);
-    add_benchmark_pil(&mut group, "fib_macro", vec![]);
-    add_benchmark_pil(&mut group, "global", vec![]);
-    add_benchmark_pil(&mut group, "sum_via_witness_query", vec![7, 8, 2, 3]);
-    add_benchmark_pil(&mut group, "witness_lookup", vec![3, 5, 2, 7]);
-    add_benchmark_pil(&mut group, "pair_lookup", vec![]);
-    add_benchmark_pil(&mut group, "block_lookup_or", vec![]);
-    add_benchmark_pil(&mut group, "halo_without_lookup", vec![]);
-    add_benchmark_pil(&mut group, "simple_div", vec![]);
-    add_benchmark_pil(&mut group, "single_line_blocks", vec![]);
-    add_benchmark_pil(&mut group, "two_block_machine_functions", vec![]);
-    add_benchmark_pil(&mut group, "fixed_columns", vec![]);
-
-    group.finish();
-
     // Keccak with smaller sample size
     let mut group = c.benchmark_group("keccak-executor-benchmark");
     group.sample_size(10);
-    add_benchmark_asm(&mut group, "keccak", vec![]);
+    add_benchmark_pil(&mut group, "keccak", vec![]);
     group.finish();
 }
 
