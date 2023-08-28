327b9711326d5599721f8130e41df1fafa03849f
Georg Wiese <georgwiese@gmail.com>
2023-08-28 17:29:49 +0200

    Add print

--- riscv/tests/riscv_data/poseidon.rs
+++ riscv/tests/riscv_data/poseidon.rs
@@ -2,11 +2,12 @@
 
 use runtime::coprocessors::poseidon_hash;
 
+use runtime::print;
+
 #[no_mangle]
 pub fn main() {
     let h = poseidon_hash(1, 2);
-    // This is the value returned by the coprocessor stub,
-    // this needs to be updated when the final version is
-    // merged.
+    let foo: u32 = 256;
+    print!("Hash is: 0x{h:x}\n");
     assert_eq!(h, 0x50e0a9e4);
 }
