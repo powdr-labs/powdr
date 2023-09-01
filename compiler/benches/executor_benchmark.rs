414fd4266eef5df5e06a566908dcf51448c9498e
Georg Wiese <georgwiese@gmail.com>
2023-09-01 14:28:33 +0000

    Build Keccak PIL in benchmark

--- compiler/benches/executor_benchmark.rs
+++ compiler/benches/executor_benchmark.rs
@@ -1,56 +1,25 @@
 use analysis::analyze;
+use asm_utils::compiler::Compiler;
 use ast::analyzed::Analyzed;
-use criterion::{
-    criterion_group, criterion_main, measurement::Measurement, BenchmarkGroup, Criterion,
-};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 use compiler::inputs_to_query_callback;
 use executor::constant_evaluator;
 use number::{FieldElement, GoldilocksField};
-use std::{fs, path::Path};
+use riscv::{compile_rust_crate_to_riscv_asm, compiler::Risc};
 
 type T = GoldilocksField;
 
-fn to_field(arr: &[i32]) -> Vec<T> {
-    arr.iter().cloned().map(|x| x.into()).collect()
-}
-
-fn asm_to_analyzed(input_file: &Path) -> Analyzed<T> {
-    let contents = fs::read_to_string(input_file).unwrap();
+fn get_pil() -> Analyzed<GoldilocksField> {
+    let riscv_asm_files =
+        compile_rust_crate_to_riscv_asm("../riscv/tests/riscv_data/keccak/Cargo.toml");
+    let contents = Risc::compile(riscv_asm_files);
     let parsed = parser::parse_asm::<T>(None, &contents).unwrap();
     let analyzed = analyze(parsed).unwrap();
     let graph = airgen::compile(analyzed);
     let pil = linker::link(graph).unwrap();
-
-    pil_analyzer::analyze_string(&format!("{pil}"))
-}
-
-fn add_benchmark_asm<M: Measurement>(
-    group: &mut BenchmarkGroup<'_, M>,
-    name: &str,
-    input: Vec<i32>,
-) {
-    let input_file = Path::new(&format!("../test_data/asm/{name}.asm"))
-        .canonicalize()
-        .unwrap();
-    let name = input_file.file_name().unwrap().to_str().unwrap();
-    let analyzed = asm_to_analyzed(&input_file);
-    let analyzed = pilopt::optimize(analyzed);
-    group.bench_function(name, |b| b.iter(|| run_witgen(&analyzed, to_field(&input))));
-}
-
-fn add_benchmark_pil<M: Measurement>(
-    group: &mut BenchmarkGroup<'_, M>,
-    name: &str,
-    input: Vec<i32>,
-) {
-    let input_file = Path::new(&format!("../test_data/pil/{name}.pil"))
-        .canonicalize()
-        .unwrap();
-    let name = input_file.file_name().unwrap().to_str().unwrap();
-    let analyzed = pil_analyzer::analyze::<T>(&input_file);
-    let analyzed = pilopt::optimize(analyzed);
-    group.bench_function(name, |b| b.iter(|| run_witgen(&analyzed, to_field(&input))));
+    let analyzed = pil_analyzer::analyze_string(&format!("{pil}"));
+    pilopt::optimize(analyzed)
 }
 
 fn run_witgen<T: FieldElement>(analyzed: &Analyzed<T>, input: Vec<T>) {
@@ -60,10 +29,11 @@ fn run_witgen<T: FieldElement>(analyzed: &Analyzed<T>, input: Vec<T>) {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    // Keccak with smaller sample size
     let mut group = c.benchmark_group("keccak-executor-benchmark");
     group.sample_size(10);
-    add_benchmark_pil(&mut group, "keccak", vec![]);
+
+    let keccak_pil = get_pil();
+    group.bench_function("keccak", |b| b.iter(|| run_witgen(&keccak_pil, vec![])));
     group.finish();
 }
 
