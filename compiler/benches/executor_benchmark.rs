5589b2f9e62b8de03250f0a2779754f39fae0a54
Georg Wiese <georgwiese@gmail.com>
2023-09-02 09:30:11 +0200

    Fix benchmark (again) (#554)

--- compiler/benches/executor_benchmark.rs
+++ compiler/benches/executor_benchmark.rs
@@ -5,14 +5,16 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 use compiler::inputs_to_query_callback;
 use executor::constant_evaluator;
+use mktemp::Temp;
 use number::{FieldElement, GoldilocksField};
 use riscv::{compile_rust_crate_to_riscv_asm, compiler::Risc};
 
 type T = GoldilocksField;
 
 fn get_pil() -> Analyzed<GoldilocksField> {
+    let tmp_dir = Temp::new_dir().unwrap();
     let riscv_asm_files =
-        compile_rust_crate_to_riscv_asm("../riscv/tests/riscv_data/keccak/Cargo.toml");
+        compile_rust_crate_to_riscv_asm("../riscv/tests/riscv_data/keccak/Cargo.toml", &tmp_dir);
     let contents = Risc::compile(riscv_asm_files);
     let parsed = parser::parse_asm::<T>(None, &contents).unwrap();
     let analyzed = analyze(parsed).unwrap();
