aa766cdbf5142d79ea4efc17567f4f88cae1e5b2
Georg Wiese <georgwiese@gmail.com>
2023-09-07 12:26:23 +0000

    Add comment

--- riscv/tests/riscv_data/poseidon.rs
+++ riscv/tests/riscv_data/poseidon.rs
@@ -5,5 +5,9 @@ use runtime::coprocessors::poseidon_hash;
 #[no_mangle]
 pub fn main() {
     let h = poseidon_hash(1, 2);
+
+    // Running BN254-Poseidon permutation using the Goldilocks field
+    // on input (1, 2, 0) returns field element: 0xd0e79bcd50e0a9e4
+    // Of those, the least significant 32 bit should be selected.
     assert_eq!(h, 0x50e0a9e4);
 }
