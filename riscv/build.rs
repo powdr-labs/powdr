ed146ef3c005986abfc9064c1c48881076595d75
Lucas Clemente Vella <lvella@powdrlabs.com>
2023-09-18 13:00:28 +0100

    Letting lalrpop emit the cargo:rerun-if directives.

--- riscv/build.rs
+++ riscv/build.rs
@@ -12,8 +12,10 @@ fn main() {
 }
 
 fn build_lalrpop() {
-    println!("cargo:rerun-if-changed=src/riscv_asm.lalrpop");
-    lalrpop::process_root().unwrap();
+    lalrpop::Configuration::new()
+        .emit_rerun_directives(true)
+        .process_current_dir()
+        .unwrap();
 }
 
 fn build_instruction_tests() {
