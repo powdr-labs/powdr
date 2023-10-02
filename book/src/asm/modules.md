c857fe08b8661371784d003f6209a08404cb64cd
Georg Wiese <georgwiese@gmail.com>
2023-10-02 11:34:30 +0000

    Add Powdr standard library

--- book/src/asm/modules.md
+++ book/src/asm/modules.md
@@ -5,3 +5,5 @@ powdr exposes a module system to help organise and reuse code.
 ```
 {{#include ../../../test_data/asm/book/modules.asm}}
 ```
+
+Note that a module can't be called `std`, as this name is reserved for an upcoming powdr standard library.
--- importer/src/lib.rs
+++ importer/src/lib.rs
@@ -1,5 +1,6 @@
 mod module_loader;
 mod path_canonicalizer;
+mod powdr_std;
 
 use std::path::PathBuf;
 
@@ -8,12 +9,15 @@ pub use module_loader::load_module_files;
 use number::FieldElement;
 use parser::parse_asm;
 use path_canonicalizer::canonicalize_paths;
+use powdr_std::add_std;
 
 pub fn resolve<T: FieldElement>(
     path: Option<PathBuf>,
     module: ASMProgram<T>,
 ) -> Result<ASMProgram<T>, String> {
-    load_module_files(path, module).and_then(canonicalize_paths)
+    load_module_files(path, module)
+        .and_then(add_std)
+        .and_then(canonicalize_paths)
 }
 
 /// A test utility to process a source file until after import resolution
