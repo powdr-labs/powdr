5cd0c60f1788d2ad806ab89e774205495c8171f1
Maddiaa <47148561+Maddiaa0@users.noreply.github.com>
2023-11-14 10:11:06 +0000

    fix: remove artifacts (#10)

--- ExampleRelation_arith.hpp
+++
@@ -1,8 +0,0 @@
-
-namespace arithmetization {
-    class ExampleRelationArithmetization : public Arithmetization<5, 0> {
-        public:
-            using FF = barretenberg::fr;
-            struct Selectors {};
-    };
-} // namespace arithmetization
