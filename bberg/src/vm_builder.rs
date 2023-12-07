dbf2d4c2ada4423138ef3ef3500c6e9987c054df
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2023-12-07 15:25:46 +0000

    temp

--- bberg/src/vm_builder.rs
+++ bberg/src/vm_builder.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
 
-use ast::analyzed::AlgebraicExpression as Expression;
+use ast::analyzed::AlgebraicExpression;
 use ast::analyzed::Analyzed;
 use ast::analyzed::Identity;
 
+use ast::analyzed::IdentityKind;
+use ast::parsed::SelectedExpressions;
 use itertools::Itertools;
 use number::FieldElement;
 
@@ -42,8 +44,78 @@ pub(crate) fn analyzed_to_cpp<F: FieldElement>(
     // Inlining step to remove the intermediate poly definitions
     let analyzed_identities = analyzed.identities_with_inlined_intermediate_polynomials();
 
+    // ----------------------- Handle Standard Relation Identities -----------------------
+    // We collect all references to shifts as we traverse all identities and create relation files
+    let shifted_polys = create_relation_files(&bb_files, file_name, &analyzed_identities);
+
+    // ----------------------- Handle Lookup / Permutation Relation Identities -----------------------
+    // We will likely need to create a new set of permutation identities for each lookup / permutation pair we come across
+    handle_permutations(analyzed);
+    // handle_lookups();
+
+
+
+    // TODO: hack - this can be removed with some restructuring
+    let shifted_polys: Vec<String> = shifted_polys
+        .clone()
+        .iter()
+        .map(|s| s.replace("_shift", ""))
+        .collect();
+
+    // Collect all column names and determine if they need a shift or not
+    let ColumnGroups {
+        fixed,
+        witness,
+        all_cols,
+        unshifted,
+        to_be_shifted,
+        shifted,
+        all_cols_with_shifts,
+    } = get_all_col_names(fixed, witness, &shifted_polys);
+
+    // bb_files.create_declare_views(file_name, &all_cols_with_shifts);
+
+    // // ----------------------- Create the circuit builder file -----------------------
+    // bb_files.create_circuit_builder_hpp(
+    //     file_name,
+    //     &relations,
+    //     &all_cols,
+    //     &to_be_shifted,
+    //     &all_cols_with_shifts,
+    // );
+
+    // // ----------------------- Create the flavor file -----------------------
+    // bb_files.create_flavor_hpp(
+    //     file_name,
+    //     &relations,
+    //     &fixed,
+    //     &witness,
+    //     &all_cols,
+    //     &to_be_shifted,
+    //     &shifted,
+    //     &all_cols_with_shifts,
+    // );
+
+    // // ----------------------- Create the composer files -----------------------
+    // bb_files.create_composer_cpp(file_name, &all_cols);
+    // bb_files.create_composer_hpp(file_name);
+
+    // // ----------------------- Create the Verifier files -----------------------
+    // bb_files.create_verifier_cpp(file_name, &witness);
+    // bb_files.create_verifier_hpp(file_name);
+
+    // // ----------------------- Create the Prover files -----------------------
+    // bb_files.create_prover_cpp(file_name, &unshifted, &to_be_shifted);
+    // bb_files.create_prover_hpp(file_name);
+}
+
+
+/// TODO: MOVE THIS OUT OF THIS FILE????
+/// Does this need to return all of the shifted polys that it collects>
+/// TODO: restructure this so that we do not have to pass in bb files abd the name at the top level
+fn create_relation_files<F: FieldElement>(bb_files: &BBFiles, file_name: &str, analyzed_identities: &Vec<Identity<AlgebraicExpression<F>>>) -> Vec<String> {
     // Group relations per file
-    let grouped_relations = group_relations_per_file(&analyzed_identities);
+    let grouped_relations: HashMap<String, Vec<Identity<AlgebraicExpression<F>>>> = group_relations_per_file(&analyzed_identities);
     let relations = grouped_relations.keys().cloned().collect_vec();
 
     // Contains all of the rows in each relation, will be useful for creating composite builder types
@@ -74,59 +146,62 @@ pub(crate) fn analyzed_to_cpp<F: FieldElement>(
         );
     }
 
-    // TODO: hack - this can be removed with some restructuring
-    let shifted_polys: Vec<String> = shifted_polys
-        .clone()
-        .iter()
-        .map(|s| s.replace("_shift", ""))
-        .collect();
+    shifted_polys
 
-    // Collect all column names and determine if they need a shift or not
-    let ColumnGroups {
-        fixed,
-        witness,
-        all_cols,
-        unshifted,
-        to_be_shifted,
-        shifted,
-        all_cols_with_shifts,
-    } = get_all_col_names(fixed, witness, &shifted_polys);
-
-    bb_files.create_declare_views(file_name, &all_cols_with_shifts);
-
-    // ----------------------- Create the circuit builder file -----------------------
-    bb_files.create_circuit_builder_hpp(
-        file_name,
-        &relations,
-        &all_cols,
-        &to_be_shifted,
-        &all_cols_with_shifts,
-    );
-
-    // ----------------------- Create the flavor file -----------------------
-    bb_files.create_flavor_hpp(
-        file_name,
-        &relations,
-        &fixed,
-        &witness,
-        &all_cols,
-        &to_be_shifted,
-        &shifted,
-        &all_cols_with_shifts,
-    );
-
-    // ----------------------- Create the composer files -----------------------
-    bb_files.create_composer_cpp(file_name, &all_cols);
-    bb_files.create_composer_hpp(file_name);
-
-    // ----------------------- Create the Verifier files -----------------------
-    bb_files.create_verifier_cpp(file_name, &witness);
-    bb_files.create_verifier_hpp(file_name);
-
-    // ----------------------- Create the Prover files -----------------------
-    bb_files.create_prover_cpp(file_name, &unshifted, &to_be_shifted);
-    bb_files.create_prover_hpp(file_name);
 }
+
+
+// Maybe this is not the right thing to do?
+#[derive(Debug)]
+struct Permutation {
+    left: PermSide,
+    right: PermSide
+}
+
+// TODO: rename
+#[derive(Debug)]
+struct PermSide {
+    selector: Option<String>,
+    cols: Vec<String>
+}
+
+fn handle_permutations<F: FieldElement>(analyzed: &Analyzed<F>) -> Vec<Permutation> {
+    let perms: Vec<&Identity<AlgebraicExpression<F>>> = analyzed.identities.iter().filter(|identity| matches!(identity.kind, IdentityKind::Permutation)).collect();
+    let new_perms = perms.iter().map(|perm| 
+        Permutation {
+            left: get_perm_side(&perm.left),
+            right: get_perm_side(&perm.right)
+        }).collect_vec();
+
+    dbg!(&new_perms);
+
+    // For every permutation set that we create, we will need to create an inverse column ( helper function )
+    // TODO: how do we determine the name of this inverse column?
+
+
+    new_perms
+}
+
+fn get_perm_side<F: FieldElement>(def: &SelectedExpressions<AlgebraicExpression<F>>) -> PermSide {
+    let get_name = |expr: &AlgebraicExpression<F>| match expr {
+        AlgebraicExpression::Reference(a_ref) => sanitize_name(&a_ref.name),
+        _ => panic!("Expected reference")
+    };
+
+    PermSide {
+        selector: def.selector.as_ref().map(|expr| get_name(&expr)),
+        cols: def.expressions.iter().map(|expr| get_name(&expr)).collect_vec()
+    }
+}
+
+
+
+// pub struct SelectedExpressions<Expr> {
+//     pub selector: Option<Expr>,
+//     pub expressions: Vec<Expr>,
+// }
+
+
 
 /// Group relations per file
 ///
@@ -149,8 +224,8 @@ pub(crate) fn analyzed_to_cpp<F: FieldElement>(
 ///
 /// This allows us to generate a relation.hpp file containing ONLY the relations for that .pil file
 fn group_relations_per_file<F: FieldElement>(
-    identities: &[Identity<Expression<F>>],
-) -> HashMap<String, Vec<Identity<Expression<F>>>> {
+    identities: &[Identity<AlgebraicExpression<F>>],
+) -> HashMap<String, Vec<Identity<AlgebraicExpression<F>>>> {
     identities
         .iter()
         .cloned()
