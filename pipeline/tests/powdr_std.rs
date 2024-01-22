fc15d927447a042eb4d5a345f1ca09744a6f5a4d
chriseth <chris@ethereum.org>
2024-01-22 18:40:19 +0100

    Modular operations.

--- pipeline/tests/powdr_std.rs
+++ pipeline/tests/powdr_std.rs
@@ -1,6 +1,11 @@
-use powdr_number::GoldilocksField;
-use powdr_pipeline::test_util::{gen_estark_proof, gen_halo2_proof, verify_test_file};
+use powdr_number::{FieldElement, GoldilocksField};
+
+use powdr_pipeline::test_util::{
+    evaluate_integer_function, gen_estark_proof, gen_halo2_proof, std_analyzed, verify_test_file,
+};
 use test_log::test;
+
+use num_traits::Num;
 
 #[test]
 fn poseidon_bn254_test() {
@@ -35,4 +40,127 @@ fn arith_test() {
     gen_estark_proof(f, Default::default());
     // Halo2 test runs out of memory on CI
     // gen_halo2_proof(f, Default::default());
+}
+
+#[test]
+fn ff_reduce_mod_7() {
+    let test_inputs = vec![
+        -22, -21, -20, -8, -7, -6, -2, -1, -0, -1, -2, -3, 4, 5, 6, 7, 8, 9, 13, 14, 15, 20, 21, 22,
+    ];
+    let analyzed = std_analyzed::<GoldilocksField>();
+    for x in test_inputs {
+        let x = num_bigint::BigInt::from(x);
+        let modulus = num_bigint::BigInt::from(7);
+        let result = evaluate_integer_function(
+            &analyzed,
+            "std::math::ff::reduce",
+            vec![x.clone(), modulus.clone()],
+        );
+        assert!(num_bigint::BigInt::from(0) <= result && result < modulus);
+        if x < result {
+            assert_eq!((result - x) % modulus, 0.into());
+        } else {
+            assert_eq!((x - result) % modulus, 0.into());
+        }
+    }
+}
+
+#[test]
+fn ff_inverse() {
+    let test_inputs = vec![
+        (1, 11),
+        (1, 7),
+        (2, 7),
+        (3, 7),
+        (4, 7),
+        (5, 7),
+        (6, 7),
+        (2, 17),
+        (3, 17),
+        (9, 17),
+        (15, 17),
+        (16, 17),
+    ];
+    let analyzed = std_analyzed::<GoldilocksField>();
+    for (x, modulus) in test_inputs {
+        let x = num_bigint::BigInt::from(x);
+        let modulus = num_bigint::BigInt::from(modulus);
+        let result = evaluate_integer_function(
+            &analyzed,
+            "std::math::ff::inverse",
+            vec![x.clone(), modulus.clone()],
+        );
+        assert_eq!((result * x) % modulus, 1.into());
+    }
+}
+
+#[test]
+fn ff_add_sub_mul_div() {
+    let inputs = vec![
+        (1, 0, 11),
+        (1, 6, 7),
+        (6, 6, 7),
+        (0, 0, 17),
+        (0, 16, 17),
+        (16, 16, 17),
+        (3, 8, 17),
+        (16, 1, 17),
+        (5, 9, 17),
+        (3, 14, 17),
+    ];
+    let analyzed = std_analyzed::<GoldilocksField>();
+    for (x, y, modulus) in inputs {
+        let x = num_bigint::BigInt::from(x);
+        let y = num_bigint::BigInt::from(y);
+        let modulus = num_bigint::BigInt::from(modulus);
+        let result = evaluate_integer_function(
+            &analyzed,
+            "std::math::ff::add",
+            vec![x.clone(), y.clone(), modulus.clone()],
+        );
+        assert_eq!((x.clone() + y.clone()) % modulus.clone(), result);
+
+        let result = evaluate_integer_function(
+            &analyzed,
+            "std::math::ff::sub",
+            vec![x.clone(), y.clone(), modulus.clone()],
+        );
+        assert_eq!(
+            (x.clone() - y.clone() + modulus.clone()) % modulus.clone(),
+            result
+        );
+
+        let result = evaluate_integer_function(
+            &analyzed,
+            "std::math::ff::mul",
+            vec![x.clone(), y.clone(), modulus.clone()],
+        );
+        assert_eq!((x.clone() * y.clone()) % modulus.clone(), result);
+
+        if y != 0.into() {
+            let result = evaluate_integer_function(
+                &analyzed,
+                "std::math::ff::div",
+                vec![x.clone(), y.clone(), modulus.clone()],
+            );
+            assert_eq!(x, (result * y) % modulus);
+        }
+    }
+}
+
+#[test]
+fn ff_inv_big() {
+    let analyzed = std_analyzed::<GoldilocksField>();
+    // modulus of the secp256k1 base field
+    let modulus = num_bigint::BigInt::from_str_radix(
+        "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f",
+        16,
+    )
+    .unwrap();
+    let x = modulus.clone() - num_bigint::BigInt::from(17);
+    let result = evaluate_integer_function(
+        &analyzed,
+        "std::math::ff::inverse",
+        vec![x.clone(), modulus.clone()],
+    );
 }
---
+++ std/math/ff.asm
@@ -0,0 +1,43 @@
+/// Inverts `x` in the finite field with modulus `modulus`.
+/// Assumes that `modulus` is prime, but does not check it.
+let inverse = |x, modulus|
+    if x <= 0 || x >= modulus {
+        std::check::panic("Tried to compute the inverse of zero, of a negative number or a number outside the field.")
+    } else {
+        reduce(extended_gcd(x, modulus)[0], modulus)
+    };
+
+/// Computes `x + y` modulo the modulus.
+let add = |x, y, modulus| reduce(x + y, modulus);
+
+/// Computes `x - y` modulo the modulus.
+let sub = |x, y, modulus| reduce(x - y, modulus);
+
+/// Computes `x * y` modulo the modulus.
+let mul = |x, y, modulus| reduce(x * y, modulus);
+
+/// Computes `x / y` modulo the modulus.
+let div = |x, y, modulus| mul(x, inverse(y, modulus), modulus);
+
+/// Reduces `x` modulo `modulus`, so that it is in the range
+/// between `0` and `modulus`. Works on negative `x`.
+let reduce = |x, modulus|
+    if x < 0 {
+        (modulus - ((-x) % modulus)) % modulus
+    } else {
+        x % modulus
+    };
+
+let extended_gcd = |a, b|
+    if b == 0 {
+        if a == 1 {
+            [1, 0]
+        } else {
+            // a is the gcd, but we do not really want to compute it.
+            std::check::panic("Inputs are not co-prime, inverse does not exist.")
+        }
+    } else {
+        // TODO this is written in a complicated way
+        // because we do not have tuple destructuring assignment
+        (|r| [r[1], r[0] - (a / b) * r[1]])(extended_gcd(b, a % b))
+    };
