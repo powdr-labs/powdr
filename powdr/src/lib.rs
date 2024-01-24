62d8bb269016d1f1f3f10acb34920df6ccf9c866
Leo Alt <leo@ethereum.org>
2024-01-24 02:02:22 +0100

    add some aliases to powdr crate

--- powdr/src/lib.rs
+++ powdr/src/lib.rs
@@ -1,5 +1,12 @@
-pub use powdr_executor;
-pub use powdr_number;
-pub use powdr_pipeline;
-pub use powdr_riscv;
-pub use powdr_riscv_executor;
+pub use powdr_backend as backend;
+pub use powdr_executor as executor;
+pub use powdr_number as number;
+pub use powdr_pipeline as pipeline;
+pub use powdr_riscv as riscv;
+pub use powdr_riscv_executor as riscv_executor;
+
+pub use powdr_pipeline::Pipeline;
+
+pub use powdr_number::Bn254Field;
+pub use powdr_number::FieldElement;
+pub use powdr_number::GoldilocksField;
