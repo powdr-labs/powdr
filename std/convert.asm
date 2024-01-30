9a946bf8828e487947adc26688d37718b3f33908
chriseth <chris@ethereum.org>
2024-01-30 10:45:54 +0100

    Add to_col conversion function.

--- std/convert.asm
+++ std/convert.asm
@@ -8,3 +8,7 @@ let int = [];
 /// Panics if the input is negative or larger or equal to the field modulus.
 /// The actual implementation is replaced by a built-in function.
 let fe = [];
+
+/// Converts a function `int -> int` to a column, i.e. converts its
+/// return type to field element.
+let to_col: (int -> int) -> col = |f| |i| fe(f(i));
