559db51154c29745c22090373b2af4fc13202d6c
Georg Wiese <georgwiese@gmail.com>
2024-02-19 14:40:30 +0100

    Modify ASM so that Poseidon machine takes 2 memory addresses

--- test_data/std/poseidon_bn254_test.asm
+++ test_data/std/poseidon_bn254_test.asm
@@ -10,9 +10,34 @@ machine Main {
     reg X3[<=];
     reg A;
 
+    // Memory
+    col witness m_addr;
+    col witness m_step;
+    col witness m_change;
+    col witness m_value;
+    col witness m_is_write;
+    col witness m_is_read;
+
+    col fixed POSITIVE(i) { i + 1 };
+    col fixed LAST  = [0]* + [1];
+    col fixed STEP(i) { i };
+
+    m_change * (1 - m_change) = 0;
+    (m_addr' - m_addr) * (1 - m_change) = 0;
+    (1 - LAST) { m_change * (m_addr' - m_addr) + (1 - m_change) * (m_step' - m_step) } in POSITIVE;
+    (1 - m_change) * LAST = 0;
+    m_is_write * (1 - m_is_write) = 0;
+    m_is_read * (1 - m_is_read) = 0;
+    m_is_read * m_is_write = 0;
+    (1 - m_is_write') * (1 - m_change) * (m_value' - m_value) = 0;
+    (1 - m_is_write') * m_change * m_value' = 0;
+
+    instr mstore X0, X1 { { X0, STEP, X1 } is m_is_write { m_addr, m_step, m_value } }
+    instr mload X0 -> X1 { { X0, STEP, X1 } is m_is_read { m_addr, m_step, m_value } }
+
     PoseidonBN254 poseidon;
 
-    instr poseidon X0, X1, X2 -> X3 = poseidon.poseidon_permutation;
+    instr poseidon X0, X1 -> = poseidon.poseidon_permutation;
 
     instr assert_eq X0, X1 {
         X0 = X1
@@ -22,16 +47,23 @@ machine Main {
 
         // Test vector for poseidonperm_x5_254_3 from:
         // https://extgit.iaik.tugraz.at/krypto/hadeshash/-/blob/master/code/test_vectors.txt
-        A <== poseidon(0, 1, 2);
+        
+        // Memory addresses need to be "aligned" (i.e., divisible by 4)
+        mstore 40, 0;
+        mstore 44, 1;
+        mstore 48, 2;
+        poseidon 40, 400;
+        A <== mload(400);
+        // This fails unless the Poseidon machine writes the correct result to memory, which requires PIL modification.
         assert_eq A, 0x115cc0f5e7d690413df64c6b9662e9cf2a3617f2743245519e19607a4417189a;
 
         // Validated by modifying and running `code/poseidonperm_x5_254_3.sage` from the same repository.
-        A <== poseidon(0, 0, 0);
-        assert_eq A, 0x2098f5fb9e239eab3ceac3f27b81e481dc3124d55ffed523a839ee8446b64864;
-        A <== poseidon(-1, -2, -3);
-        assert_eq A, 0x15492e60e5ae9f3d254f2d44650795c4cac1c924981fb7ca8645a7790971b70c;
-        A <== poseidon(A + 1, A + 2, A + 3);
-        assert_eq A, 0x188ada144ed909426b0396e967a82e26d739652cff288d13306279d91f29010c;
+        // A <== poseidon(0, 0, 0);
+        // assert_eq A, 0x2098f5fb9e239eab3ceac3f27b81e481dc3124d55ffed523a839ee8446b64864;
+        // A <== poseidon(-1, -2, -3);
+        // assert_eq A, 0x15492e60e5ae9f3d254f2d44650795c4cac1c924981fb7ca8645a7790971b70c;
+        // A <== poseidon(A + 1, A + 2, A + 3);
+        // assert_eq A, 0x188ada144ed909426b0396e967a82e26d739652cff288d13306279d91f29010c;
 
         return;
     }
