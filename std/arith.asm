6b585e63938ec0bb75619f49bf6a6500c4a15636
chriseth <chris@ethereum.org>
2024-03-25 17:32:06 +0100

    Remove braces.

--- std/arith.asm
+++ std/arith.asm
@@ -105,28 +105,22 @@ machine Arith(CLK32_31, operation_id){
     };
 
     let q1_hint = || if is_ec_operation() == 1 {
-        // TODO remove braces
-        {
-            let x1 = x1_int();
-            let x2 = x2_int();
-            let s = s_int();
-            compute_q1(x1, x2, compute_x3_int(x1, x2, s), s)
-        }
+        let x1 = x1_int();
+        let x2 = x2_int();
+        let s = s_int();
+        compute_q1(x1, x2, compute_x3_int(x1, x2, s), s)
     } else {
         0
     };
 
     let q2_hint = || if is_ec_operation() == 1 {
-        // TODO remove braces
-        {
-            let x1 = x1_int();
-            let x2 = x2_int();
-            let y1 = y1_int();
-            let s = s_int();
-            let x3 = compute_x3_int(x1, x2, s);
-            let y3 = compute_y3_int(x1, y1, x3, s);
-            compute_q2(x1, y1, x3, y3, s)
-        }
+        let x1 = x1_int();
+        let x2 = x2_int();
+        let y1 = y1_int();
+        let s = s_int();
+        let x3 = compute_x3_int(x1, x2, s);
+        let y3 = compute_y3_int(x1, y1, x3, s);
+        compute_q2(x1, y1, x3, y3, s)
     } else {
         0
     };
