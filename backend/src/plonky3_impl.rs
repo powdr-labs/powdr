538ee58b95bfe0dfc0de580c1f5a71a3425801c2
schaeff <thibaut@schaeff.fr>
2024-04-04 18:34:05 +0200

    fix plonky3 impl

--- backend/src/plonky3_impl.rs
+++ backend/src/plonky3_impl.rs
@@ -1,6 +1,7 @@
 use std::{io, path::Path};
 
 use powdr_ast::analyzed::Analyzed;
+use powdr_executor::witgen::WitgenCallback;
 use powdr_number::{DegreeType, FieldElement};
 use powdr_plonky3::Plonky3Prover;
 
@@ -8,45 +9,42 @@ use crate::{Backend, BackendFactory, Error, Proof};
 
 pub(crate) struct Plonky3ProverFactory;
 
-impl<F: FieldElement> BackendFactory<F> for Plonky3ProverFactory {
+impl<T: FieldElement> BackendFactory<T> for Plonky3ProverFactory {
     fn create<'a>(
         &self,
-        pil: &'a Analyzed<F>,
-        fixed: &'a [(String, Vec<F>)],
+        _pil: &'a Analyzed<T>,
+        _fixed: &'a [(String, Vec<T>)],
         _output_dir: Option<&'a Path>,
-        setup: Option<&mut dyn io::Read>,
-        verification_key: Option<&mut dyn io::Read>,
-    ) -> Result<Box<dyn crate::Backend<'a, F> + 'a>, Error> {
+        _setup: Option<&mut dyn io::Read>,
+        _verification_key: Option<&mut dyn io::Read>,
+    ) -> Result<Box<dyn crate::Backend<'a, T> + 'a>, Error> {
         todo!()
     }
 
-    fn generate_setup(
-        &self,
-        size: DegreeType,
-        mut output: &mut dyn io::Write,
-    ) -> Result<(), Error> {
+    fn generate_setup(&self, _size: DegreeType, _output: &mut dyn io::Write) -> Result<(), Error> {
         todo!()
     }
 }
 
 impl<'a, T: FieldElement> Backend<'a, T> for Plonky3Prover<'a, T> {
-    fn verify(&self, proof: &[u8], instances: &[Vec<T>]) -> Result<(), Error> {
+    fn verify(&self, _proof: &[u8], _instances: &[Vec<T>]) -> Result<(), Error> {
         todo!()
     }
 
     fn prove(
         &self,
-        witness: &[(String, Vec<T>)],
-        prev_proof: Option<Proof>,
+        _witness: &[(String, Vec<T>)],
+        _prev_proof: Option<Proof>,
+        _witgen_callback: WitgenCallback<T>,
     ) -> Result<Proof, Error> {
         todo!()
     }
 
-    fn export_setup(&self, mut output: &mut dyn io::Write) -> Result<(), Error> {
+    fn export_setup(&self, _output: &mut dyn io::Write) -> Result<(), Error> {
         todo!()
     }
 
-    fn export_verification_key(&self, mut output: &mut dyn io::Write) -> Result<(), Error> {
+    fn export_verification_key(&self, _output: &mut dyn io::Write) -> Result<(), Error> {
         todo!()
     }
 }
