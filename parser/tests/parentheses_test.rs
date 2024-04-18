f9ee92f98605a69007da8a0d6ecb8fc0703758ef
Amin Latifi <a.latifi.al@gmail.com>
2024-04-18 12:51:30 +0330

    Added more tests

---
+++ parser/tests/parentheses_test.rs
@@ -0,0 +1,34 @@
+#[cfg(test)]
+mod test {
+    use powdr_parser::parse;
+    use powdr_parser_util::UnwrapErrToStderr;
+    use pretty_assertions::assert_eq;
+    use test_log::test;
+
+    fn test_paren(input: &str, expected: &str) {
+        let printed = format!("{}", parse(None, input).unwrap_err_to_stderr());
+        assert_eq!(expected.trim(), printed.trim());
+    }
+
+    #[test]
+    fn test_parentheses() {
+        type TestCase = (&'static str, &'static str);
+        let test_cases: Vec<TestCase> = vec![
+            // Remove unneeded
+            ("((x + y) * z);", "(x + y) * z;"),
+            ("(x * y) * z;", "x * y * z;"),
+            ("x * (y * z);", "x * y * z;"),
+            ("(x / y) / z;", "x / y / z;"),
+            ("x / (y / z);", "x / y / z;"),
+            // Don't remove needed
+            ("(x + y) * z;", "(x + y) * z;"),
+            // Don't add extra
+            ("x * y * z;", "x * y * z;"),
+            ("x / y / z;", "x / y / z;"),
+        ];
+
+        for (input, expected) in test_cases {
+            test_paren(input, expected);
+        }
+    }
+}
