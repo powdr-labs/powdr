d2d11c7ae1e1f42be9643c327441c559231210bc
Georg Wiese <georgwiese@gmail.com>
2024-04-18 23:25:15 +0200

    More complex example

--- executor/src/witgen/processor.rs
+++ executor/src/witgen/processor.rs
@@ -33,24 +33,37 @@ impl CopyConstraints {
     ) -> Self {
         let is_example = witness_cols
             .iter()
-            .all(|c| fixed_data.column_name(c).starts_with("PlonkCircuit."));
+            .all(|c| fixed_data.column_name(c).starts_with("main_pythagoras."));
 
         if is_example {
-            // Hard-code copy constraints for now.
-            // - a[0] = b[0]
-            // - a[1] = b[1]
-            // - c[0] = a[2]
-            // - c[1] = b[2]
-            let a = fixed_data.try_column_by_name("PlonkCircuit.a").unwrap();
-            let b = fixed_data.try_column_by_name("PlonkCircuit.b").unwrap();
-            let c = fixed_data.try_column_by_name("PlonkCircuit.c").unwrap();
+            log::info!("Using hard-coded copy constraints for Pythagoras example");
+            // Hard-code copy constraints every 4 rows:
+            // - a[0] = a[1]
+            // - a[0] = b[1]
+            // - b[0] = a[2]
+            // - b[0] = b[2]
+            // - c[1] = a[3]
+            // - c[2] = b[3]
+            // - c[0] = c[3]
+            let a = fixed_data.try_column_by_name("main_pythagoras.a").unwrap();
+            let b = fixed_data.try_column_by_name("main_pythagoras.b").unwrap();
+            let c = fixed_data.try_column_by_name("main_pythagoras.c").unwrap();
             let d = fixed_data.degree;
-            CopyConstraints::new(vec![
-                ((a, RowIndex::from_i64(0, d)), (b, RowIndex::from_i64(0, d))),
-                ((a, RowIndex::from_i64(1, d)), (b, RowIndex::from_i64(1, d))),
-                ((c, RowIndex::from_i64(0, d)), (a, RowIndex::from_i64(2, d))),
-                ((c, RowIndex::from_i64(1, d)), (b, RowIndex::from_i64(2, d))),
-            ])
+            let index =
+                |block_index, local_index| RowIndex::from_degree(block_index * 4 + local_index, d);
+
+            let mut constraints = Vec::new();
+
+            for i in 0..d / 4 {
+                constraints.push(((a, index(i, 0)), (a, index(i, 1))));
+                constraints.push(((a, index(i, 0)), (b, index(i, 1))));
+                constraints.push(((b, index(i, 0)), (a, index(i, 2))));
+                constraints.push(((b, index(i, 0)), (b, index(i, 2))));
+                constraints.push(((c, index(i, 1)), (a, index(i, 3))));
+                constraints.push(((c, index(i, 2)), (b, index(i, 3))));
+                constraints.push(((c, index(i, 0)), (c, index(i, 3))));
+            }
+            CopyConstraints::new(constraints)
         } else {
             CopyConstraints::default()
         }
