ef0af35e522d4772b3a27e791635a9012c7721e5
Amin Latifi <a.latifi.al@gmail.com>
2024-04-21 09:44:46 +0330

    Removed parentheses tests from display.rs

--- parser/tests/parentheses_test.rs
+++ parser/tests/parentheses_test.rs
@@ -5,16 +5,18 @@ mod test {
     use pretty_assertions::assert_eq;
     use test_log::test;
 
-    fn test_paren(input: &str, expected: &str) {
+    type TestCase = (&'static str, &'static str);
+
+    fn test_paren(test_case: &TestCase) {
+        let (input, expected) = test_case;
         let printed = format!("{}", parse(None, input).unwrap_err_to_stderr());
         assert_eq!(expected.trim(), printed.trim());
     }
 
     #[test]
-    fn test_parentheses() {
-        type TestCase = (&'static str, &'static str);
+    fn test_parentheses_simple() {
         let test_cases: Vec<TestCase> = vec![
-            ("-x + y;", "-x + y;"),
+            ("-x + y * !z;", "-x + y * !z;"),
             ("x = (y <= z);", "x = (y <= z);"),
             // Remove unneeded
             ("(x * y) * z;", "x * y * z;"),
@@ -36,8 +38,28 @@ mod test {
             ("x / y / z;", "x / y / z;"),
         ];
 
-        for (input, expected) in test_cases {
-            test_paren(input, expected);
+        for test_case in test_cases {
+            test_paren(&test_case);
+        }
+    }
+
+    #[test]
+    fn test_parentheses_complex() {
+        let test_cases: Vec<TestCase> = vec![
+            // Don't change concise expression
+            (
+                "a | b * (c << d + e) & (f ^ g) = h * (i + g);",
+                "a | b * (c << d + e) & (f ^ g) = h * (i + g);",
+            ),
+            // Remove extra parentheses
+            (
+                "(a | ((b * (c << (d + e))) & (f ^ g))) = (h * ((i + g)));",
+                "a | b * (c << d + e) & (f ^ g) = h * (i + g);",
+            ),
+        ];
+
+        for test_case in test_cases {
+            test_paren(&test_case);
         }
     }
 }
