63224ea58e99e59d3eadf8e8c2b10fc3935b78e4
Leo Alt <leo@powdrlabs.com>
2024-04-25 14:46:56 +0200

    increase continuations bootloader's page size

--- riscv/src/continuations/bootloader.rs
+++ riscv/src/continuations/bootloader.rs
@@ -9,7 +9,7 @@ use super::memory_merkle_tree::MerkleTree;
 pub const MEMORY_SIZE_LOG: usize = 32;
 
 /// Page size is 1KB
-pub const PAGE_SIZE_BYTES_LOG: usize = 10;
+pub const PAGE_SIZE_BYTES_LOG: usize = 11;
 
 /// 32-Bit architecture -> 4 bytes per word
 pub const BYTES_PER_WORD: usize = 4;
