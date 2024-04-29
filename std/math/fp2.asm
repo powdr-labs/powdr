f4068b241008d528c2dff708e2fba2d503f11775
Georg Wiese <georgwiese@gmail.com>
2024-04-29 11:43:05 +0200

    Add doc strings

--- std/math/fp2.asm
+++ std/math/fp2.asm
@@ -5,11 +5,15 @@ use std::convert::expr;
 use std::field::modulus;
 use std::prover::eval;
 
-
+// An extension field element, represented as a tuple (a0, a1) of expressions (which evaluate to field elements).
+// An element of F_{p^2} is a polynomial a0 + a1 * x (mod x^2 - 7).
+// The polynomial x^2 - 7 is irreducible in both the Goldilocks and BN254 field
+// (which follows from 7 not having a square root in both fields).
 enum Fp2Expr {
     Fp2(expr, expr)
 }
 
+// An element of F_{p^2}, but represented as a tuple of *evaluated* field elements.
 enum Fp2Value {
     Fp2(fe, fe)
 }
@@ -36,9 +40,9 @@ let sub_ext = |a, b| match (a, b) {
 
 let mul_ext = |a, b| match (a, b) {
     (Fp2Expr::Fp2(a0, a1), Fp2Expr::Fp2(b0, b1)) => Fp2Expr::Fp2(
-        // Multiplication modulo the polynomial x^2 - 7 => x^2 = 7 (mod x^2 - 7)
-        // x^2 - 7 is an irreducible polynomial in both the Goldilocks and BN254 field
-        // (because 7 is not a square residue in both)
+        // Multiplication modulo the polynomial x^2 - 7. We'll use the fact
+        // that x^2 == 7 (mod x^2 - 7), so:
+        // (a0 + a1 * x) * (b0 + b1 * x) = a0 * b0 + 7 * a1 * b1 + (a1 * b0 + a0 * b1) * x (mod x^2 - 7)
         a0 * b0 + 7 * a1 * b1,
         a1 * b0 + a0 * b1
     )
@@ -57,7 +61,7 @@ let eq_ext = |a, b| match (a, b) {
     (Fp2Value::Fp2(a0, a1), Fp2Value::Fp2(b0, b1)) => (a0 == b0) && (a1 == b1)
 };
 
-// Extension field inversion (defined on fe instead of expression)
+// Extension field inversion (defined on fe instead of int)
 let inv_field: fe -> fe = |x| fe(inverse(int(x), modulus()));
 let inv_ext: Fp2Value -> Fp2Value = |a| match a {
     // The inverse of (a0, a1) is a point (b0, b1) such that:
@@ -73,20 +77,19 @@ let inv_ext: Fp2Value -> Fp2Value = |a| match a {
 };
 
 mod test {
-    use std::math::fp2::Fp2Expr;
-    use std::math::fp2::Fp2Value;
-    use std::math::fp2::add_ext;
-    use std::math::fp2::sub_ext;
-    use std::math::fp2::mul_ext;
-    use std::math::fp2::inv_ext;
-    use std::math::fp2::eval_ext;
-    use std::math::fp2::expr_ext;
-    use std::math::fp2::eq_ext;
-    use std::math::fp2::zero_expr;
-    use std::math::fp2::zero_value;
-    use std::math::fp2::one_expr;
-    use std::math::fp2::one_value;
-    use std::prover::eval;
+    use super::Fp2Expr;
+    use super::Fp2Value;
+    use super::add_ext;
+    use super::sub_ext;
+    use super::mul_ext;
+    use super::inv_ext;
+    use super::eval_ext;
+    use super::expr_ext;
+    use super::eq_ext;
+    use super::zero_expr;
+    use super::zero_value;
+    use super::one_expr;
+    use super::one_value;
     use std::check::assert;
     use std::array::map;
 
