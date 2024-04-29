0b97d39f4bda07184f2e294f11fbab6ae96bc358
Georg Wiese <georgwiese@gmail.com>
2024-04-29 11:02:07 +0200

    Use Fp2 library

--- std/protocols/permutation.asm
+++ std/protocols/permutation.asm
@@ -4,6 +4,17 @@ use std::array::len;
 use std::convert::int;
 use std::check::assert;
 use std::field::modulus;
+use std::math::fp2::Fp2Expr;
+use std::math::fp2::add_ext;
+use std::math::fp2::sub_ext;
+use std::math::fp2::mul_ext;
+
+let next_ext = |a| match a {
+    Fp2Expr::Fp2(a0, a1) => Fp2Expr::Fp2(a0', a1')
+};
+let unpack_ext = |a| match a {
+    Fp2Expr::Fp2(a0, a1) => [a0, a1]
+};
 
 let is_first: col = |i| if i == 0 { 1 } else { 0 };
 
@@ -29,6 +40,8 @@ let permutation = |acc, lhs_selector, lhs, rhs_selector, rhs| {
     let _ = assert(modulus() > 2**100 || modulus() == GOLDILOCKS_PRIME, || "No implementation on this small field");
     let _ = assert(len(acc) == 2, || "Expected 2 accumulators");
 
+    let acc_ext = Fp2Expr::Fp2(acc[0], acc[1]);
+
     // On the Goldilocks field, we evaluate the polynomial on the F_{p^2} extension field
     // modulo the irreducible polynomial x^2 - 7.
     // TODO: This is always true, to test that phase-2 witgen works
@@ -38,31 +51,22 @@ let permutation = |acc, lhs_selector, lhs, rhs_selector, rhs| {
     // On the extension field, we'll need two field elements to represent the challenge.
     // If we don't need an extension field, we can simply set the second component to 0,
     // in which case the operations below effectively only operate on the first component.
-    let alpha = if needs_extension {[alpha1, alpha2]} else {[alpha1, 0]};
-    let beta = if needs_extension {[beta1, beta2]} else {[beta1, 0]};
-
-    // Define operations on extension field elements, which are array of length 2.
-    // Addition and subtraction is just performed element-wise.
-    // Multiplication works by multiplying out the two input polynomials and using the fact
-    // that x^2 == 7 (mod x^2 - 7) to get rid of the x^2 terms.
-    let add_ext = |a, b| [a[0] + b[0], a[1] - b[1]];
-    let sub_ext = |a, b| [a[0] - b[0], a[1] - b[1]];
-    let mul_ext = |a, b| [a[0] * b[0] + 7 * a[1] * b[1], a[1] * b[0] + a[0] * b[1]];
-    let next_ext = |a| [a[0]', a[1]'];
+    let alpha = if needs_extension {Fp2Expr::Fp2(alpha1, alpha2)} else {Fp2Expr::Fp2(alpha1, 0)};
+    let beta = if needs_extension {Fp2Expr::Fp2(beta1, beta2)} else {Fp2Expr::Fp2(beta1, 0)};
 
     // Maps [x_1, x_2, ..., x_n] to alpha**(n - 1) * x_1 + alpha ** (n - 2) * x_2 + ... + x_n
-    let compress_expression_array = |expr_array| fold(expr_array, [0, 0], |sum_acc, el| add_ext(mul_ext(alpha, sum_acc), [el, 0]));
+    let compress_expression_array = |expr_array| fold(expr_array, Fp2Expr::Fp2(0, 0), |sum_acc, el| add_ext(mul_ext(alpha, sum_acc), Fp2Expr::Fp2(el, 0)));
 
-    let lhs_folded = mul_ext([lhs_selector, 0], compress_expression_array(lhs));
-    let rhs_folded = mul_ext([rhs_selector, 0], compress_expression_array(rhs));
+    let lhs_folded = mul_ext(Fp2Expr::Fp2(lhs_selector, 0), compress_expression_array(lhs));
+    let rhs_folded = mul_ext(Fp2Expr::Fp2(rhs_selector, 0), compress_expression_array(rhs));
 
     // Update rule:
     // acc' = acc * (beta - lhs_folded) / (beta - rhs_folded)
     // => (beta - rhs_folded) * acc' - (beta - lhs_folded) * acc = 0
-    let update_expr = sub_ext(
-        mul_ext(sub_ext(beta, rhs_folded), next_ext(acc)),
-        mul_ext(sub_ext(beta, lhs_folded), acc)
-    );
+    let update_expr = unpack_ext(sub_ext(
+        mul_ext(sub_ext(beta, rhs_folded), next_ext(acc_ext)),
+        mul_ext(sub_ext(beta, lhs_folded), acc_ext)
+    ));
 
     [
         // First and last z needs to be 1
