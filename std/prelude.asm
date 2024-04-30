5a0bc1eba18b194a22de3e52a4b2de0af8bfd410
chriseth <chris@ethereum.org>
2024-04-30 09:49:40 +0200

    Actually add the prelude.

---
+++ std/prelude.asm
@@ -0,0 +1,9 @@
+/// Like in Rust, all symbols from this file are available in all modules,
+/// meaning that a failed symbol resolution always re-tries relative to this module.
+
+/// The Option type.
+use std::utils::Option;
+
+use std::convert::expr as to_expr;
+use std::convert::fe as to_fe;
+use std::convert::int as to_int;
