43db72e1e0b7ff24f6f43276f8f9117486d0cd03
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2024-05-06 13:11:50 -0400

    fmt

--- pipeline/tests/asm.rs
+++ pipeline/tests/asm.rs
@@ -463,7 +463,10 @@ fn keccak() {
     use std::{fs, sync::Arc};
 
     // Set up the file to test
-    let code_path = format!("{}/../test_data/asm/keccakf.asm", env!("CARGO_MANIFEST_DIR"),);
+    let code_path = format!(
+        "{}/../test_data/asm/keccakf.asm",
+        env!("CARGO_MANIFEST_DIR"),
+    );
     let code = fs::read_to_string(code_path).unwrap();
     let mut pipeline = Pipeline::<GoldilocksField>::default().from_asm_string(code, None);
     let analyzed = pipeline.compute_analyzed_pil().unwrap().clone();
