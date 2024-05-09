e2310b1b66a4c85fddee5d8d954a170f42e8b4b8
Gaston Zanitti <gzanitti@gmail.com>
2024-05-09 17:57:40 -0300

    test_utils

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -132,8 +132,8 @@ pub fn unescape_string(s: &str) -> String {
 mod test {
     use super::*;
     use powdr_ast::parsed::{
-        asm::ASMProgram, build::direct_reference, visitor::Children, PILFile, PilStatement,
-        PolynomialName, SelectedExpressions,
+        asm::ASMProgram, build::direct_reference, PILFile, PilStatement, PolynomialName,
+        SelectedExpressions,
     };
     use powdr_parser_util::UnwrapErrToStderr;
     use pretty_assertions::assert_eq;
@@ -243,56 +243,6 @@ mod test {
                 _ => None,
             }
         })
-    }
-
-    // helper function to clear SourceRef's inside the AST so we can compare for equality
-    fn pil_clear_source_refs(ast: &mut PILFile) {
-        ast.0.iter_mut().for_each(pil_statement_clear_source_ref);
-    }
-
-    fn pil_statement_clear_source_ref(stmt: &mut PilStatement) {
-        stmt.children_mut()
-            .for_each(pil_expression_clear_source_ref);
-
-        match stmt {
-            PilStatement::Include(s, _)
-            | PilStatement::Namespace(s, _, _)
-            | PilStatement::LetStatement(s, _, _, _)
-            | PilStatement::PolynomialDefinition(s, _, _)
-            | PilStatement::PublicDeclaration(s, _, _, _, _)
-            | PilStatement::PolynomialConstantDeclaration(s, _)
-            | PilStatement::PolynomialConstantDefinition(s, _, _)
-            | PilStatement::PolynomialCommitDeclaration(s, _, _, _)
-            | PilStatement::PlookupIdentity(s, _, _)
-            | PilStatement::PermutationIdentity(s, _, _)
-            | PilStatement::ConnectIdentity(s, _, _)
-            | PilStatement::ConstantDefinition(s, _, _)
-            | PilStatement::Expression(s, _)
-            | PilStatement::EnumDeclaration(s, _) => *s = SourceRef::unknown(),
-        }
-    }
-
-    fn pil_expression_clear_source_ref(expr: &mut powdr_ast::parsed::Expression) {
-        use powdr_ast::parsed::Expression::*;
-        match expr {
-            Reference(s, _)
-            | PublicReference(s, _)
-            | Number(s, _)
-            | String(s, _)
-            | Tuple(s, _)
-            | ArrayLiteral(s, _)
-            | UnaryOperation(s, _)
-            | BinaryOperation(s, _)
-            | IndexAccess(s, _)
-            | IfExpression(s, _)
-            | MatchExpression(s, _)
-            | FunctionCall(s, _)
-            | LambdaExpression(s, _)
-            | BlockExpression(s, _)
-            | FreeInput(s, _) => {
-                *s = SourceRef::unknown();
-            }
-        }
     }
 
     // helper function to clear SourceRef's inside the AST so we can compare for equality
--- parser/src/test_utils.rs
+++ parser/src/test_utils.rs
@@ -1,8 +1,12 @@
+use powdr_ast::parsed::visitor::Children;
 use powdr_ast::{
     parsed::{PILFile, PilStatement},
     SourceRef,
 };
 pub fn pil_statement_clear_source_ref(stmt: &mut PilStatement) {
+    stmt.children_mut()
+        .for_each(pil_expression_clear_source_ref);
+
     match stmt {
         PilStatement::Include(s, _)
         | PilStatement::Namespace(s, _, _)
@@ -20,6 +24,30 @@ pub fn pil_statement_clear_source_ref(stmt: &mut PilStatement) {
         | PilStatement::EnumDeclaration(s, _) => *s = SourceRef::unknown(),
     }
 }
+
+fn pil_expression_clear_source_ref(expr: &mut powdr_ast::parsed::Expression) {
+    use powdr_ast::parsed::Expression::*;
+    match expr {
+        Reference(s, _)
+        | PublicReference(s, _)
+        | Number(s, _)
+        | String(s, _)
+        | Tuple(s, _)
+        | ArrayLiteral(s, _)
+        | UnaryOperation(s, _)
+        | BinaryOperation(s, _)
+        | IndexAccess(s, _)
+        | IfExpression(s, _)
+        | MatchExpression(s, _)
+        | FunctionCall(s, _)
+        | LambdaExpression(s, _)
+        | BlockExpression(s, _)
+        | FreeInput(s, _) => {
+            *s = SourceRef::unknown();
+        }
+    }
+}
+
 // helper function to clear SourceRef's inside the AST so we can compare for equality
 pub fn pil_clear_source_refs(ast: &mut PILFile) {
     ast.0.iter_mut().for_each(pil_statement_clear_source_ref);
