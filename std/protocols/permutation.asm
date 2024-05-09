23c76a90a256d25b9089e5e27c439cae2dfa2730
Georg Wiese <georgwiese@gmail.com>
2024-05-09 13:19:03 +0200

    Polish

--- std/protocols/permutation.asm
+++ std/protocols/permutation.asm
@@ -1,7 +1,6 @@
 use std::prover::challenge;
 use std::array::fold;
 use std::array::len;
-use std::convert::int;
 use std::check::assert;
 use std::check::panic;
 use std::field::known_field;
@@ -44,13 +43,15 @@ let needs_extension: -> bool = || match known_field() {
 };
 
 // Maps [x_1, x_2, ..., x_n] to alpha**(n - 1) * x_1 + alpha ** (n - 2) * x_2 + ... + x_n
-let compress_expression_array = |expr_array, alpha| fold(
+let compress_expression_array: expr[], Fp2Expr -> Fp2Expr = |expr_array, alpha| fold(
     expr_array,
     Fp2Expr::Fp2(0, 0),
     |sum_acc, el| add_ext(mul_ext(alpha, sum_acc), Fp2Expr::Fp2(el, 0))
 );
 
 // Compute z' = z * (beta - a) / (beta - b), using extension field arithmetic
+// This is intended to be used as a hint in the extension field case; for the base case
+// automatic witgen is smart enough to figure out the value if the accumulator.
 let compute_next_z: Fp2Expr, Constr -> fe[] = query |acc, permutation_constraint| {
 
     let (lhs_selector, lhs, rhs_selector, rhs) = unpack_permutation_constraint(permutation_constraint);
@@ -67,7 +68,13 @@ let compute_next_z: Fp2Expr, Constr -> fe[] = query |acc, permutation_constraint
     let lhs_folded = mul_ext(Fp2Expr::Fp2(lhs_selector, 0), compress_expression_array(lhs, alpha));
     let rhs_folded = mul_ext(Fp2Expr::Fp2(rhs_selector, 0), compress_expression_array(rhs, alpha));
     
-    let res = eval_ext(mul_ext(mul_ext(acc, sub_ext(beta, lhs_folded)), expr_ext(inv_ext(eval_ext(sub_ext(beta, rhs_folded))))));
+    // acc' = acc * (beta - lhs_folded) / (beta - rhs_folded)
+    let res = eval_ext(
+        mul_ext(
+            mul_ext(acc, sub_ext(beta, lhs_folded)),
+            expr_ext(inv_ext(eval_ext(sub_ext(beta, rhs_folded))))
+        )
+    );
 
     match res {
         Fp2Value::Fp2(a0_fe, a1_fe) => [a0_fe, a1_fe]
@@ -76,7 +83,8 @@ let compute_next_z: Fp2Expr, Constr -> fe[] = query |acc, permutation_constraint
 
 // Adds constraints that enforce that lhs is a permutation of rhs
 // Arguments:
-// - acc: A phase-2 witness column to be used as the accumulator
+// - acc: A phase-2 witness column to be used as the accumulator. If 2 are provided, computations
+//        are done on the F_{p^2} extension field.
 // - lhs_selector: (assumed to be) binary selector to check which elements from the LHS to include
 // - lhs: An array of expressions
 // - rhs_selector: (assumed to be) binary selector to check which elements from the RHS to include
@@ -86,7 +94,11 @@ let permutation: expr[], Constr -> Constr[] = |acc, permutation_constraint| {
     let (lhs_selector, lhs, rhs_selector, rhs) = unpack_permutation_constraint(permutation_constraint);
 
     let _ = assert(len(lhs) == len(rhs), || "LHS and RHS should have equal length");
-    let with_extension = len(acc) == 2;
+    let with_extension = match len(acc) {
+        1 => false,
+        2 => true,
+        _ => panic("Expected 1 or 2 accumulator columns!")
+    };
 
     let _ = if !with_extension {
         assert(!needs_extension(), || "The Goldilocks field is too small and needs to move to the extension field. Pass two accumulators instead!")
@@ -95,15 +107,10 @@ let permutation: expr[], Constr -> Constr[] = |acc, permutation_constraint| {
     // On the extension field, we'll need two field elements to represent the challenge.
     // If we don't need an extension field, we can simply set the second component to 0,
     // in which case the operations below effectively only operate on the first component.
-    let acc_ext = if with_extension {
-        let _ = assert(len(acc) == 2, || "Expected 2 accumulators");
-        Fp2Expr::Fp2(acc[0], acc[1])
-    } else {
-        let _ = assert(len(acc) == 1, || "Expected 1 accumulators");
-        Fp2Expr::Fp2(acc[0], 0)
-    };
-    let alpha = if with_extension {Fp2Expr::Fp2(alpha1, alpha2)} else {Fp2Expr::Fp2(alpha1, 0)};
-    let beta = if with_extension {Fp2Expr::Fp2(beta1, beta2)} else {Fp2Expr::Fp2(beta1, 0)};
+    let fp2_from_array = |arr| if with_extension { Fp2Expr::Fp2(arr[0], arr[1]) } else { Fp2Expr::Fp2(arr[0], 0) };
+    let acc_ext = fp2_from_array(acc);
+    let alpha = fp2_from_array([alpha1, alpha2]);
+    let beta = fp2_from_array([beta1, beta2]);
 
     let lhs_folded = mul_ext(Fp2Expr::Fp2(lhs_selector, 0), compress_expression_array(lhs, alpha));
     let rhs_folded = mul_ext(Fp2Expr::Fp2(rhs_selector, 0), compress_expression_array(rhs, alpha));
@@ -111,6 +118,7 @@ let permutation: expr[], Constr -> Constr[] = |acc, permutation_constraint| {
     let next_acc = if with_extension {
         next_ext(acc_ext)
     } else {
+        // The second component is 0, but the next operator is not defined on it...
         Fp2Expr::Fp2(acc[0]', 0)
     };
 
@@ -128,11 +136,15 @@ let permutation: expr[], Constr -> Constr[] = |acc, permutation_constraint| {
         // First and last z needs to be 1
         // (because of wrapping, the z[0] and z[N] are the same)
         is_first * (acc_1 - 1) = 0,
+
+        // Note that if with_extension is false, this generates 0 = 0 and is removed
+        // by the optimizer.
         is_first * acc_2 = 0,
 
-        // Update rule:
-        // acc' = acc * (beta - lhs_folded) / (beta - rhs_folded)
+        // Assert that the update rule has been obeyed
         update_expr_1 = 0,
+
+        // Again, update_expr_2 will be equal to 0 in the non-extension case.
         update_expr_2 = 0
     ]
 };
