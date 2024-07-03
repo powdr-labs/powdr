efe9510557f6ef8ffe1a7108ae1fe33eff4debe9
chriseth <chris@ethereum.org>
2024-07-03 15:11:11 +0000

    Fix re-parsing generic paths.

--- pil-analyzer/tests/parse_display.rs
+++ pil-analyzer/tests/parse_display.rs
@@ -716,3 +716,15 @@ namespace T(8);
     assert_eq!(analyzed.degree, Some(8));
     assert_eq!(expected, analyzed.to_string());
 }
+
+#[test]
+fn reparse_generic_function_call() {
+    let input = r#"namespace X(16);
+    let<T: Add + FromLiteral> inc: T -> T = (|x| x + 1);
+namespace N(16);
+    let x: int = 7;
+    let y: int = X::inc::<int>(N.x);
+"#;
+    let formatted = analyze_string::<GoldilocksField>(input).to_string();
+    assert_eq!(formatted, input);
+}
