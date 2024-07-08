4dfd011c7747c0ca1c2e6c7243272b3b040add17
Gastón Zanitti <gzanitti@gmail.com>
2024-07-08 12:54:28 -0300

    Update pil-analyzer/src/type_builtins.rs
    Co-authored-by: chriseth <chris@ethereum.org>

--- pil-analyzer/src/type_builtins.rs
+++ pil-analyzer/src/type_builtins.rs
@@ -89,6 +89,7 @@ lazy_static! {
     .into_iter()
     .map(|(op, (vars, ty))| (op, parse_type_scheme(vars, ty)))
     .collect();
+
     static ref CONSTR_FUNCTION_STATEMENT_TYPE: ExpectedType = ExpectedType {
         ty: Type::NamedType(SymbolPath::from_str("std::prelude::Constr").unwrap(), None),
         allow_array: true,
