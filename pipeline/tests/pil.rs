2f4b39d83371c2197d4b66cf84023294be55497f
Georg Wiese <georgwiese@gmail.com>
2024-07-10 20:41:12 +0200

    Format

--- pipeline/tests/pil.rs
+++ pipeline/tests/pil.rs
@@ -2,7 +2,11 @@
 use powdr_number::Bn254Field;
 use powdr_number::GoldilocksField;
 use powdr_pipeline::test_util::{
-    assert_proofs_fail_for_invalid_witnesses, assert_proofs_fail_for_invalid_witnesses_estark, assert_proofs_fail_for_invalid_witnesses_halo2, assert_proofs_fail_for_invalid_witnesses_pilcom, gen_estark_proof, gen_estark_proof_with_backend_variant, test_halo2, test_halo2_with_backend_variant, test_plonky3, verify_test_file, BackendVariant
+    assert_proofs_fail_for_invalid_witnesses, assert_proofs_fail_for_invalid_witnesses_estark,
+    assert_proofs_fail_for_invalid_witnesses_halo2,
+    assert_proofs_fail_for_invalid_witnesses_pilcom, gen_estark_proof,
+    gen_estark_proof_with_backend_variant, test_halo2, test_halo2_with_backend_variant,
+    test_plonky3, verify_test_file, BackendVariant,
 };
 
 use test_log::test;
