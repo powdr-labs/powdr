210047194fe22095a9a14df3608f722a0e6d737d
Georg Wiese <georgwiese@gmail.com>
2024-07-12 23:00:57 +0200

    Undo change

--- std/machines/split/split_gl.asm
+++ std/machines/split/split_gl.asm
@@ -70,11 +70,11 @@ machine SplitGL with
     let P_B: col = b;
     col fixed P_LT(i) { if a(i) < b(i) { 1 } else { 0 } };
     col fixed P_GT(i) { if a(i) > b(i) { 1 } else { 0 } };
-    [ bytes, BYTES_MAX, lt, gt ] in [ P_A, P_B, P_LT, P_GT ];
 
     // Compare the current byte with the corresponding byte of the maximum value.
     col witness lt;
     col witness gt;
+    [ bytes, BYTES_MAX, lt, gt ] in [ P_A, P_B, P_LT, P_GT ];
 
     // Compute whether the current or any previous byte has been less than
     // the corresponding byte of the maximum value.
