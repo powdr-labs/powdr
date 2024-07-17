7f3953b4c9375fd529458511a7baaa2c170c062d
Georg Wiese <georgwiese@gmail.com>
2024-07-17 12:38:07 +0200

    Bug fix

--- pipeline/tests/pil.rs
+++ pipeline/tests/pil.rs
@@ -6,7 +6,8 @@ use powdr_pipeline::test_util::{
     assert_proofs_fail_for_invalid_witnesses_halo2,
     assert_proofs_fail_for_invalid_witnesses_pilcom, gen_estark_proof,
     gen_estark_proof_with_backend_variant, make_prepared_pipeline, run_pilcom_test_file,
-    test_halo2, test_halo2_with_backend_variant, test_plonky3, BackendVariant,
+    run_pilcom_with_backend_variant, test_halo2, test_halo2_with_backend_variant, test_plonky3,
+    BackendVariant,
 };
 
 use test_log::test;
