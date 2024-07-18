638ce06bd041a22e83c1303606135f30b41dba17
Georg Wiese <georgwiese@gmail.com>
2024-07-18 11:07:08 +0200

    Test re-parsing for std tests too

--- pipeline/build.rs
+++ pipeline/build.rs
@@ -8,35 +8,36 @@ use walkdir::WalkDir;
 fn main() {
     build_book_tests("asm");
     build_book_tests("pil");
-    build_reparse_test("asm");
-    build_reparse_test("pil");
+    build_reparse_test("asm", "asm");
+    build_reparse_test("pil", "pil");
+    build_reparse_test("asm", "std");
 }
 
 fn build_book_tests(kind: &str) {
-    build_tests(kind, "book", "book")
+    build_tests(kind, kind, "book", "book")
 }
 
-fn build_reparse_test(kind: &str) {
-    build_tests(kind, "", "reparse")
+fn build_reparse_test(kind: &str, dir: &str) {
+    build_tests(kind, dir, "", "reparse")
 }
 
 #[allow(clippy::print_stdout)]
-fn build_tests(kind: &str, sub_dir: &str, name: &str) {
+fn build_tests(kind: &str, dir: &str, sub_dir: &str, name: &str) {
     let sub_dir = if sub_dir.is_empty() {
         "".to_string()
     } else {
         format!("{sub_dir}/")
     };
     let out_dir = env::var("OUT_DIR").unwrap();
-    let destination = Path::new(&out_dir).join(format!("{kind}_{name}_tests.rs"));
+    let destination = Path::new(&out_dir).join(format!("{dir}_{name}_tests.rs"));
     let mut test_file = BufWriter::new(File::create(destination).unwrap());
 
-    let dir = format!("../test_data/{kind}/{sub_dir}");
-    for file in WalkDir::new(&dir) {
+    let full_dir = format!("../test_data/{dir}/{sub_dir}");
+    for file in WalkDir::new(&full_dir) {
         let file = file.unwrap();
         let relative_name = file
             .path()
-            .strip_prefix(&dir)
+            .strip_prefix(&full_dir)
             .unwrap()
             .to_str()
             .unwrap()
@@ -45,13 +46,13 @@ fn build_tests(kind: &str, sub_dir: &str, name: &str) {
             .replace('/', "_sub_")
             .strip_suffix(&format!(".{kind}"))
         {
-            println!("cargo:rerun-if-changed={dir}/{relative_name}");
+            println!("cargo:rerun-if-changed={full_dir}/{relative_name}");
             write!(
                 test_file,
                 r#"
 #[test]
 fn {test_name}() {{
-    run_{name}_test("{kind}/{sub_dir}{relative_name}");
+    run_{name}_test("{dir}/{sub_dir}{relative_name}");
 }}
 "#,
             )
--- pipeline/tests/powdr_std.rs
+++ pipeline/tests/powdr_std.rs
@@ -381,3 +381,32 @@ fn btree() {
     let f = "std/btree_test.asm";
     execute_test_file(f, Default::default(), vec![]).unwrap();
 }
+
+mod reparse {
+
+    use powdr_number::GoldilocksField;
+    use powdr_pipeline::{test_util::resolve_test_file, Pipeline};
+    use test_log::test;
+
+    /// Files that we don't expect to parse, analyze, and optimize without error.
+    const BLACKLIST: [&str; 1] = ["std/bus_permutation_via_challenges.asm"];
+
+    fn run_reparse_test(file: &str) {
+        if BLACKLIST.contains(&file) {
+            return;
+        }
+
+        // Compute the optimized PIL
+        let optimized_pil = Pipeline::<GoldilocksField>::default()
+            .from_asm_file(resolve_test_file(file))
+            .compute_optimized_pil()
+            .unwrap();
+        // Run the pipeline using the string serialization of the optimized PIL.
+        // This panics if the re-parsing fails.
+        Pipeline::<GoldilocksField>::default()
+            .from_pil_string(optimized_pil.to_string())
+            .compute_optimized_pil()
+            .unwrap();
+    }
+    include!(concat!(env!("OUT_DIR"), "/std_reparse_tests.rs"));
+}
