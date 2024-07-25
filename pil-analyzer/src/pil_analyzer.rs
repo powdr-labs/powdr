f1cb39f339739f015e5e609b64e56a135a2bcefa
Gaston Zanitti <gzanitti@gmail.com>
2024-07-25 12:43:24 -0300

    clippy and minor fix

--- pil-analyzer/src/pil_analyzer.rs
+++ pil-analyzer/src/pil_analyzer.rs
@@ -453,17 +453,14 @@ impl PILAnalyzer {
 
                 let types1 = impl1.type_scheme.types.clone();
                 let absolute_name = self.driver().resolve_decl(impl1.name.name());
-                if types1.iter().any(|t| matches!(t, Type::NamedType(_, None))) {
-                    panic!("Named variables are not supported in impls: {impl1}")
-                }
 
                 let trait_decl = self
                     .definitions
                     .get(&absolute_name)
-                    .unwrap_or_else(|| panic!("Trait {} not found", absolute_name))
+                    .unwrap_or_else(|| panic!("Trait {absolute_name} not found"))
                     .1
                     .as_ref()
-                    .unwrap_or_else(|| panic!("Trait definition for {} not found", absolute_name));
+                    .unwrap_or_else(|| panic!("Trait definition for {absolute_name} not found"));
 
                 let trait_decl = match trait_decl {
                     FunctionValueDefinition::TraitDeclaration(trait_decl) => trait_decl,
