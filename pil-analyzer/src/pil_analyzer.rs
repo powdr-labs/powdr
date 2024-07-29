cd69350c70f1f0bb0d132452e35158865b388513
Gaston Zanitti <gzanitti@gmail.com>
2024-07-29 09:41:04 -0300

    default

--- pil-analyzer/src/pil_analyzer.rs
+++ pil-analyzer/src/pil_analyzer.rs
@@ -159,7 +159,7 @@ impl PILAnalyzer {
                     .process_trait_implementation(trait_impl.clone());
                     self.implementations
                         .entry(ti.name.name().clone())
-                        .or_insert_with(Vec::new)
+                        .or_default()
                         .push((sr.clone(), ti))
                 }
                 self.handle_statement(statement.clone());
