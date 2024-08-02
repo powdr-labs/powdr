8bf70f62d97557378b1a8013d7024a6754b7682c
chriseth <chris@ethereum.org>
2024-08-02 14:22:12 +0000

    Compute function only once.

--- executor/src/constant_evaluator/mod.rs
+++ executor/src/constant_evaluator/mod.rs
@@ -94,15 +94,14 @@ fn generate_values<T: FieldElement>(
             } else {
                 e
             };
+            let fun = evaluator::evaluate(e, &mut symbols.clone()).unwrap();
             (0..degree)
                 .into_par_iter()
                 .map(|i| {
-                    let mut symbols = symbols.clone();
-                    let fun = evaluator::evaluate(e, &mut symbols).unwrap();
                     evaluator::evaluate_function_call(
-                        fun,
+                        fun.clone(),
                         vec![Arc::new(Value::Integer(BigInt::from(i)))],
-                        &mut symbols,
+                        &mut symbols.clone(),
                     )
                     .and_then(|v| v.try_to_field_element())
                 })
