3e81d25b87e718a327db00da7ed6fcf5951dc50c
Georg Wiese <georgwiese@gmail.com>
2024-08-06 11:38:52 +0200

    Refactor

--- executor/src/witgen/machines/fixed_lookup_machine.rs
+++ executor/src/witgen/machines/fixed_lookup_machine.rs
@@ -169,86 +169,34 @@ impl<T: FieldElement> IndexedColumns<T> {
     }
 }
 
-impl<'a, T: FieldElement> Machine<'a, T> for FixedLookup<'a, T> {
-    fn name(&self) -> &str {
-        "FixedLookup"
-    }
-
-    fn degree(&self) -> powdr_number::DegreeType {
-        // TODO: This function is never actually called by the outside (some machines
-        // are using it in their own implementation though). Also, the FixedLookup machine
-        // is weird, because it is actually responsible for lookups into many different fixed
-        // columns which might have different lengths. That should probably also change in the future.
-        panic!("The FixedLookup machine might not have a consistent degree!")
-    }
-
-    fn process_plookup<'b, Q: crate::witgen::QueryCallback<T>>(
-        &mut self,
-        _mutable_state: &'b mut crate::witgen::MutableState<'a, 'b, T, Q>,
-        identity_id: u64,
-        caller_rows: &'b RowPair<'b, 'a, T>,
-    ) -> EvalResult<'a, T> {
-        let identity = self
-            .identities
-            .iter()
-            .find(|i| i.id == identity_id)
-            .expect("identity not found");
-        let right = &identity.right;
-
-        // get the values of the fixed columns
-        let right = right
-            .expressions
-            .iter()
-            .filter_map(try_to_simple_poly_ref)
-            .peekable();
-
-        let outer_query = OuterQuery::new(caller_rows, identity);
-        self.process_plookup_internal(caller_rows, &outer_query.left, right)
-    }
-
-    fn take_witness_col_values<'b, Q: QueryCallback<T>>(
-        &mut self,
-        _mutable_state: &'b mut MutableState<'a, 'b, T, Q>,
-    ) -> HashMap<String, Vec<T>> {
-        Default::default()
-    }
-
-    fn identity_ids(&self) -> Vec<u64> {
-        // TODO: This should be computed only once
-        self.identities
-            .iter()
-            .filter_map(|i| {
-                if i.kind != IdentityKind::Plookup
-                    || i.right.selector.is_some()
-                    || i.right.expressions.iter().any(|e| e.contains_witness_ref())
-                    || i.right.expressions.is_empty()
-                {
-                    return None;
-                }
-                Some(i.id)
-            })
-            .collect()
-    }
-}
-
 /// Machine to perform a lookup in fixed columns only.
 pub struct FixedLookup<'a, T: FieldElement> {
     global_constraints: GlobalConstraints<T>,
     indices: IndexedColumns<T>,
-    identities: Vec<&'a Identity<T>>,
+    connecting_identities: BTreeMap<u64, &'a Identity<T>>,
     fixed_data: &'a FixedData<'a, T>,
 }
 
 impl<'a, T: FieldElement> FixedLookup<'a, T> {
     pub fn new(
         global_constraints: GlobalConstraints<T>,
-        identities: Vec<&'a Identity<T>>,
+        all_identities: Vec<&'a Identity<T>>,
         fixed_data: &'a FixedData<'a, T>,
     ) -> Self {
+        let connecting_identities = all_identities
+            .into_iter()
+            .filter_map(|i| {
+                (i.kind == IdentityKind::Plookup
+                    && i.right.selector.is_none()
+                    && !i.right.expressions.iter().any(|e| e.contains_witness_ref())
+                    && !i.right.expressions.is_empty())
+                .then_some((i.id, i))
+            })
+            .collect();
         Self {
             global_constraints,
             indices: Default::default(),
-            identities,
+            connecting_identities,
             fixed_data,
         }
     }
@@ -361,6 +309,51 @@ impl<'a, T: FieldElement> FixedLookup<'a, T> {
                 .collect(),
             IncompleteCause::NotConcrete,
         ))
+    }
+}
+
+impl<'a, T: FieldElement> Machine<'a, T> for FixedLookup<'a, T> {
+    fn name(&self) -> &str {
+        "FixedLookup"
+    }
+
+    fn degree(&self) -> powdr_number::DegreeType {
+        // TODO: This function is never actually called by the outside (some machines
+        // are using it in their own implementation though). Also, the FixedLookup machine
+        // is weird, because it is actually responsible for lookups into many different fixed
+        // columns which might have different lengths. That should probably also change in the future.
+        panic!("The FixedLookup machine might not have a consistent degree!")
+    }
+
+    fn process_plookup<'b, Q: crate::witgen::QueryCallback<T>>(
+        &mut self,
+        _mutable_state: &'b mut crate::witgen::MutableState<'a, 'b, T, Q>,
+        identity_id: u64,
+        caller_rows: &'b RowPair<'b, 'a, T>,
+    ) -> EvalResult<'a, T> {
+        let identity = self.connecting_identities[&identity_id];
+        let right = &identity.right;
+
+        // get the values of the fixed columns
+        let right = right
+            .expressions
+            .iter()
+            .filter_map(try_to_simple_poly_ref)
+            .peekable();
+
+        let outer_query = OuterQuery::new(caller_rows, identity);
+        self.process_plookup_internal(caller_rows, &outer_query.left, right)
+    }
+
+    fn take_witness_col_values<'b, Q: QueryCallback<T>>(
+        &mut self,
+        _mutable_state: &'b mut MutableState<'a, 'b, T, Q>,
+    ) -> HashMap<String, Vec<T>> {
+        Default::default()
+    }
+
+    fn identity_ids(&self) -> Vec<u64> {
+        self.connecting_identities.keys().copied().collect()
     }
 }
 
