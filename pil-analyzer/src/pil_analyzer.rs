296117110905e43b63e8f742e5e1ff9da2274035
Gastón Zanitti <gzanitti@gmail.com>
2024-08-08 09:45:55 -0300

    Update pil-analyzer/src/pil_analyzer.rs
    Co-authored-by: chriseth <chris@ethereum.org>

--- pil-analyzer/src/pil_analyzer.rs
+++ pil-analyzer/src/pil_analyzer.rs
@@ -446,7 +446,7 @@ impl PILAnalyzer {
 }
 
 #[derive(Clone, Copy)]
-pub struct Driver<'a>(&'a PILAnalyzer);
+struct Driver<'a>(&'a PILAnalyzer);
 
 impl<'a> AnalysisDriver for Driver<'a> {
     fn resolve_namespaced_decl(&self, path: &[&String]) -> AbsoluteSymbolPath {
