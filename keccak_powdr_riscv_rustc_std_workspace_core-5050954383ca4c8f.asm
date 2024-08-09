4e5a82fcf78f370d008a6f78dd962b8164be2e9a
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2024-08-09 11:27:14 +0800

    deleted unwanted files

--- keccak_powdr_riscv_rustc_std_workspace_core-5050954383ca4c8f.asm
+++
@@ -1,7 +0,0 @@
-	.text
-	.attribute	4, 16
-	.attribute	5, "rv32i2p1_m2p0_a2p1_c2p0"
-	.file	"rustc_std_workspace_core.ce124ed232b9df04-cgu.0"
-	.section	.debug_aranges,"",@progbits
-	.ident	"rustc version 1.77.0-nightly (11f32b73e 2024-01-31)"
-	.section	".note.GNU-stack","",@progbits
