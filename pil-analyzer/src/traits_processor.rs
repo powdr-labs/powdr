d793c2ed1f32796dcde03d8e910202c5f9bc0d72
Gaston Zanitti <gzanitti@gmail.com>
2024-08-12 10:53:33 -0300

    Identities in TraitProcessor

--- pil-analyzer/src/traits_processor.rs
+++ pil-analyzer/src/traits_processor.rs
@@ -30,7 +30,7 @@ impl<'a> TraitsProcessor<'a> {
     }
 
     pub fn traits_resolution(&mut self) {
-        let keys = self
+        let keys: Vec<String> = self
             .definitions
             .iter()
             .filter(|(_, (_, def))| {
@@ -40,56 +40,128 @@ impl<'a> TraitsProcessor<'a> {
                 )
             })
             .map(|(name, _)| name.clone())
-            .collect::<Vec<_>>();
+            .collect();
 
         for name in keys {
             self.resolve_trait(&name);
         }
 
-        let refs_in_identity: Vec<(String, Vec<Type>)> = self
-            .identities
-            .iter()
-            .flat_map(|id| {
-                let Identity {
-                    left:
-                        SelectedExpressions {
-                            selector: selector_left,
-                            expressions: expressions_left,
-                        },
-                    right:
-                        SelectedExpressions {
-                            selector: selector_right,
-                            expressions: expressions_right,
-                        },
-                    ..
-                } = id;
+        self.resolve_trait_identities();
+    }
 
-                selector_left
-                    .iter()
-                    .chain(std::iter::once(expressions_left.as_ref()))
-                    .chain(selector_right.iter())
-                    .chain(std::iter::once(expressions_right.as_ref()))
-                    .flat_map(move |e| {
-                        e.all_children().filter_map(move |e| match e {
-                            Expression::Reference(
-                                _,
-                                Reference::Poly(PolynomialReference {
-                                    name,
-                                    type_args: Some(types),
-                                    ..
-                                }),
-                            ) => Some((name.clone(), types.clone())),
-                            _ => None,
-                        })
-                    })
-                    .collect::<Vec<_>>()
+    fn resolve_trait_identities(&mut self) {
+        let mut updates = Vec::new();
+        for (index, identity) in self.identities.iter().enumerate() {
+            let refs_in_identity = self.collect_refs_in_identity(identity);
+            for collected_ref in refs_in_identity {
+                let (trait_name, resolved_impl_pos) =
+                    self.resolve_trait_function(collected_ref.clone());
+                updates.push((index, trait_name, collected_ref.1, resolved_impl_pos));
+            }
+        }
+
+        for (index, trait_name, type_args, resolved_impl_pos) in updates {
+            let identity = &mut self.identities[index];
+            TraitsProcessor::update_references_in_identity(
+                identity,
+                &trait_name,
+                &type_args,
+                &resolved_impl_pos,
+            );
+        }
+    }
+
+    fn collect_refs_in_identity(
+        &self,
+        identity: &Identity<SelectedExpressions<Expression>>,
+    ) -> Vec<(String, Vec<Type>)> {
+        let Identity {
+            left:
+                SelectedExpressions {
+                    selector: selector_left,
+                    expressions: expressions_left,
+                },
+            right:
+                SelectedExpressions {
+                    selector: selector_right,
+                    expressions: expressions_right,
+                },
+            ..
+        } = identity;
+
+        selector_left
+            .iter()
+            .chain(std::iter::once(expressions_left.as_ref()))
+            .chain(selector_right.iter())
+            .chain(std::iter::once(expressions_right.as_ref()))
+            .flat_map(move |e| {
+                e.all_children().filter_map(move |e| match e {
+                    Expression::Reference(
+                        _,
+                        Reference::Poly(PolynomialReference {
+                            name,
+                            type_args: Some(types),
+                            ..
+                        }),
+                    ) => Some((name.clone(), types.clone())),
+                    _ => None,
+                })
             })
-            .collect();
+            .collect()
+    }
+
+    fn update_references_in_identity(
+        identity: &mut Identity<SelectedExpressions<Expression>>,
+        trait_name: &str,
+        type_args: &[Type],
+        resolved_impl_pos: &HashMap<String, Box<Expression>>,
+    ) {
+        let Identity {
+            left:
+                SelectedExpressions {
+                    selector: selector_left,
+                    expressions: expressions_left,
+                },
+            right:
+                SelectedExpressions {
+                    selector: selector_right,
+                    expressions: expressions_right,
+                },
+            ..
+        } = identity;
+
+        let to_update = selector_left
+            .iter_mut()
+            .chain(std::iter::once(expressions_left.as_mut()))
+            .chain(selector_right.iter_mut())
+            .chain(std::iter::once(expressions_right.as_mut()));
+
+        for expr in to_update {
+            update_reference(trait_name, type_args, expr, resolved_impl_pos);
+        }
     }
 
     fn resolve_trait(&mut self, current: &str) {
         let current_def = &self.definitions.get(current).unwrap().1;
-        let refs_in_def = match current_def {
+        let refs_in_def = self.collect_refs_in_def(current_def);
+
+        for collected_ref in refs_in_def {
+            let (trait_name, resolved_impl_pos) =
+                self.resolve_trait_function(collected_ref.clone());
+
+            if let Some(FunctionValueDefinition::Expression(TypedExpression { e: expr, .. })) =
+                self.definitions.get_mut(current).unwrap().1.as_mut()
+            {
+                update_reference(&trait_name, &collected_ref.1, expr, &resolved_impl_pos);
+            }
+        }
+    }
+
+    fn collect_refs_in_def(
+        &self,
+        current_def: &Option<FunctionValueDefinition>,
+    ) -> Vec<(String, Vec<Type>)> {
+        match current_def {
             Some(FunctionValueDefinition::Expression(TypedExpression { e, .. })) => e
                 .all_children()
                 .filter_map(|e| match e {
@@ -105,38 +177,18 @@ impl<'a> TraitsProcessor<'a> {
                 })
                 .collect(),
             _ => vec![],
-        };
-
-        for collected_ref in refs_in_def {
-            let (trait_name, resolved_impl_pos) =
-                self.resolve_trait_function(collected_ref.clone());
-
-            let to_update = &mut self
-                .definitions
-                .get_mut(current)
-                .unwrap()
-                .1
-                .as_mut()
-                .unwrap();
-
-            if let FunctionValueDefinition::Expression(TypedExpression { e: expr, .. }) = to_update
-            {
-                update_reference(&trait_name, &collected_ref.1, expr, &resolved_impl_pos);
-            } else {
-                unreachable!();
-            }
         }
     }
 
     fn resolve_trait_function(
-        &mut self,
+        &self,
         collected_ref: (String, Vec<Type>),
     ) -> (String, HashMap<String, Box<Expression>>) {
         let mut resolved_impl_pos = HashMap::new();
-        let Some(FunctionValueDefinition::TraitFunction(ref trait_decl, ref mut trait_fn)) =
-            self.definitions.get_mut(&collected_ref.0).unwrap().1
+        let Some(FunctionValueDefinition::TraitFunction(ref trait_decl, ref trait_fn)) =
+            self.definitions.get(&collected_ref.0).unwrap().1
         else {
-            return ("".to_string(), resolved_impl_pos); //TODO GZ: Error
+            return ("".to_string(), resolved_impl_pos);
         };
 
         if let Some(impls) = self.implementations.get(&trait_decl.name) {
@@ -156,7 +208,7 @@ impl<'a> TraitsProcessor<'a> {
         }
 
         (
-            format!("{}::{}", trait_decl.name.replace('.', "::"), trait_fn.name), // TODO GZ: we probably have a better way to do this
+            format!("{}::{}", trait_decl.name.replace('.', "::"), trait_fn.name),
             resolved_impl_pos,
         )
     }
@@ -164,13 +216,13 @@ impl<'a> TraitsProcessor<'a> {
 
 fn update_reference(
     ref_name: &str,
-    type_args: &Vec<Type>,
+    type_args: &[Type],
     expr: &mut Expression,
     resolved_impl_pos: &HashMap<String, Box<Expression>>,
 ) {
     fn process_expr(
         ref_name: &str,
-        type_args: &Vec<Type>,
+        type_args: &[Type],
         c: &mut Expression,
         resolved_impl_pos: &HashMap<String, Box<Expression>>,
     ) {
@@ -179,7 +231,7 @@ fn update_reference(
             Reference::Poly(PolynomialReference {
                 name,
                 poly_id,
-                type_args,
+                type_args: current_type_args,
                 ..
             }),
         ) = c
@@ -189,7 +241,7 @@ fn update_reference(
                     sr.clone(),
                     Reference::Poly(PolynomialReference {
                         name: name.clone(),
-                        type_args: type_args.clone(),
+                        type_args: current_type_args.clone(),
                         poly_id: poly_id.clone(),
                         resolved_impls: resolved_impl_pos.clone(),
                     }),
@@ -202,7 +254,5 @@ fn update_reference(
         }
     }
 
-    for c in expr.children_mut() {
-        process_expr(ref_name, type_args, c, resolved_impl_pos);
-    }
+    process_expr(ref_name, type_args, expr, resolved_impl_pos);
 }
