e310a7e31eb1a49b7921b4563dc3bf44993db859
topanisto <ankaa@mit.edu>
2024-08-16 13:27:21 +0200

    associated types for FieldElementMap

--- plonky3/src/baby_bear.rs
+++ plonky3/src/baby_bear.rs
@@ -19,65 +19,46 @@ use rand::{distributions::Standard, Rng, SeedableRng};
 
 use powdr_number::{BabyBearField, FieldElement};
 
-const DEGREE: usize = 2;
-type Challenge<T: FieldElementMap> = BinomialExtensionField<T::P3Field, DEGREE>;
+pub trait Poseidon2Compatible {
+    const D: u64;
+    const ROUNDS_F: usize;
+    const ROUNDS_P: usize;
+    const PERM_WIDTH: usize;
+    const RNG_SEED: u64 = 42;
 
-const WIDTH: usize = 16;
-const D: u64 = 7;
-type Perm = Poseidon2<BabyBear, Poseidon2ExternalMatrixGeneral, DiffusionMatrixBabyBear, WIDTH, D>;
+    type Poseidon2Perm: Poseidon2<
+        BabyBear,
+        Poseidon2ExternalMatrixGeneral,
+        DiffusionMatrixBabyBear,
+        WIDTH,
+        D,
+    >;
+}
 
-// const ROUNDS_F: usize = poseidon2_round_numbers_128::<dyn BabyBear>(WIDTH, D).0;
-// const ROUNDS_P: usize = poseidon2_round_numbers_128::<dyn BabyBear>(WIDTH, D).1;
+impl Poseidon2Compatible for BabyBearField {
+    const D: u64 = 7;
+    // params directly taken from plonky3's poseidon2_round_numbers_128 function
+    // to guarentee 128-bit security.
+    const ROUNDS_F: usize = 8;
+    const ROUNDS_P: usize = 13;
+    const PERM_WIDTH: usize = 16;
 
-// params directly taken from plonky3's poseidon2_round_numbers_128 function
-// to guarentee 128-bit security.
-
-const ROUNDS_F: usize = 8;
-const ROUNDS_P: usize = 13;
-
-const RATE: usize = 8;
-const OUT: usize = 8;
-type Hash<T: FieldElementMap> = PaddingFreeSponge<Perm, WIDTH, RATE, OUT>;
-
-const N: usize = 2;
-const CHUNK: usize = 8;
-type Compress<T: FieldElementMap> = TruncatedPermutation<Perm, N, CHUNK, WIDTH>;
-
-const DIGEST_ELEMS: usize = 8;
-type ValMmcs<T: FieldElementMap> = FieldMerkleTreeMmcs<
-    <T::P3Field as Field>::Packing,
-    <T::P3Field as Field>::Packing,
-    Hash<T>,
-    Compress<T>,
-    DIGEST_ELEMS,
->;
-
-pub type Val = BabyBear;
-
-pub type Challenger<T: FieldElementMap> = DuplexChallenger<T::P3Field, Perm, WIDTH, RATE>;
-type ChallengeMmcs<T: FieldElementMap> = ExtensionMmcs<T::P3Field, Challenge<T>, ValMmcs<T>>;
-type Dft = Radix2DitParallel;
-type MyPcs<T: FieldElementMap> = TwoAdicFriPcs<T::P3Field, Dft, ValMmcs<T>, ChallengeMmcs<T>>;
-pub type Config<T: FieldElementMap> = StarkConfig<MyPcs<T>, Challenge<T>, Challenger<T>>;
-
-const FRI_LOG_BLOWUP: usize = 1;
-const FRI_NUM_QUERIES: usize = 100;
-const FRI_PROOF_OF_WORK_BITS: usize = 16;
-
-const RNG_SEED: u64 = 42;
+    type Poseidon2Perm =
+        Poseidon2<BabyBear, Poseidon2ExternalMatrixGeneral, DiffusionMatrixBabyBear, WIDTH, D>;
+}
 
 lazy_static! {
-    static ref PERM_BB: Perm = Perm::new(
-        ROUNDS_F,
-        rand_chacha::ChaCha8Rng::seed_from_u64(RNG_SEED)
+    static ref PERM_BB: Poseidon2Compatible::Perm = BabyBearField::Poseidon2Perm::new(
+        BabyBearField::ROUNDS_F,
+        rand_chacha::ChaCha8Rng::seed_from_u64(BabyBearField::RNG_SEED)
             .sample_iter(Standard)
-            .take(ROUNDS_F)
-            .collect::<Vec<[BabyBear; WIDTH]>>(),
+            .take(BabyBearField::ROUNDS_F)
+            .collect::<Vec<[BabyBear; BabyBearField::PERM_WIDTH]>>(),
         Poseidon2ExternalMatrixGeneral,
-        ROUNDS_P,
-        rand_chacha::ChaCha8Rng::seed_from_u64(RNG_SEED)
+        BabyBearField::ROUNDS_P,
+        rand_chacha::ChaCha8Rng::seed_from_u64(BabyBearField::RNG_SEED)
             .sample_iter(Standard)
-            .take(ROUNDS_P)
+            .take(BabyBearField::ROUNDS_P)
             .collect(),
         DiffusionMatrixBabyBear::default()
     );
@@ -87,35 +68,44 @@ impl FieldElementMap for BabyBearField {
     type P3Field = BabyBear;
     type MdsMatrix = MdsMatrixBabyBear;
 
+    const DEGREE: usize = 2;
+    const WIDTH: usize = BabyBearField::PERM_WIDTH;
+    const RATE: usize = 8;
+    const OUT: usize = 8;
+    const N: usize = 2;
+    const CHUNK: usize = 8;
+    const DIGEST_ELEMS: usize = 8;
+
+    type Perm = BabyBearField::Poseidon2Perm;
+
     fn to_p3_field(&self) -> Self::P3Field {
         BabyBear::from_canonical_u32(self.to_integer().try_into_u32().unwrap())
     }
-}
 
-pub(crate) fn get_challenger_baby_bear<T: FieldElementMap>() -> Challenger<T> {
-    Challenger::new(PERM_BB.clone())
-}
+    fn get_challenger() -> Challenger {
+        Challenger::new(PERM_BB.clone())
+    }
 
-pub(crate) fn get_config_baby_bear<T: FieldElementMap>(
-) -> StarkConfig<MyPcs<T>, Challenge<T>, Challenger<T>> {
-    let hash = Hash::<T>::new(PERM_BB.clone());
+    fn get_config() -> Config {
+        let hash = Hash::new(PERM_BB.clone());
 
-    let compress = Compress::<T>::new(PERM_BB.clone());
+        let compress = Compress::new(PERM_BB.clone());
 
-    let val_mmcs = ValMmcs::<T>::new(hash, compress);
+        let val_mmcs = ValMmcs::new(hash, compress);
 
-    let challenge_mmcs = ChallengeMmcs::<T>::new(val_mmcs.clone());
+        let challenge_mmcs = ChallengeMmcs::new(val_mmcs.clone());
 
-    let dft = Dft {};
+        let dft = Dft {};
 
-    let fri_config = FriConfig {
-        log_blowup: FRI_LOG_BLOWUP,
-        num_queries: FRI_NUM_QUERIES,
-        proof_of_work_bits: FRI_PROOF_OF_WORK_BITS,
-        mmcs: challenge_mmcs,
-    };
+        let fri_config = FriConfig {
+            log_blowup: FRI_LOG_BLOWUP,
+            num_queries: FRI_NUM_QUERIES,
+            proof_of_work_bits: FRI_PROOF_OF_WORK_BITS,
+            mmcs: challenge_mmcs,
+        };
 
-    let pcs = MyPcs::<T>::new(dft, val_mmcs, fri_config);
+        let pcs = MyPcs::new(dft, val_mmcs, fri_config);
 
-    Config::new(pcs)
+        Config::new(pcs)
+    }
 }
--- plonky3/src/params.rs
+++ plonky3/src/params.rs
@@ -1,32 +1,43 @@
 // use lazy_static::lazy_static;
 
-// use p3_baby_bear::MdsMatrixBabyBear;
-// use p3_challenger::DuplexChallenger;
-// use p3_commit::ExtensionMmcs;
-// use p3_dft::Radix2DitParallel;
-use p3_field::PrimeField;
-// use p3_fri::{FriConfig, TwoAdicFriPcs};
-// use p3_goldilocks::{Goldilocks, MdsMatrixGoldilocks};
-// use p3_merkle_tree::FieldMerkleTreeMmcs;
-// use p3_poseidon::Poseidon;
-// use p3_symmetric::{PaddingFreeSponge, TruncatedPermutation};
-// use p3_uni_stark::StarkConfig;
+use p3_challenger::DuplexChallenger;
+use p3_commit::ExtensionMmcs;
+use p3_dft::Radix2DitParallel;
+use p3_field::{extension::BinomialExtensionField, AbstractField, PrimeField};
+use p3_fri::{FriConfig, TwoAdicFriPcs};
+use p3_symmetric::{compression::TruncatedPermutation, CryptographicPermutation, Permutation};
+use p3_uni_stark::StarkConfig;
 
-// use rand::{distributions::Standard, Rng, SeedableRng};
-
-// use crate::data_traits::FieldElementMap;
-// use powdr_number::{BabyBearField, GoldilocksField};
+type Dft = Radix2DitParallel;
 
 pub trait FieldElementMap: Clone + Send + Sync {
     type P3Field: PrimeField;
     type MdsMatrix;
+    type Perm: Permutation<[AbstractField; WIDTH]>
+        + CryptographicPermutation<[AbstractField; WIDTH]>;
 
-    fn to_p3_field(&self) -> Self::P3Field;
+    type Hash = PaddingFreeSponge<Perm, WIDTH, RATE, OUT>;
+    type Compress = TruncatedPermutation<Perm, N, CHUNK, WIDTH>;
+    type Challenge = BinomialExtensionField<P3Field, DEGREE>;
+    type Challenger = DuplexChallenger<P3Field, Perm>;
+    type ChallengeMmcs = ExtensionMmcs<P3Field, Challenge, ValMmcs>;
+    type MyPcs = TwoAdicFriPcs<P3Field, Dft, ValMmcs, ChallengeMmcs>;
+    type Config = StarkConfig<MyPcs, Challenge, Challenger>;
+
+    const DEGREE: usize;
+    const WIDTH: usize;
+    const RATE: usize;
+    const OUT: usize;
+    const N: usize;
+    const CHUNK: usize;
+    const DIGEST_ELEMS: usize;
+    const FRI_LOG_BLOWUP: usize = 1;
+    const FRI_NUM_QUERIES: usize = 100;
+    const FRI_PROOF_OF_WORK_BITS: usize = 16;
+
+    pub(crate) fn to_p3_field(&self) -> Self::P3Field;
+
+    pub(crate) fn get_challenger() -> Challenger;
+
+    pub(crate) fn get_config() -> Config;
 }
-// pub trait Permutation {
-//     fn get_challenger<T: FieldElementMap>() -> Challenger<T>;
-// }
-
-// pub trait Configure {
-//     pub fn get_config<T: FieldElementMap>() -> StarkConfig<MyPcs<T>, Challenge<T>, Challenger<T>>;
-// }
