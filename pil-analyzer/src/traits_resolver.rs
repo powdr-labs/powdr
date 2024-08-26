b596989647491229f8c0accf121a5006936ae164
Gaston Zanitti <gzanitti@gmail.com>
2024-08-26 15:53:19 -0300

    all_children_mut removed

--- pil-analyzer/src/traits_resolver.rs
+++ pil-analyzer/src/traits_resolver.rs
@@ -1,14 +1,11 @@
-use std::{
-    collections::{BTreeMap, HashMap},
-    iter::once,
-};
+use std::collections::{BTreeMap, HashMap};
 
 use powdr_ast::{
     analyzed::{
         Expression, FunctionValueDefinition, Identity, PolynomialReference, Reference, Symbol,
         TypedExpression,
     },
-    parsed::{types::Type, visitor::AllChildren, SelectedExpressions, TraitImplementation},
+    parsed::{types::Type, visitor::ExpressionVisitable, SelectedExpressions, TraitImplementation},
 };
 
 pub struct TraitsResolver<'a> {
@@ -31,87 +28,66 @@ impl<'a> TraitsResolver<'a> {
         &'a mut self,
         trait_impls: &HashMap<String, Vec<TraitImplementation<Expression>>>,
     ) {
-        let references_to_update = self.collect_references_to_update();
-
-        for reference in references_to_update {
-            let PolynomialReference {
-                name, type_args, ..
-            } = reference.clone();
-
-            let types = type_args.unwrap();
-
-            Self::resolve_and_update_reference(reference, types, trait_impls, &name);
+        for (_, (_, def)) in self.definitions.iter_mut() {
+            if let Some(FunctionValueDefinition::Expression(TypedExpression { e: expr, .. })) = def
+            {
+                expr.pre_visit_expressions_mut(&mut |e| {
+                    if let Expression::Reference(
+                        _,
+                        Reference::Poly(
+                            reference @ PolynomialReference {
+                                type_args: Some(_), ..
+                            },
+                        ),
+                    ) = e
+                    {
+                        Self::resolve_and_update_reference(reference, trait_impls);
+                    }
+                });
+            }
         }
-    }
 
-    fn collect_references_to_update(
-        &'a mut self,
-    ) -> impl Iterator<Item = &mut PolynomialReference> {
-        self.definitions
-            .iter_mut()
-            .flat_map(|(_, (_, def))| match def {
-                Some(FunctionValueDefinition::Expression(TypedExpression { e: expr, .. })) => expr
-                    .all_children_mut()
-                    .filter_map(|e| match e {
-                        Expression::Reference(
-                            _,
-                            Reference::Poly(
-                                poly @ PolynomialReference {
-                                    type_args: Some(_), ..
-                                },
-                            ),
-                        ) => Some(poly),
-                        _ => None,
-                    })
-                    .collect::<Vec<_>>(),
-                _ => Vec::new(),
-            })
-            .chain(self.identities.iter_mut().flat_map(|identity| {
-                identity
-                    .left
-                    .selector
-                    .iter_mut()
-                    .chain(once(identity.left.expressions.as_mut()))
-                    .chain(identity.right.selector.iter_mut())
-                    .chain(once(identity.right.expressions.as_mut()))
-                    .flat_map(|expr| {
-                        expr.all_children_mut()
-                            .filter_map(|e| match e {
-                                Expression::Reference(
-                                    _,
-                                    Reference::Poly(
-                                        poly @ PolynomialReference {
-                                            type_args: Some(_), ..
-                                        },
-                                    ),
-                                ) => Some(poly),
-                                _ => None,
-                            })
-                            .collect::<Vec<_>>()
-                    })
-            }))
-            .into_iter()
+        for identity in self.identities.iter_mut() {
+            let visit = |expr: &mut Expression| {
+                expr.pre_visit_expressions_mut(&mut |e| {
+                    if let Expression::Reference(
+                        _,
+                        Reference::Poly(
+                            reference @ PolynomialReference {
+                                type_args: Some(_), ..
+                            },
+                        ),
+                    ) = e
+                    {
+                        Self::resolve_and_update_reference(reference, trait_impls);
+                    }
+                });
+            };
+            identity.left.selector.iter_mut().for_each(visit);
+            visit(identity.left.expressions.as_mut());
+            identity.right.selector.iter_mut().for_each(visit);
+            visit(identity.right.expressions.as_mut());
+        }
     }
 
     fn resolve_and_update_reference(
         reference: &mut PolynomialReference,
-        types: Vec<Type>,
         trait_impls: &HashMap<String, Vec<TraitImplementation<Expression>>>,
-        name: &str,
     ) {
         let mut resolved_impl_pos: BTreeMap<Vec<Type>, usize> = BTreeMap::new();
 
+        let name = reference.name.clone();
         let (trait_decl_name, trait_fn_name) = name.rsplit_once("::").unwrap();
         if let Some(impls) = trait_impls.get(trait_decl_name) {
             for (index, impl_) in impls.iter().enumerate() {
                 if impl_.function_by_name(trait_fn_name).is_some() {
-                    resolved_impl_pos.insert(types.to_vec(), index);
+                    resolved_impl_pos.insert(reference.type_args.as_ref().unwrap().to_vec(), index);
                 }
             }
         }
 
         let full_name = format!("{trait_decl_name}::{trait_fn_name}");
-        Self::update_reference(&full_name, reference, &resolved_impl_pos, name);
+        Self::update_reference(&full_name, reference, &resolved_impl_pos, &name);
     }
 
     fn update_reference(
