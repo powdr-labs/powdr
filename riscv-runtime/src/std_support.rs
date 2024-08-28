c61c4ef6efe1c7d49e013212f9099d1ced2c0dff
Lucas Clemente Vella <lvella@powdrlabs.com>
2024-08-28 20:51:26 +0100

    Improving comment.

--- riscv-runtime/src/std_support.rs
+++ riscv-runtime/src/std_support.rs
@@ -31,8 +31,12 @@ fn powdr_getrandom(buf: &mut [u8]) -> Result<(), Error> {
 }
 register_custom_getrandom!(powdr_getrandom);
 
-/// This is a placeholder to pretend to provide a random number generator,
-/// for places like the hash function of HashMap who needs something.
+/// This is a placeholder to pretend to provide a random number generator, for
+/// places like the hash function of HashMap who needs something.
+///
+/// This could be improved by using the rand_chacha crate, but I think it is
+/// worse, as it will just mask the fact that we are not providing a real
+/// entropy source.
 ///
 /// TODO: figure how to be truly random
 fn powdr_getrandom_impl(s: &mut [u8]) {
