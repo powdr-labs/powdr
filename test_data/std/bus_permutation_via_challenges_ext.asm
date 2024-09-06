6a2c2af6dc85c92fa903ccf4b61dce839d36ce4e
chriseth <chris@ethereum.org>
2024-09-06 14:23:35 +0000

    funs

--- test_data/std/bus_permutation_via_challenges_ext.asm
+++ test_data/std/bus_permutation_via_challenges_ext.asm
@@ -42,16 +42,25 @@ machine Main with degree: 8 {
     let is_first: col = std::well_known::is_first;
     permutation(is_first, 1, [z1,z2], [u1, u2], alpha, beta, permutation_constraint);
 
-    let hint_send = query |i| Query::Hint(compute_next_z_send_permutation(is_first, 1, z, alpha, beta, permutation_constraint)[i]);
-    col witness stage(1) z1_next(i) query hint_send(0);
-    col witness stage(1) z2_next(i) query hint_send(1);
-
+    col witness stage(1) z1_next;
+    col witness stage(1) z2_next;
+    query |i| {
+        // TODO can we move this into the `permutation` function?
+        let hint_send = compute_next_z_send_permutation(is_first, 1, z, alpha, beta, permutation_constraint);
+        std::prover::provide_value(z1_next, i, hint_send[0]);
+        std::prover::provide_value(z2_next, i, hint_send[1]);
+    };
+     
     z1' = z1_next;
     z2' = z2_next;
 
-    let hint_receive = query |i| Query::Hint(compute_next_z_receive_permutation(is_first, 1, u, alpha, beta, permutation_constraint)[i]);
-    col witness stage(1) u1_next(i) query hint_receive(0);
-    col witness stage(1) u2_next(i) query hint_receive(1);
+    col witness stage(1) u1_next;
+    col witness stage(1) u2_next;
+    query |i| {
+        let hint_receive = compute_next_z_receive_permutation(is_first, 1, z, alpha, beta, permutation_constraint);
+        std::prover::provide_value(u1_next, i, hint_receive[0]);
+        std::prover::provide_value(u2_next, i, hint_receive[1]);
+    };
 
     u1' = u1_next;
     u2' = u2_next;
