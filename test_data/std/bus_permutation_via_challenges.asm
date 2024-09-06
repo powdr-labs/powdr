70f4fc6053c43cbfb9752bd737caf06f5418a1ff
chriseth <chris@ethereum.org>
2024-09-06 17:28:32 +0000

    more prover functions.

--- test_data/std/bus_permutation_via_challenges.asm
+++ test_data/std/bus_permutation_via_challenges.asm
@@ -12,10 +12,13 @@ machine Main with degree: 8 {
 
     // Two pairs of witness columns, claimed to be permutations of one another
     // (when selected by first_four and (1 - first_four), respectively)
-    col witness a1(i) query Query::Hint(fe(i));
-    col witness a2(i) query Query::Hint(fe(i + 42));
-    col witness b1(i) query Query::Hint(fe(7 - i));
-    col witness b2(i) query Query::Hint(fe(7 - i + 42));
+    col witness a1, a2, b1, b2;
+    query |i| {
+        std::prover::provide_value(a1, i, fe(i));
+        std::prover::provide_value(a2, i, fe(i + 42));
+        std::prover::provide_value(b1, i, fe(7 - i));
+        std::prover::provide_value(b2, i, fe(7 - i + 42));
+    };
 
     let permutation_constraint = Constr::Permutation(
         (Option::Some(first_four), Option::Some(1 - first_four)),
