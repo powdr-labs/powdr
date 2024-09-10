555806e0ac4be140f570d1ce28432eb09b70dcb6
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2024-09-10 15:09:15 +0200

    adapted from arith-bb but do mul and div for 32 bit numbers only instead of 256

--- std/machines/arith_bb.asm
+++ std/machines/arith_bb.asm
@@ -14,7 +14,7 @@ use std::machines::range::Byte;
 // Arithmetic machine, ported mainly from Polygon: https://github.com/0xPolygonHermez/zkevm-proverjs/blob/main/pil/arith.pil
 // Currently only supports "Equation 0", i.e., 256-Bit addition and multiplication.
 machine Arith16 with
-    latch: CLK64_63,
+    latch: CLK8_7,
     operation_id: operation_id,
     // Allow this machine to be connected via a permutation
     call_selectors: sel,
@@ -24,12 +24,62 @@ machine Arith16 with
     // The operation ID will be bit-decomposed to yield selEq[], controlling which equations are activated.
     col witness operation_id;
 
+    // operation_id has to be either mul or div.
+    operation_id * (1 - operation_id) = 0;
+
     // Computes x1 * y1 + x2, where all inputs / outputs are 256-bit words (represented as 16-Bit limbs in little-endian order).
     // More precisely, affine_256(x1, y1, x2) = (y2, y3), where x1 * y1 + x2 = 2**256 * y2 + y3
     // Operation ID is 1 = 0b0001, i.e., we activate equation 0.
-    operation affine_256<1> x1c[0], x1c[1], x1c[2], x1c[3], x1c[4], x1c[5], x1c[6], x1c[7], x1c[8], x1c[9], x1c[10], x1c[11], x1c[12], x1c[13], x1c[14], x1c[15], y1c[0], y1c[1], y1c[2], y1c[3], y1c[4], y1c[5], y1c[6], y1c[7], y1c[8], y1c[9], y1c[10], y1c[11], y1c[12], y1c[13], y1c[14], y1c[15], x2c[0], x2c[1], x2c[2], x2c[3], x2c[4], x2c[5], x2c[6], x2c[7], x2c[8], x2c[9], x2c[10], x2c[11], x2c[12], x2c[13], x2c[14], x2c[15] -> y2c[0], y2c[1], y2c[2], y2c[3], y2c[4], y2c[5], y2c[6], y2c[7], y2c[8], y2c[9], y2c[10], y2c[11], y2c[12], y2c[13], y2c[14], y2c[15], y3c[0], y3c[1], y3c[2], y3c[3], y3c[4], y3c[5], y3c[6], y3c[7], y3c[8], y3c[9], y3c[10], y3c[11], y3c[12], y3c[13], y3c[14], y3c[15];
 
-    pol commit x1[32], y1[32], x2[32], y2[32], y3[32];
+    // x1 * y1 = y2 * 2**16 + y3
+    operation mul<0> x1c[0], x1c[1], y1c[0], y1c[1] -> y2c[0], y2c[1], y3c[0], y3c[1];
+
+    // Constrain that x2 = 0 when operation is mul.
+    array::new(4, |i| (1 - operation_id) * x2[i] = 0);
+    
+    // y3 / x1 = y1 (remainder x2)
+    // WARNING: it's not constrained that remainder is less than the divisor.
+    operation div<1> y3c[0], y3c[1], x1c[0], x1c[1] -> y1c[0], y1c[1], x2c[0], x2c[1];
+
+    // We need to provide hints for the quotient and remainder, because they are not unique under our current constraints.
+    let quotient_hint = query |limb| match(eval(operation_id)) {
+        1 => {
+            let y3 = y3_int();
+            let x1 = x1_int();
+            let quotient = y3 / x1;
+            Query::Hint(fe(select_limb(quotient, limb)))
+        },
+        _ => Query::None
+    };
+
+    col witness y1_0(i) query quotient_hint(0);
+    col witness y1_1(i) query quotient_hint(1);
+    col witness y1_2(i) query quotient_hint(2);
+    col witness y1_3(i) query quotient_hint(3);
+    
+    let y1: expr[] = [y1_0, y1_1, y1_2, y1_3];
+
+    let remainder_hint = query |limb| match(eval(operation_id)) {
+        1 => {
+            let y3 = y3_int();
+            let x1 = x1_int();
+            let remainder = y3 % x1;
+            Query::Hint(fe(select_limb(remainder, limb)))
+        },
+        _ => Query::None
+    };
+
+    col witness x2_0(i) query remainder_hint(0);
+    col witness x2_1(i) query remainder_hint(1);
+    col witness x2_2(i) query remainder_hint(2);
+    col witness x2_3(i) query remainder_hint(3);
+
+    let x2: expr[] = [x2_0, x2_1, x2_2, x2_3];
+
+    // Constrain that y2 = 0 when operation is div.
+    array::new(4, |i| operation_id * y2[i] = 0);
+
+    pol commit x1[4], y2[4], y3[4];
 
     // Selects the ith limb of x (little endian)
     // All limbs are 8 bits
@@ -49,14 +99,14 @@ machine Arith16 with
 
     let combine: expr[] -> expr[] = |x| array::new(array::len(x) / 2, |i| x[2 * i + 1] * 2**8 + x[2 * i]);
     // Intermediate polynomials, arrays of 16 columns, 16 bit per column.
-    col x1c[16] = combine(x1);
-    col y1c[16] = combine(y1);
-    col x2c[16] = combine(x2);
-    col y2c[16] = combine(y2);
-    col y3c[16] = combine(y3);
+    col x1c[2] = combine(x1);
+    col y1c[2] = combine(y1);
+    col x2c[2] = combine(x2);
+    col y2c[2] = combine(y2);
+    col y3c[2] = combine(y3);
 
-    let CLK64: col[64] = array::new(64, |i| |row| if row % 64 == i { 1 } else { 0 });
-    let CLK64_63: expr = CLK64[63];
+    let CLK8: col[8] = array::new(8, |i| |row| if row % 8 == i { 1 } else { 0 });
+    let CLK8_7: expr = CLK8[7];
 
     // TODO: Add the equivalent of these constraints for soundness: https://github.com/0xPolygonHermez/zkevm-proverjs/blob/main/pil/arith.pil#L43-L243
 
@@ -66,13 +116,13 @@ machine Arith16 with
     *
     *****/
 
-    let fixed_inside_64_block = |e| unchanged_until(e, CLK64[63]);
+    let fixed_inside_8_block = |e| unchanged_until(e, CLK8[7]);
 
-    array::map(x1, fixed_inside_64_block);
-    array::map(y1, fixed_inside_64_block);
-    array::map(x2, fixed_inside_64_block);
-    array::map(y2, fixed_inside_64_block);
-    array::map(y3, fixed_inside_64_block);
+    array::map(x1, fixed_inside_8_block);
+    array::map(y1, fixed_inside_8_block);
+    array::map(x2, fixed_inside_8_block);
+    array::map(y2, fixed_inside_8_block);
+    array::map(y3, fixed_inside_8_block);
 
     /****
     *
@@ -80,9 +130,9 @@ machine Arith16 with
     *
     *****/
 
-    link => byte.check(sum(32, |i| x1[i] * CLK64[i]) + sum(32, |i| y1[i] * CLK64[32 + i]));
-    link => byte.check(sum(32, |i| x2[i] * CLK64[i]) + sum(32, |i| y2[i] * CLK64[32 + i]));
-    link => byte.check(sum(32, |i| y3[i] * CLK64[i]) + sum(32, |i| y3[i] * CLK64[32 + i]));
+    link => byte.check(sum(4, |i| x1[i] * CLK8[i]) + sum(4, |i| y1[i] * CLK8[4 + i]));
+    link => byte.check(sum(4, |i| x2[i] * CLK8[i]) + sum(4, |i| y2[i] * CLK8[4 + i]));
+    link => byte.check(sum(4, |i| y3[i] * CLK8[i]));
 
     /*******
     *
@@ -110,11 +160,11 @@ machine Arith16 with
     let y2f = array_as_fun(y2);
     let y3f = array_as_fun(y3);
 
-    // Defined for arguments from 0 to 63 (inclusive)
+    // Defined for arguments from 0 to 7 (inclusive)
     let eq0 = |nr|
         product(x1f, y1f)(nr)
         + x2f(nr)
-        - shift_right(y2f, 32)(nr)
+        - shift_right(y2f, 4)(nr)
         - y3f(nr);
 
     /*******
@@ -133,7 +183,7 @@ machine Arith16 with
     // Carries can be any integer in the range [-2**31, 2**31 - 1)
     let carry = carry_high * 2**8 + carry_low;
     
-    carry * CLK64[0] = 0;
+    carry * CLK8[0] = 0;
 
     /*******
     *
@@ -141,7 +191,7 @@ machine Arith16 with
     *
     *******/
     
-    col eq0_sum = sum(64, |i| eq0(i) * CLK64[i]);
+    col eq0_sum = sum(8, |i| eq0(i) * CLK8[i]);
 
     eq0_sum + carry = carry' * 2**8;
 }
