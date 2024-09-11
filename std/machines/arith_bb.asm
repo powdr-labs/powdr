67ebfd67c8b17a81ae9c65c4b6205fc337507a5d
Leo Alt <leo@powdrlabs.com>
2024-09-11 15:08:52 +0200

    maccccc

---
+++ std/machines/arith_bb.asm
@@ -0,0 +1,32 @@
+use std::machines::range::Byte2;
+
+  machine ArithBB(byte2: Byte2) with
+    latch: latch,
+    operation_id: operation_id,
+    // Allow this machine to be connected via a permutation
+    call_selectors: sel,
+{
+    operation add<0> A_h, A_l, B_h, B_l -> C_h, C_l;
+    operation sub<1> A_h, A_l, C_h, C_l -> B_h, B_l;
+
+    col witness operation_id;
+
+    col fixed latch(i) { 1 };
+
+    let carry16 = std::utils::new_bool();
+    let carry32 = std::utils::new_bool();
+
+    link => byte2.check(A_l);
+    link => byte2.check(A_h);
+    link => byte2.check(B_l);
+    link => byte2.check(B_h);
+    link => byte2.check(C_l);
+    link => byte2.check(C_h);
+
+    col witness A_h, A_l;
+    col witness B_h, B_l;
+    col witness C_h, C_l;
+
+    A_l + B_l = C_l + carry16 * 2**16;
+    A_h + B_h + carry16 = C_h + carry32 * 2**16;
+}
