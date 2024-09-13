08b31790df6022ae13c470e94afb169c204259e3
chriseth <chris@ethereum.org>
2024-09-13 13:23:16 +0000

    test

--- pil-analyzer/tests/condenser.rs
+++ pil-analyzer/tests/condenser.rs
@@ -774,3 +774,39 @@ namespace N(16);
     let formatted = analyze_string(input).to_string();
     assert_eq!(formatted, expected);
 }
+
+#[test]
+fn capture_enums() {
+    let input = r#"
+    namespace N(16);
+        enum E<T> { A(T), B, C(T, int), D() }
+        (||
+            let x = E::A("abc");
+            let y = E::B::<int[][]>;
+            let z: E<int[]> = E::C([1, 2], 9);
+            let w: E<fe> = E::D();
+            query |_| {
+                let t = (x, y, z, w);
+        })();
+
+    "#;
+    let expected = r#"namespace N(16);
+    enum E<T> {
+        A(T),
+        B,
+        C(T, int),
+        D(),
+    }
+    let x: N::E<string> = N::E::A::<string>("abc");
+    let y: N::E<int[][]> = N::E::B::<int[][]>;
+    let z: N::E<int[]> = N::E::C::<int[]>([1, 2], 9);
+    let w: N::E<fe> = N::E::D::<fe>();
+    query |_| {
+        let t: (N::E<string>, N::E<int[][]>, N::E<int[]>, N::E<fe>) = (N::x, N::y, N::z, N::w);
+    };
+"#;
+    let formatted = analyze_string(input).to_string();
+    assert_eq!(formatted, expected);
+    let re_analyzed = analyze_string(&formatted);
+    assert_eq!(re_analyzed.to_string(), expected);
+}
