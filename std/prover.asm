8813b95ce6cfc651bc92ddd112fc173ad3544453
chriseth <chris@ethereum.org>
2024-09-13 13:12:10 +0000

    fix

--- std/prover.asm
+++ std/prover.asm
@@ -15,8 +15,8 @@ let try_eval: expr -> Option<fe> = [];
 let provide_value: expr, int, fe -> () = [];
 
 /// Provide a value to a column only if it has not been set yet.
-let provide_if_unknown: expr, int, (-> fe) -> () = query |column, row, f| match std::prover::try_eval(column) {
-    Option::None => std::prover::provide_value(column, row, f()),
+let provide_if_unknown: expr, int, (-> fe) -> () = query |column, row, f| match try_eval(column) {
+    Option::None => provide_value(column, row, f()),
     _ => (),
 };
 
@@ -33,11 +33,11 @@ let get_input_from_channel: int, int -> fe = [];
 let output_byte: int, int -> () = [];
 
 let handle_query: expr, int, std::prelude::Query -> () = query |column, row, v| match v {
-    Query::Input(i) => std::prover::provide_if_unknown(column, row, || std::prover::get_input(i)),
-    Query::DataIdentifier(i, j) => std::prover::provide_if_unknown(column, row, || std::prover::get_input_from_channel(i, j)),
-    Query::Hint(h) => std::prover::provide_if_unknown(column, row, || h),
+    Query::Hint(h) => provide_if_unknown(column, row, || h),
+    Query::Input(i) => provide_if_unknown(column, row, || get_input(i)),
+    Query::DataIdentifier(i, j) => provide_if_unknown(column, row, || get_input_from_channel(i, j)),
+    Query::Output(fd, b) => provide_if_unknown(column, row, || { output_byte(fd, b); 0 }),
     Query::None => (),
-    Query::Output(fd, b) => std::prover::output_byte(fd, b),
 };
 
 /// Constructs a challenge object.
