ddad0afd7a2ad293f6bc92efe159de8f2db85f3f
Lucas Clemente Vella <lvella@powdrlabs.com>
2024-09-17 11:40:50 +0100

    Extracting both matrices.

--- plonky3/src/bin/gen_poseidon2_bb_consts.rs
+++ plonky3/src/bin/gen_poseidon2_bb_consts.rs
@@ -1,10 +1,37 @@
 use itertools::Itertools;
-use p3_baby_bear::BabyBear;
+use p3_baby_bear::{BabyBear, DiffusionMatrixBabyBear};
+use p3_field::AbstractField;
 use p3_poseidon2::Poseidon2ExternalMatrixGeneral;
+use p3_symmetric::Permutation;
 use powdr_plonky3::{
-    baby_bear::{ROUNDS_F, ROUNDS_P, WIDTH},
+    baby_bear::{self, ROUNDS_F, ROUNDS_P, WIDTH},
     poseidon2,
 };
+
+fn extract_matrix(
+    mat: impl Permutation<[BabyBear; baby_bear::WIDTH]>,
+) -> Vec<[BabyBear; baby_bear::WIDTH]> {
+    let zeroed = [BabyBear::zero(); baby_bear::WIDTH];
+
+    let mut cols = Vec::with_capacity(baby_bear::WIDTH);
+    for i in 0..baby_bear::WIDTH {
+        let mut col = zeroed;
+        col[i] = BabyBear::one();
+        mat.permute_mut(&mut col);
+        cols.push(col);
+    }
+
+    // Transpose to row-major order for easier printing.
+    let mut rows = Vec::with_capacity(baby_bear::WIDTH);
+    for i in 0..baby_bear::WIDTH {
+        let mut row = [BabyBear::zero(); baby_bear::WIDTH];
+        for j in 0..baby_bear::WIDTH {
+            row[j] = cols[j][i];
+        }
+        rows.push(row);
+    }
+    rows
+}
 
 fn main() {
     println!("EXTERNAL_CONSTANTS = [");
@@ -14,14 +41,24 @@ fn main() {
     }
     println!("];");
 
-    println!("EXTERNAL_MDS = [");
-    let mds = Poseidon2ExternalMatrixGeneral;
-    // TODO: to be continued
+    println!("EXTERNAL_MATRIX = [");
+    let mds = extract_matrix(Poseidon2ExternalMatrixGeneral);
+    for row in mds {
+        println!("    [{}],", row.into_iter().format(", "));
+    }
+    println!("];");
 
     println!("INTERNAL_CONSTANTS = [");
     let ic = poseidon2::internal_constants::<BabyBear>(*ROUNDS_P);
     for &elem in ic.iter() {
         println!("    {},", elem);
+    }
+    println!("];");
+
+    println!("INTERNAL_MATRIX = [");
+    let diffusion = extract_matrix(DiffusionMatrixBabyBear::default());
+    for row in diffusion {
+        println!("    [{}],", row.into_iter().format(", "));
     }
     println!("];");
 }
