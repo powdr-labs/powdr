5745bc47be32ac31bc59281b135f7ef968d0d1ca
chriseth <chriseth.github@gmail.com>
2024-09-20 12:43:16 +0000

    Simplify parsed identities and introduce the concept of proof item.

--- pil-analyzer/src/condenser.rs
+++ pil-analyzer/src/condenser.rs
@@ -25,7 +25,7 @@ use powdr_ast::{
         types::{ArrayType, Type},
         visitor::{AllChildren, ExpressionVisitable},
         ArrayLiteral, BlockExpression, FunctionCall, FunctionKind, LambdaExpression,
-        LetStatementInsideBlock, Number, Pattern, TypedExpression, UnaryOperation,
+        LetStatementInsideBlock, Number, Pattern, SourceReference, TypedExpression, UnaryOperation,
     },
 };
 use powdr_number::{BigUint, FieldElement};
@@ -36,14 +36,13 @@ use crate::{
     statement_processor::Counters,
 };
 
-type ParsedIdentity = Identity<parsed::SelectedExpressions<Expression>>;
 type AnalyzedIdentity<T> = Identity<SelectedExpressions<AlgebraicExpression<T>>>;
 
 pub fn condense<T: FieldElement>(
     mut definitions: HashMap<String, (Symbol, Option<FunctionValueDefinition>)>,
     solved_impls: HashMap<String, HashMap<Vec<Type>, Arc<Expression>>>,
     public_declarations: HashMap<String, PublicDeclaration>,
-    identities: &[ParsedIdentity],
+    proof_items: &[Expression],
     source_order: Vec<StatementIdentifier>,
     auto_added_symbols: HashSet<String>,
 ) -> Analyzed<T> {
@@ -65,8 +64,8 @@ pub fn condense<T: FieldElement>(
                 condenser.set_namespace_and_degree(namespace, definitions[name].0.degree);
             }
             let statement = match s {
-                StatementIdentifier::Identity(index) => {
-                    condenser.condense_identity(&identities[index]);
+                StatementIdentifier::ProofItem(index) => {
+                    condenser.condense_proof_item(&proof_items[index]);
                     None
                 }
                 StatementIdentifier::Definition(name)
@@ -135,7 +134,7 @@ pub fn condense<T: FieldElement>(
                 .map(|identity| {
                     let index = condensed_identities.len();
                     condensed_identities.push(identity);
-                    StatementIdentifier::Identity(index)
+                    StatementIdentifier::ProofItem(index)
                 })
                 .collect::<Vec<_>>();
 
@@ -240,34 +239,21 @@ impl<'a, T: FieldElement> Condenser<'a, T> {
         }
     }
 
-    pub fn condense_identity(&mut self, identity: &'a ParsedIdentity) {
-        if identity.kind == IdentityKind::Polynomial {
-            let expr = identity.expression_for_poly_id();
-            evaluator::evaluate(expr, self)
-                .and_then(|expr| {
-                    if let Value::Tuple(items) = expr.as_ref() {
-                        assert!(items.is_empty());
-                        Ok(())
-                    } else {
-                        self.add_constraints(expr, identity.source.clone())
-                    }
-                })
-                .unwrap_or_else(|err| {
-                    panic!(
-                        "Error reducing expression to constraint:\nExpression: {expr}\nError: {err:?}"
-                    )
-                });
-        } else {
-            let left = self.condense_selected_expressions(&identity.left);
-            let right = self.condense_selected_expressions(&identity.right);
-            self.new_constraints.push(Identity {
-                id: self.counters.dispense_identity_id(),
-                kind: identity.kind,
-                source: identity.source.clone(),
-                left,
-                right,
+    pub fn condense_proof_item(&mut self, item: &'a Expression) {
+        evaluator::evaluate(item, self)
+            .and_then(|expr| {
+                if let Value::Tuple(items) = expr.as_ref() {
+                    assert!(items.is_empty());
+                    Ok(())
+                } else {
+                    self.add_proof_items(expr, item.source_reference().clone())
+                }
             })
-        }
+            .unwrap_or_else(|err| {
+                panic!(
+                    "Error reducing expression to constraint:\nExpression: {item}\nError: {err:?}"
+                )
+            });
     }
 
     /// Sets the current namespace which will be used for newly generated witness columns.
@@ -306,19 +292,6 @@ impl<'a, T: FieldElement> Condenser<'a, T> {
     /// Returns the new prover functions generated since the last call to this function.
     pub fn extract_new_prover_functions(&mut self) -> Vec<Expression> {
         std::mem::take(&mut self.new_prover_functions)
-    }
-
-    fn condense_selected_expressions(
-        &mut self,
-        sel_expr: &'a parsed::SelectedExpressions<Expression>,
-    ) -> SelectedExpressions<AlgebraicExpression<T>> {
-        SelectedExpressions {
-            selector: sel_expr
-                .selector
-                .as_ref()
-                .map(|expr| self.condense_to_algebraic_expression(expr)),
-            expressions: self.condense_to_array_of_algebraic_expressions(&sel_expr.expressions),
-        }
     }
 
     /// Evaluates the expression and expects it to result in an algebraic expression.
@@ -574,12 +547,12 @@ impl<'a, T: FieldElement> SymbolLookup<'a, T> for Condenser<'a, T> {
         Ok(())
     }
 
-    fn add_constraints(
+    fn add_proof_items(
         &mut self,
-        constraints: Arc<Value<'a, T>>,
+        items: Arc<Value<'a, T>>,
         source: SourceRef,
     ) -> Result<(), EvalError> {
-        match constraints.as_ref() {
+        match items.as_ref() {
             Value::Array(items) => {
                 for item in items {
                     self.new_constraints.push(to_constraint(
@@ -590,7 +563,7 @@ impl<'a, T: FieldElement> SymbolLookup<'a, T> for Condenser<'a, T> {
                 }
             }
             Value::Closure(..) => {
-                let e = try_value_to_expression(&constraints).map_err(|e| {
+                let e = try_value_to_expression(&items).map_err(|e| {
                     EvalError::TypeError(format!("Error adding prover function: {e}"))
                 })?;
 
@@ -598,7 +571,7 @@ impl<'a, T: FieldElement> SymbolLookup<'a, T> for Condenser<'a, T> {
             }
             _ => self
                 .new_constraints
-                .push(to_constraint(&constraints, source, &mut self.counters)),
+                .push(to_constraint(&items, source, &mut self.counters)),
         }
         Ok(())
     }
