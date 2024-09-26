bd515f120d62c72c69af1f13e6f52cbba7b05b42
chriseth <chriseth.github@gmail.com>
2024-09-26 12:39:44 +0000

    Make function safe.

--- jit-compiler/src/lib.rs
+++ jit-compiler/src/lib.rs
@@ -15,12 +15,12 @@ use powdr_number::FieldElement;
 pub struct LoadedFunction {
     #[allow(dead_code)]
     library: Arc<libloading::Library>,
-    function: unsafe extern "C" fn(u64) -> u64,
+    function: extern "C" fn(u64) -> u64,
 }
 
 impl LoadedFunction {
     pub fn call(&self, arg: u64) -> u64 {
-        unsafe { (self.function)(arg) }
+        (self.function)(arg)
     }
 }
 
