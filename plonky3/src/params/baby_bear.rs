7506cc0d1c1b95244496231eeaaa3ccab85ad4d4
Georg Wiese <georgwiese@gmail.com>
2024-10-11 17:10:21 +0100

    Plonky3 + Goldilocks: Use Poseidon2 instead of Poseidon (#1880)
    After discussion with the Polygon Zero team, this should be
    significantly faster when using AVX, while still recursion friendly.
    
    Tested this by running the Poseidon example (on our server) using the
    following script:
    ```
    cargo run -r --bin powdr-rs compile riscv/tests/riscv_data/keccak-o output --field bb
    cargo run -r --features plonky3,halo2 pil output/keccak.asm -o output -f --field bb --prove-with plonky3-composite
    ```
    
    I got these numbers:
    ```
    # main (stable Rust)
    
    ./keccak.sh
    3.0752053s
    
    RUSTFLAGS='-C target-cpu=native' ./keccak.sh
    5.150651s
    
    (using nightly-features and nightly Rust)
    RUSTFLAGS='-C target-cpu=native' ./keccak.sh
    3.8711894s
    
    # use-poseidon2 (stable Rust)
    
    ./keccak.sh
    2.4612818s
    
    RUSTFLAGS='-C target-cpu=native' ./keccak.sh
    2.2226317s
    
    (using nightly-features and nightly Rust)
    RUSTFLAGS='-C target-cpu=native' ./keccak.sh
    1.8559729s
    ```
    
    If I understand this right, this is still not using AVX, because we have
    stable Rust and have not activated `nightly-features`. But the AVX
    implementation of Poseidon on Goldilocks used a placeholder
    implementation that was slower than the non-AVX implementation, so the
    Rust flag actually harmed. This seems to be fixed by switching to
    Poseidon2.

--- plonky3/src/params/baby_bear.rs
+++ plonky3/src/params/baby_bear.rs
@@ -11,7 +11,7 @@ use p3_dft::Radix2DitParallel;
 use p3_field::{extension::BinomialExtensionField, Field};
 use p3_fri::{FriConfig, TwoAdicFriPcs};
 use p3_merkle_tree::MerkleTreeMmcs;
-use p3_poseidon2::{Poseidon2, Poseidon2ExternalMatrixGeneral};
+use p3_poseidon2::{poseidon2_round_numbers_128, Poseidon2, Poseidon2ExternalMatrixGeneral};
 use p3_symmetric::{PaddingFreeSponge, TruncatedPermutation};
 use p3_uni_stark::StarkConfig;
 
@@ -20,10 +20,6 @@ use rand::{distributions::Standard, Rng, SeedableRng};
 use powdr_number::BabyBearField;
 
 const D: u64 = 7;
-// params directly taken from plonky3's poseidon2_round_numbers_128 function
-// to guarantee 128-bit security.
-const ROUNDS_F: usize = 8;
-const ROUNDS_P: usize = 13;
 const WIDTH: usize = 16;
 type Perm = Poseidon2<BabyBear, Poseidon2ExternalMatrixGeneral, DiffusionMatrixBabyBear, WIDTH, D>;
 
@@ -58,11 +54,14 @@ const FRI_PROOF_OF_WORK_BITS: usize = 16;
 const RNG_SEED: u64 = 42;
 
 lazy_static! {
+    static ref ROUNDS: (usize, usize) = poseidon2_round_numbers_128::<BabyBear>(WIDTH, D);
+    static ref ROUNDS_F: usize = ROUNDS.0;
+    static ref ROUNDS_P: usize = ROUNDS.1;
     static ref PERM_BB: Perm = Perm::new(
-        ROUNDS_F,
+        *ROUNDS_F,
         poseidon2_external_constants(),
         Poseidon2ExternalMatrixGeneral,
-        ROUNDS_P,
+        *ROUNDS_P,
         poseidon2_internal_constants(),
         DiffusionMatrixBabyBear::default()
     );
@@ -71,7 +70,7 @@ lazy_static! {
 fn poseidon2_external_constants() -> Vec<[BabyBear; WIDTH]> {
     rand_chacha::ChaCha8Rng::seed_from_u64(RNG_SEED)
         .sample_iter(Standard)
-        .take(ROUNDS_F)
+        .take(*ROUNDS_F)
         .collect()
 }
 
@@ -79,7 +78,7 @@ fn poseidon2_internal_constants() -> Vec<BabyBear> {
     // Use a different seed here so numbers don't repeat.
     rand_chacha::ChaCha8Rng::seed_from_u64(RNG_SEED + 1)
         .sample_iter(Standard)
-        .take(ROUNDS_P)
+        .take(*ROUNDS_P)
         .collect()
 }
 
