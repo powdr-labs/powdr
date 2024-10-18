02e2c8f8c0caa06b2c132dd5ba216382a1857fe2
Georg Wiese <georgwiese@gmail.com>
2024-10-18 15:30:58 +0200

    Review feedback

--- riscv/src/continuations/bootloader.rs
+++ riscv/src/continuations/bootloader.rs
@@ -1,4 +1,5 @@
 use powdr_number::FieldElement;
+use powdr_number::FieldSize;
 use powdr_number::LargeInt;
 
 use super::memory_merkle_tree::MerkleTree;
@@ -57,24 +58,20 @@ pub fn shutdown_routine_upper_bound(num_pages: usize) -> usize {
 }
 
 pub fn bootloader_specific_instruction_names(field: KnownField) -> [&'static str; 2] {
-    match field {
-        KnownField::BabyBearField | KnownField::KoalaBearField | KnownField::Mersenne31Field => {
+    match field.field_size() {
+        FieldSize::Small => {
             todo!()
         }
-        KnownField::GoldilocksField | KnownField::Bn254Field => {
-            large_field::bootloader::BOOTLOADER_SPECIFIC_INSTRUCTION_NAMES
-        }
+        FieldSize::Large => large_field::bootloader::BOOTLOADER_SPECIFIC_INSTRUCTION_NAMES,
     }
 }
 
 pub fn bootloader_preamble(field: KnownField) -> String {
-    match field {
-        KnownField::BabyBearField | KnownField::KoalaBearField | KnownField::Mersenne31Field => {
+    match field.field_size() {
+        FieldSize::Small => {
             todo!()
         }
-        KnownField::GoldilocksField | KnownField::Bn254Field => {
-            large_field::bootloader::bootloader_preamble()
-        }
+        FieldSize::Large => large_field::bootloader::bootloader_preamble(),
     }
 }
 
@@ -82,11 +79,11 @@ pub fn bootloader_and_shutdown_routine(
     field: KnownField,
     submachine_initialization: &[String],
 ) -> String {
-    match field {
-        KnownField::BabyBearField | KnownField::KoalaBearField | KnownField::Mersenne31Field => {
+    match field.field_size() {
+        FieldSize::Small => {
             todo!()
         }
-        KnownField::GoldilocksField | KnownField::Bn254Field => {
+        FieldSize::Large => {
             large_field::bootloader::bootloader_and_shutdown_routine(submachine_initialization)
         }
     }
