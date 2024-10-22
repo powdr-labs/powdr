d2abf4869fdd8d709da31623e85979717ac9d914
Leo Alt <leo@powdrlabs.com>
2024-10-22 09:58:08 +0200

    first version of a user facing prover session

--- pipeline/tests/asm.rs
+++ pipeline/tests/asm.rs
@@ -350,14 +350,14 @@ fn read_poly_files() {
         pipeline.compute_proof().unwrap();
 
         // check fixed cols (may have no fixed cols)
-        let fixed = FixedPolySet::<Bn254Field>::read(tmp_dir.as_path());
+        let fixed = FixedPolySet::<Bn254Field>::read(tmp_dir.as_path()).unwrap();
         let fixed = get_uniquely_sized(&fixed).unwrap();
         if !fixed.is_empty() {
             assert_eq!(pil.degree(), fixed[0].1.len() as u64);
         }
 
         // check witness cols (examples assumed to have at least one witness col)
-        let witness = WitnessPolySet::<Bn254Field>::read(tmp_dir.as_path());
+        let witness = WitnessPolySet::<Bn254Field>::read(tmp_dir.as_path()).unwrap();
         assert_eq!(pil.degree(), witness[0].1.len() as u64);
     }
 }
