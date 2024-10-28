47f65a8f674835b54dac6050aec2ca96664a4a8b
Leo Alt <leo@powdrlabs.com>
2024-10-28 15:47:20 +0100

    cargo powdr

---
+++ cargo-powdr/template/guest/src/main.rs
@@ -0,0 +1,12 @@
+use powdr_riscv_runtime;
+use powdr_riscv_runtime::io::read;
+
+fn main() {
+    // Read some data from channel 1.
+    let data: Vec<u32> = read(1);
+    // Read the claimed sum from channel 2.
+    let sum: u32 = read(2);
+
+    // Check that the claimed sum is correct.
+    assert_eq!(data.iter().sum::<u32>(), sum);
+}
