024154d83e3092f91da25db77ad8b4183b662ea3
Lucas Clemente Vella <lvella@powdrlabs.com>
2024-10-28 12:14:30 +0000

    Making the increment amount an argument.

--- std/machines/small_field/pointer_arith.asm
+++ std/machines/small_field/pointer_arith.asm
@@ -1,6 +1,10 @@
-/// Creates constraints that increments a pointer by one full 32-bit word.
+/// Creates constraints that increments a pointer by amount.
 ///
-/// Preconditions: pre_low must be 4-byte aligned and in range [0, 0xfffc].
+/// Preconditions:
+/// - amount must be a power of two,
+/// - amount must be in range [1, 0x10000],
+/// - pre_low must be a multiple of amount,
+/// - pre_low must be in range [0, 0xffff].
 ///
 /// The pointers are given in 2 16-bit limbs. This function returns a set of constraints
 /// ensuring that the following are equal:
@@ -11,16 +15,22 @@
 /// caller will be able to latch-disable them in rows they aren't needed.
 ///
 /// This constr function introduces 2 new witness column and some helper constraints.
-let word_increment_ptr: expr, expr, expr, expr -> Constr[] = constr |pre_high, pre_low, post_high, post_low| {
+let increment_ptr: expr, expr, expr, expr, expr -> Constr[] = constr |
+    amount,
+    pre_high,
+    pre_low,
+    post_high,
+    post_low
+| {
     // Is the low limb at its maximum value?
     let carry;
-    carry = std::utils::is_zero(pre_low - (0x10000 - 4));
+    carry = std::utils::is_zero(pre_low - (0x10000 - amount));
 
     // The increment constraints returned to the caller:
     [
         // If low limb is about to overflow, next value must be 0,
         // otherwise, next value is current plus 4.
-        std::constraints::if_else(carry, post_low = 0, post_low = pre_low + 4),
+        std::constraints::if_else(carry, post_low = 0, post_low = pre_low + amount),
 
         // Set high limb, incremented if low overflowed:
         post_high = pre_high + carry
