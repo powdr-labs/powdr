cacb5ac8a797e1616094de3bbafc08fa2246ce29
Georg Wiese <georgwiese@gmail.com>
2024-10-30 13:17:36 +0100

    Add std::math::fp4::square_ext

--- pipeline/tests/powdr_std.rs
+++ pipeline/tests/powdr_std.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
-use powdr_number::{BabyBearField, BigInt, Bn254Field, GoldilocksField};
+use powdr_number::{
+    BabyBearField, BigInt, Bn254Field, GoldilocksField, KoalaBearField, Mersenne31Field,
+};
 
 use powdr_pil_analyzer::evaluator::Value;
 use powdr_pipeline::{
@@ -422,18 +424,35 @@ fn fp4() {
     evaluate_function(&analyzed, "std::math::fp4::test::add", vec![]);
     evaluate_function(&analyzed, "std::math::fp4::test::sub", vec![]);
     evaluate_function(&analyzed, "std::math::fp4::test::mul", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::square", vec![]);
     evaluate_function(&analyzed, "std::math::fp4::test::inverse", vec![]);
 
     let analyzed = std_analyzed::<Bn254Field>();
     evaluate_function(&analyzed, "std::math::fp4::test::add", vec![]);
     evaluate_function(&analyzed, "std::math::fp4::test::sub", vec![]);
     evaluate_function(&analyzed, "std::math::fp4::test::mul", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::square", vec![]);
     evaluate_function(&analyzed, "std::math::fp4::test::inverse", vec![]);
 
     let analyzed = std_analyzed::<BabyBearField>();
     evaluate_function(&analyzed, "std::math::fp4::test::add", vec![]);
     evaluate_function(&analyzed, "std::math::fp4::test::sub", vec![]);
     evaluate_function(&analyzed, "std::math::fp4::test::mul", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::square", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::inverse", vec![]);
+
+    let analyzed = std_analyzed::<Mersenne31Field>();
+    evaluate_function(&analyzed, "std::math::fp4::test::add", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::sub", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::mul", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::square", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::inverse", vec![]);
+
+    let analyzed = std_analyzed::<KoalaBearField>();
+    evaluate_function(&analyzed, "std::math::fp4::test::add", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::sub", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::mul", vec![]);
+    evaluate_function(&analyzed, "std::math::fp4::test::square", vec![]);
     evaluate_function(&analyzed, "std::math::fp4::test::inverse", vec![]);
 }
 
