ca4716dfdb0e109d4d0ae8e0c810da0f81a29c8c
chriseth <chriseth.github@gmail.com>
2024-11-04 17:28:30 +0000

    Prover function for latch row.

--- std/machines/large_field/binary.asm
+++ std/machines/large_field/binary.asm
@@ -35,5 +35,21 @@ machine Binary(byte_binary: ByteBinary) with
     B' = B * (1 - latch) + B_byte * FACTOR;
     C' = C * (1 - latch) + C_byte * FACTOR;
 
+    query |row| {
+        // TODO somehow the row is always 4.
+        if row % 4 == 3 {
+            let a = std::convert::int(std::prover::eval(A));
+            let b = std::convert::int(std::prover::eval(B));
+            let c = match operation_id {
+                0 => a & b,
+                1 => a | b,
+                2 => a ^ b,
+                _ => std::check::panic("Invalid operation_id"),
+            };
+            std::prover::provide_value(C, row, std::convert::fe(c));
+        } else {
+        }
+    };
+
     link => C_byte = byte_binary.run(operation_id', A_byte, B_byte);
 }
