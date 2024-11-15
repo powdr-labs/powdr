f34a539e7f1dc9264e2b5e2ee24574e275223a05
Georg Wiese <georgwiese@gmail.com>
2024-11-15 17:46:21 +0100

    Refactor MachineExtractor

--- executor/src/witgen/mod.rs
+++ executor/src/witgen/mod.rs
@@ -2,6 +2,7 @@ use std::collections::{BTreeMap, HashMap, HashSet};
 use std::sync::Arc;
 
 use itertools::Itertools;
+use machines::machine_extractor::MachineExtractor;
 use machines::MachineParts;
 use powdr_ast::analyzed::{
     AlgebraicExpression, AlgebraicReference, Analyzed, DegreeRange, Expression,
@@ -220,7 +221,7 @@ impl<'a, 'b, T: FieldElement> WitnessGenerator<'a, 'b, T> {
             mut machines,
             base_parts,
         } = if self.stage == 0 {
-            machines::machine_extractor::split_out_machines(&fixed, retained_identities, self.stage)
+            MachineExtractor::new(&fixed).split_out_machines(retained_identities, self.stage)
         } else {
             // We expect later-stage witness columns to be accumulators for lookup and permutation arguments.
             // These don't behave like normal witness columns (e.g. in a block machine), and they might depend
