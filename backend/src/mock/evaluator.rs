ffcc086b424eb81ec01af5e7c096b76921a6aff1
Georg Wiese <georgwiese@gmail.com>
2024-11-29 12:11:06 +0100

    Mock Backend: Handle constant LHS / RHS in connections

--- backend/src/mock/evaluator.rs
+++ backend/src/mock/evaluator.rs
@@ -1,7 +1,9 @@
 use std::collections::BTreeMap;
 
-use powdr_ast::analyzed::{Challenge, PolynomialType};
-use powdr_executor::witgen::{AffineResult, AlgebraicVariable, SymbolicVariables};
+use powdr_ast::analyzed::{AlgebraicExpression, Challenge, PolynomialType};
+use powdr_executor::witgen::{
+    AffineExpression, AffineResult, AlgebraicVariable, ExpressionEvaluator, SymbolicVariables,
+};
 use powdr_number::FieldElement;
 
 use super::machine::Machine;
@@ -37,5 +39,27 @@ impl<'a, F: FieldElement> SymbolicVariables<F> for &Variables<'a, F> {
 
     fn challenge<'b>(&self, challenge: &'b Challenge) -> AffineResult<AlgebraicVariable<'b>, F> {
         Ok(self.challenges[&challenge.id].into())
+    }
+}
+
+pub struct EmptyVariables;
+
+impl<T> SymbolicVariables<T> for EmptyVariables
+where
+    T: FieldElement,
+{
+    fn value<'a>(&self, _var: AlgebraicVariable<'a>) -> AffineResult<AlgebraicVariable<'a>, T> {
+        unimplemented!()
+    }
+}
+
+pub fn evaluate_to_fe<'a, F: FieldElement, SV: SymbolicVariables<F>>(
+    evaluator: &mut ExpressionEvaluator<'a, F, SV>,
+    expr: &'a AlgebraicExpression<F>,
+) -> F {
+    let result = evaluator.evaluate(expr).unwrap();
+    match result {
+        AffineExpression::Constant(c) => c,
+        _ => unreachable!("Unexpected result: {:?}", result),
     }
 }
