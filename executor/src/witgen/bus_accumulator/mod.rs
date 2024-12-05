b5de6ed7463858d99f21e6ce8ee2bd45b430dc69
Georg Wiese <georgwiese@gmail.com>
2024-12-05 22:27:30 +0100

    Bus: Materialize folded tuple

--- executor/src/witgen/bus_accumulator/mod.rs
+++ executor/src/witgen/bus_accumulator/mod.rs
@@ -85,13 +85,7 @@ impl<'a, T: FieldElement> BusAccumulatorGenerator<'a, T> {
         let accumulators = self
             .bus_interactions
             .par_iter()
-            .flat_map(|bus_interaction| {
-                let (acc1, acc2) = self.interaction_columns(bus_interaction);
-
-                // We assume that the second-stage witness columns are in this order,
-                // for each bus interaction.
-                [acc1, acc2]
-            })
+            .flat_map(|bus_interaction| self.interaction_columns(bus_interaction))
             .collect::<Vec<_>>();
 
         self.pil
@@ -105,11 +99,13 @@ impl<'a, T: FieldElement> BusAccumulatorGenerator<'a, T> {
     fn interaction_columns(
         &self,
         bus_interaction: &PhantomBusInteractionIdentity<T>,
-    ) -> (Vec<T>, Vec<T>) {
+    ) -> Vec<Vec<T>> {
         let intermediate_definitions = self.pil.intermediate_definitions();
         let empty_challenges = BTreeMap::new();
 
         let size = self.trace_values.height();
+        let mut folded1 = vec![T::zero(); size];
+        let mut folded2 = vec![T::zero(); size];
         let mut acc1 = vec![T::zero(); size];
         let mut acc2 = vec![T::zero(); size];
 
@@ -126,26 +122,26 @@ impl<'a, T: FieldElement> BusAccumulatorGenerator<'a, T> {
             };
             let multiplicity = evaluator.evaluate(&bus_interaction.multiplicity);
 
+            let tuple = bus_interaction
+                .tuple
+                .0
+                .iter()
+                .map(|r| evaluator.evaluate(r))
+                .collect::<Vec<_>>();
+            let folded = self.beta - self.fingerprint(&tuple);
+
             let new_acc = match multiplicity.is_zero() {
                 true => current_acc,
-                false => {
-                    let tuple = bus_interaction
-                        .tuple
-                        .0
-                        .iter()
-                        .map(|r| evaluator.evaluate(r))
-                        .collect::<Vec<_>>();
-
-                    let fingerprint = self.beta - self.fingerprint(&tuple);
-                    current_acc + fingerprint.inverse() * multiplicity
-                }
+                false => current_acc + folded.inverse() * multiplicity,
             };
 
+            folded1[i] = folded.0;
+            folded2[i] = folded.1;
             acc1[i] = new_acc.0;
             acc2[i] = new_acc.1;
         }
 
-        (acc1, acc2)
+        vec![folded1, folded2, acc1, acc2]
     }
 
     /// Fingerprints a tuples of field elements, using the pre-computed powers of alpha.
