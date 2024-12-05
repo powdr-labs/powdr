230f2f5e79a3aaf9099f9d77dad96093e731de95
Georg Wiese <georgwiese@gmail.com>
2024-12-05 21:56:42 +0100

    Hand-written bus witness generation

--- backend/src/mock/mod.rs
+++ backend/src/mock/mod.rs
@@ -16,6 +16,7 @@ use powdr_ast::{
 };
 use powdr_executor::{constant_evaluator::VariablySizedColumn, witgen::WitgenCallback};
 use powdr_number::{DegreeType, FieldElement};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 use crate::{Backend, BackendFactory, BackendOptions, Error, Proof};
 
@@ -106,10 +107,8 @@ impl<F: FieldElement> Backend<F> for MockBackend<F> {
         let start = std::time::Instant::now();
         let machines = self
             .machine_to_pil
-            // TODO: We should probably iterate in parallel, because Machine::try_new might generate
-            // later-stage witnesses, which is expensive.
-            // However, for now, doing it sequentially simplifies debugging.
-            .iter()
+            // Machine::try_new generates any second-stage witnesses, so better to do it in parallel.
+            .par_iter()
             .filter_map(|(machine_name, pil)| {
                 Machine::try_new(
                     machine_name.clone(),
