42c08b239ed61edd149e20d55025397f53c10a65
Gastón Zanitti <gzanitti@gmail.com>
2024-12-10 09:59:30 -0300

    Update pilopt/src/lib.rs
    Co-authored-by: chriseth <chriseth.github@gmail.com>

--- pilopt/src/lib.rs
+++ pilopt/src/lib.rs
@@ -131,15 +131,9 @@ fn build_poly_id_to_definition_name_lookup(
 
     for (name, (symbol, _)) in &pil_file.definitions {
         if matches!(symbol.kind, SymbolKind::Poly(_)) {
-            if symbol.is_array() {
                 symbol.array_elements().for_each(|(_, id)| {
-                    poly_id_to_info.insert(id, (name, true));
+                    poly_id_to_info.insert(id, (name, symbol.is_array()));
                 });
-            } else {
-                symbol.array_elements().for_each(|(_, id)| {
-                    poly_id_to_info.insert(id, (name, false));
-                });
-            }
         }
     }
 
