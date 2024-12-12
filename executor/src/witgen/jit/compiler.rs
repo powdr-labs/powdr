8520e0b3636840da34eabb1a867960d2ab424f41
chriseth <chriseth.github@gmail.com>
2024-12-12 17:42:35 +0000

    More codegen.

--- executor/src/witgen/jit/compiler.rs
+++ executor/src/witgen/jit/compiler.rs
@@ -1,16 +1,25 @@
 use std::{mem, sync::Arc};
 
+use itertools::Itertools;
 use powdr_jit_compiler::compiler::call_cargo;
 use powdr_number::FieldElement;
 
-use super::{affine_symbolic_expression::Effect, cell::Cell};
+use super::{
+    affine_symbolic_expression::{Assertion, Effect},
+    cell::Cell,
+    symbolic_expression::{BinaryOperator, SymbolicExpression, UnaryOperator},
+};
 
 pub fn compile_effects<T: FieldElement>(
     first_column_id: u64,
     column_count: usize,
+    known_inputs: &[Cell],
     effects: &[Effect<T, Cell>],
 ) -> Result<(), String> {
-    let code = util_code::<T>(first_column_id, column_count)?;
+    let utils = util_code::<T>(first_column_id, column_count)?;
+    let witgen_code = witgen_code(known_inputs, effects);
+    let code = format!("{utils}\n//-------------------------------\n{witgen_code}");
+
     let lib_path = powdr_jit_compiler::compiler::call_cargo(&code)
         .map_err(|e| format!("Failed to compile generated code: {e}"))?;
 
@@ -34,6 +43,111 @@ pub fn compile_effects<T: FieldElement>(
     // );
     // true
     Ok(())
+}
+
+fn witgen_code<T: FieldElement>(known_inputs: &[Cell], effects: &[Effect<T, Cell>]) -> String {
+    // TODO indent
+    let fun_name = "witgen";
+    let assign_inputs = known_inputs
+        .iter()
+        .map(|c| {
+            format!(
+                "let {} = get(data, row_offset, {}, {});",
+                cell_to_var_name(c),
+                c.row_offset,
+                c.id
+            )
+        })
+        .format("\n");
+    let main_code = effects.iter().map(format_effect).format("\n");
+    let store_values = ""; // TODO
+    format!(
+        r#"
+#[no_mangle]
+extern "C" fn {fun_name}(
+    WitgenFunctionParams{{
+        data,
+        known,
+        len,
+        row_offset,
+    }}: WitgenFunctionParams,
+    _mutable_state: *const c_void,
+    _call_machine: fn(*const c_void, u64, &mut [LookupCell<'_, FieldElement>]) -> bool
+) {{
+    let data: &mut [FieldElement] = unsafe {{ std::slice::from_raw_parts_mut(data as *mut FieldElement, len as usize) }};
+    {assign_inputs}
+    {main_code}
+    {store_values}
+    // TODO store_known
+    }}
+"#
+    )
+}
+
+fn format_effect<T: FieldElement>(effect: &Effect<T, Cell>) -> String {
+    match effect {
+        Effect::Assignment(var, e) => {
+            format!("let {} = {};", cell_to_var_name(var), format_expression(e))
+        }
+        Effect::RangeConstraint(..) => {
+            unreachable!("Final code should not contain pure range constraints.")
+        }
+        Effect::Assertion(Assertion {
+            lhs,
+            rhs,
+            expected_equal,
+        }) => format!(
+            "assert!({} {} {});",
+            format_expression(lhs),
+            if *expected_equal { "==" } else { "!=" },
+            format_expression(rhs)
+        ),
+        Effect::MachineCall(id, arguments) => todo!(),
+    }
+}
+
+fn format_expression<T: FieldElement>(e: &SymbolicExpression<T, Cell>) -> String {
+    match e {
+        SymbolicExpression::Concrete(v) => format!("FieldElement::from({v})"),
+        SymbolicExpression::Symbol(cell, _) => cell_to_var_name(cell),
+        SymbolicExpression::BinaryOperation(left, op, right, _) => {
+            let left = format_expression(left);
+            let right = format_expression(right);
+            match op {
+                BinaryOperator::Add => format!("({left} + {right})"),
+                BinaryOperator::Sub => format!("({left} - {right})"),
+                BinaryOperator::Mul => format!("({left} * {right})"),
+                BinaryOperator::Div => format!("({left} / {right})"),
+                BinaryOperator::IntegerDiv => format!("integer_div({left}, {right})"),
+                BinaryOperator::BitAnd => format!("({left} & {right})"),
+                BinaryOperator::BitOr => format!("({left} | {right})"),
+            }
+        }
+        SymbolicExpression::UnaryOperation(op, inner, _) => {
+            let inner = format_expression(inner);
+            match op {
+                UnaryOperator::Neg => format!("-{}", inner),
+            }
+        }
+    }
+}
+
+fn cell_to_var_name(
+    Cell {
+        column_name,
+        row_offset,
+        ..
+    }: &Cell,
+) -> String {
+    // TODO this might still not be unique.
+    let column_name = &column_name[column_name.rfind("::").unwrap() + 2..]
+        .replace("[", "_")
+        .replace("]", "_");
+    if *row_offset < 0 {
+        format!("{column_name}_u{}", -row_offset)
+    } else {
+        format!("{column_name}_d{row_offset}")
+    }
 }
 
 /// Returns the rust code containing utility functions given a first column id and a column count
@@ -193,7 +307,7 @@ mod tests {
 
     #[test]
     fn compile_util_code() {
-        if let Err(e) = compile_effects::<GoldilocksField>(0, 2, &[]) {
+        if let Err(e) = compile_effects::<GoldilocksField>(0, 2, &[], &[]) {
             println!("{e}");
             panic!()
         }
