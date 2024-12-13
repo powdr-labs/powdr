f48dd6bbb9955d85d70b9ed84037aaa0db5e8fbf
Georg Wiese <georgwiese@gmail.com>
2024-12-13 16:39:27 +0100

    Prepare block machine processor

--- executor/src/witgen/jit/mod.rs
+++ executor/src/witgen/jit/mod.rs
@@ -1,5 +1,8 @@
 pub(crate) mod affine_symbolic_expression;
-mod cell;
+mod variable;
 pub(crate) mod jit_processor;
 mod symbolic_expression;
 pub(crate) mod witgen_inference;
+
+#[cfg(test)]
+pub(crate) mod test_util;
