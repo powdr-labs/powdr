719b32f030bb13e109df8a77ed37cc45e3e2cb26
Gaston Zanitti <gzanitti@gmail.com>
2024-12-21 10:25:40 -0300

    keccak_powdr

--- riscv/tests/riscv.rs
+++ riscv/tests/riscv.rs
@@ -185,6 +185,13 @@ fn memfuncs() {
 #[ignore = "Too slow"]
 fn keccak() {
     let case = "keccak";
+    verify_riscv_crate(case, Default::default(), false);
+}
+
+#[test]
+#[ignore = "Too slow"]
+fn keccak_powdr() {
+    let case = "keccak_powdr";
     let options = CompilerOptions::new_gl().with_keccak();
     verify_riscv_crate_gl_with_options(case, Default::default(), options, false);
 }
