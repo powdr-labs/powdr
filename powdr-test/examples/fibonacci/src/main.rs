cb0d5a39d49448fe4f58c145b0dbb04b199d9cc8
Leo Alt <leo@powdrlabs.com>
2024-12-28 10:52:11 +0100

    readd and simplify test

--- powdr-test/examples/fibonacci/src/main.rs
+++ powdr-test/examples/fibonacci/src/main.rs
@@ -3,7 +3,7 @@ use powdr::Session;
 fn main() {
     env_logger::init();
 
-    let n = 22;
+    let n = 11;
     let mut session = Session::builder()
         .guest_path("./guest")
         .out_path("powdr-target")
@@ -14,16 +14,16 @@ fn main() {
     session.run();
 
     let r: u32 = session.stdout();
-    assert_eq!(r, 17711);
+    assert_eq!(r, 89);
 
     session.prove();
 
     let r: u32 = session.stdout();
-    assert_eq!(r, 17711);
+    assert_eq!(r, 89);
 
     let publics = session.publics();
     assert_eq!(
         publics,
-        [1743086182, 3059275111, 427636475, 2461890199, 2369471556, 1082999957, 4188715485, 3633080053]
+        [555233681, 1854640251, 3298928347, 2857173302, 2660189392, 1608424695, 543896544, 3870154745]
     );
 }
