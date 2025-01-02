1588eb2077f045cf563e99c8e031c968c6084b7a
chriseth <chriseth.github@gmail.com>
2025-01-02 13:25:52 +0000

    adjust tests.

--- executor/src/witgen/jit/single_step_processor.rs
+++ executor/src/witgen/jit/single_step_processor.rs
@@ -27,7 +27,10 @@ impl<'a, T: FieldElement> SingleStepProcessor<'a, T> {
         }
     }
 
-    pub fn generate_code(&self) -> Result<Vec<Effect<T, Variable>>, String> {
+    pub fn generate_code<CanProcess: CanProcessCall<T> + Clone>(
+        &self,
+        can_process: CanProcess,
+    ) -> Result<Vec<Effect<T, Variable>>, String> {
         // All witness columns in row 0 are known.
         let known_variables = self.machine_parts.witnesses.iter().map(|id| {
             Variable::Cell(Cell {
@@ -43,11 +46,11 @@ impl<'a, T: FieldElement> SingleStepProcessor<'a, T> {
             let mut progress = false;
 
             for id in &self.machine_parts.identities {
-                let row_offset = if id.contains_next_ref() { 0 } else { 1 };
-                if !complete.contains(&(id.id(), row_offset)) {
-                    let result = witgen.process_identity(id, row_offset);
+                if !complete.contains(&id.id()) {
+                    let row_offset = if id.contains_next_ref() { 0 } else { 1 };
+                    let result = witgen.process_identity(can_process.clone(), id, row_offset);
                     if result.complete {
-                        complete.insert((id.id(), row_offset));
+                        complete.insert(id.id());
                     }
                     progress |= result.progress;
                 }
@@ -73,13 +76,14 @@ impl<'a, T: FieldElement> SingleStepProcessor<'a, T> {
             .sorted()
             .collect_vec();
 
-        if unknown_witnesses.is_empty() {
-            assert_eq!(complete, self.machine_parts.identities.len());
+            let missing_identities = self.machine_parts.len() - complete.len();
+        if unknown_witnesses.is_empty() && missing_identities == 0{
             Ok(witgen.code())
         } else {
             Err(format!(
-                "Unable to derive algorithm to compute values for witness columns in the next row for the following columns: {}",
-                unknown_witnesses.iter().map(|wit| self.fixed_data.column_name(wit)).format(", ")
+                "Unable to derive algorithm to compute values for witness columns in the next row for the following columns:\n{}\nand {missing_identities} identities are missing.",
+                unknown_witnesses.iter().map(|wit| self.fixed_data.column_name(wit)).format(", "),
+                
             ))
         }
     }
@@ -104,10 +108,7 @@ mod test {
     use crate::{
         constant_evaluator,
         witgen::{
-            global_constraints,
-            jit::{effect::Effect, test_util::format_code},
-            machines::MachineParts,
-            FixedData,
+            data_structures::mutable_state::MutableState, global_constraints, jit::{effect::Effect, test_util::read_pil}, machines::{machine_extractor::MachineExtractor, KnownMachine, MachineParts}, FixedData
         },
     };
 
@@ -116,12 +117,18 @@ mod test {
     fn generate_single_step(
         input_pil: &str,
     ) -> Result<Vec<Effect<GoldilocksField, Variable>>, String> {
-        let analyzed: Analyzed<GoldilocksField> =
-            powdr_pil_analyzer::analyze_string(input_pil).unwrap();
-        let fixed_col_vals = constant_evaluator::generate(&analyzed);
+        let (analyzed, fixed_col_vals) = read_pil(input_pil);
+
         let fixed_data = FixedData::new(&analyzed, &fixed_col_vals, &[], Default::default(), 0);
         let (fixed_data, retained_identities) =
             global_constraints::set_global_constraints(fixed_data, &analyzed.identities);
+        let machines = MachineExtractor::new(&fixed_data).split_out_machines(retained_identities);
+        let mutable_state = MutableState::new(machines.into_iter(), &|_| {
+            Err("Query not implemented".to_string())
+        });
+        
+        let KnownMachine::DynamicMachine(machine)= mutable_state.get_machine("Main") else { panic!(); }
+        assert_eq!(machines.len(), 1);
 
         let witness_columns = analyzed
             .committed_polys_in_source_order()
@@ -137,24 +144,27 @@ mod test {
             // No prover functions
             Vec::new(),
         );
+        let mutable_state = MutableState::new(machines.into_iter(), &|_| {
+            Err("Query not implemented".to_string())
+        });
 
         SingleStepProcessor {
             fixed_data: &fixed_data,
             machine_parts,
         }
-        .generate_code()
+        .generate_code(&mutable_state)
     }
 
     #[test]
     fn fib() {
-        let input = "let X; let Y; X' = Y; Y' = X + Y;";
+        let input = "namespace Main(256); let X; let Y; X' = Y; Y' = X + Y;";
         let code = generate_single_step(input).unwrap();
         assert_eq!(format_code(&code), "X[1] = Y[0];\nY[1] = (X[0] + Y[0]);");
     }
 
     #[test]
     fn no_progress() {
-        let input = "let X; let Y; X' = X;";
+        let input = "namespace Main(256); let X; let Y; X' = X;";
         let err = generate_single_step(input).err().unwrap();
         assert_eq!(
             err.to_string(),
