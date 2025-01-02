4ca572d406a604e27ffba9584947b9b900478ad7
Gaston Zanitti <gzanitti@gmail.com>
2025-01-02 11:31:37 -0300

    failing test

--- backend/src/plonky3/stark.rs
+++ backend/src/plonky3/stark.rs
@@ -350,7 +350,6 @@ mod tests {
         Commitment<F>: Send,
     {
         let mut pipeline = Pipeline::<F>::default().from_pil_string(pil.to_string());
-
         let pil = pipeline.compute_optimized_pil().unwrap();
         let witness_callback = pipeline.witgen_callback().unwrap();
         let witness = &mut pipeline.compute_witness().unwrap();
@@ -414,7 +413,7 @@ mod tests {
             col witness x;
             col witness y;
             col witness z;
-            y - 1 = 0;
+            y - 1 = 1;
             x = 0;
             x + y = z;
 
