c113bc42ed7882de15acf01471077e612fc076f5
Georg Wiese <georgwiese@gmail.com>
2025-01-10 15:13:48 +0100

    Bus permutation: Put phantom permutation in sender

--- std/protocols/permutation_via_bus.asm
+++ std/protocols/permutation_via_bus.asm
@@ -10,6 +10,9 @@ let permutation_send: expr, Constr -> () = constr |id, permutation_constraint| {
     let (lhs_selector, lhs, rhs_selector, rhs) = unpack_permutation_constraint(permutation_constraint);
 
     bus_send(id, lhs, lhs_selector);
+    
+    // Add an annotation for witness generation
+    to_phantom_permutation(permutation_constraint);
 };
 
 /// Given an ID and permutation constraints, receives the (ID, permutation_constraint.rhs...) tuple from the bus
@@ -19,7 +22,4 @@ let permutation_receive: expr, Constr, expr -> () = constr |id, permutation_cons
     let (lhs_selector, lhs, rhs_selector, rhs) = unpack_permutation_constraint(permutation_constraint);
     
     bus_receive(id, rhs, rhs_selector, latch);
-    
-    // Add an annotation for witness generation
-    to_phantom_permutation(permutation_constraint);
 };
