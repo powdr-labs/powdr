5043fb4c04ab5fe3815237473d6a6df0c9cb899e
Georg Wiese <georgwiese@gmail.com>
2025-01-13 22:11:42 -0300

    Fmt

--- executor/src/witgen/bus_accumulator/fp4.rs
+++ executor/src/witgen/bus_accumulator/fp4.rs
@@ -1,5 +1,7 @@
 use std::{
-    collections::BTreeMap, iter::Sum, ops::{Add, Div, Mul, Sub}
+    collections::BTreeMap,
+    iter::Sum,
+    ops::{Add, Div, Mul, Sub},
 };
 
 use num_traits::{One, Zero};
