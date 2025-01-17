505fdc5d4baaec68cd7872415f6f1464bb6fce72
chriseth <chriseth.github@gmail.com>
2025-01-17 21:49:41 +0000

    Compute variables in intermediate columns efficiently.

--- executor/src/witgen/jit/processor.rs
+++ executor/src/witgen/jit/processor.rs
@@ -6,7 +6,10 @@ use std::{
 
 use itertools::Itertools;
 use powdr_ast::{
-    analyzed::{AlgebraicExpression as Expression, Identity, PolyID, PolynomialType},
+    analyzed::{
+        AlgebraicExpression as Expression, AlgebraicReference, AlgebraicReferenceThin, Identity,
+        PolyID, PolynomialType,
+    },
     parsed::visitor::{AllChildren, Children},
 };
 use powdr_number::FieldElement;
@@ -50,14 +53,7 @@ impl<'a, T: FieldElement, FixedEval: FixedEvaluator<T>> Processor<'a, T, FixedEv
         max_branch_depth: usize,
     ) -> Self {
         let identities = identities.into_iter().collect_vec();
-        let occurrences = identities
-            .iter()
-            .flat_map(|(id, row)| {
-                variables_in_identity(fixed_data, id, *row).map(move |var| (var, (*id, *row)))
-            })
-            .unique()
-            .into_grouping_map()
-            .collect();
+        let occurrences = compute_occurrences_map(fixed_data, &identities);
         Self {
             fixed_data,
             fixed_evaluator,
@@ -320,46 +316,96 @@ impl<'a, T: FieldElement, FixedEval: FixedEvaluator<T>> Processor<'a, T, FixedEv
     }
 }
 
-/// Returns all variables occurring in the identity.
-/// This also resolves intermediate polynomials.
-fn variables_in_identity<'a, T: FieldElement>(
+/// Computes a map from each variable to the identitie-row-offset pairs it occurs in.
+fn compute_occurrences_map<'a, T: FieldElement>(
     fixed_data: &'a FixedData<'a, T>,
-    identity: &'a Identity<T>,
-    row_offset: i32,
-) -> impl Iterator<Item = Variable> + 'a {
-    identity
-        .children()
-        .flat_map(move |e| variables_in_expression(fixed_data, e, row_offset))
+    identities: &[(&'a Identity<T>, i32)],
+) -> HashMap<Variable, Vec<(&'a Identity<T>, i32)>> {
+    let mut references_per_identity = HashMap::new();
+    let mut intermediate_cache = HashMap::new();
+    for id in identities.iter().map(|(id, _)| *id).unique_by(|id| id.id()) {
+        references_per_identity.insert(
+            id,
+            references_in_identity(id, fixed_data, &mut intermediate_cache),
+        );
+    }
+    identities
+        .iter()
+        .flat_map(|(id, row)| {
+            references_per_identity[id].iter().map(move |reference| {
+                let name = fixed_data.column_name(&reference.poly_id).to_string();
+                let fat_ref = AlgebraicReference {
+                    name,
+                    poly_id: reference.poly_id,
+                    next: reference.next,
+                };
+                let var = Variable::from_reference(&fat_ref, *row);
+                (var, (*id, *row))
+            })
+        })
+        .into_group_map()
 }
 
-/// Returns all variables occurring in the expression.
-/// This also resolves intermediate polynomials.
-fn variables_in_expression<'a, T: FieldElement>(
-    fixed_data: &'a FixedData<'a, T>,
+/// Returns all references to witness column in the identity.
+fn references_in_identity<T: FieldElement>(
+    identity: &Identity<T>,
+    fixed_data: &FixedData<T>,
+    intermediate_cache: &mut HashMap<AlgebraicReferenceThin, Vec<AlgebraicReferenceThin>>,
+) -> Vec<AlgebraicReferenceThin> {
+    let mut result = BTreeSet::new();
+    for e in identity.children() {
+        result.extend(references_in_expression(e, fixed_data, intermediate_cache));
+    }
+    result.into_iter().collect()
+}
+
+/// Recursively resolves references in intermediate column definitions.
+fn references_in_intermediate<T: FieldElement>(
+    fixed_data: &FixedData<T>,
+    intermediate: &AlgebraicReferenceThin,
+    intermediate_cache: &mut HashMap<AlgebraicReferenceThin, Vec<AlgebraicReferenceThin>>,
+) -> Vec<AlgebraicReferenceThin> {
+    if let Some(references) = intermediate_cache.get(intermediate) {
+        return references.clone();
+    }
+    let references = references_in_expression(
+        &fixed_data.intermediate_definitions[intermediate],
+        fixed_data,
+        intermediate_cache,
+    )
+    .collect_vec();
+    intermediate_cache.insert(intermediate.clone(), references.clone());
+    references
+}
+
+/// Returns all references to witness or intermediate column in the expression.
+fn references_in_expression<'a, T: FieldElement>(
     expression: &'a Expression<T>,
-    row_offset: i32,
-) -> impl Iterator<Item = Variable> + 'a {
+    fixed_data: &'a FixedData<T>,
+    intermediate_cache: &'a mut HashMap<AlgebraicReferenceThin, Vec<AlgebraicReferenceThin>>,
+) -> impl Iterator<Item = AlgebraicReferenceThin> + 'a {
     expression
         .all_children()
-        .flat_map(move |e| -> Box<dyn Iterator<Item = Variable> + 'a> {
-            match e {
-                Expression::Reference(r) => match r.poly_id.ptype {
-                    PolynomialType::Constant => Box::new(std::iter::empty()),
-                    PolynomialType::Committed => {
-                        Box::new(std::iter::once(Variable::from_reference(r, row_offset)))
+        .flat_map(
+            move |e| -> Box<dyn Iterator<Item = AlgebraicReferenceThin> + 'a> {
+                match e {
+                    Expression::Reference(r) => match r.poly_id.ptype {
+                        PolynomialType::Constant => Box::new(std::iter::empty()),
+                        PolynomialType::Committed => Box::new(std::iter::once(r.into())),
+                        PolynomialType::Intermediate => Box::new(
+                            references_in_intermediate(fixed_data, &r.into(), intermediate_cache)
+                                .into_iter(),
+                        ),
+                    },
+                    Expression::PublicReference(_) | Expression::Challenge(_) => {
+                        // TODO we need to introduce a variable type for those.
+                        Box::new(std::iter::empty())
                     }
-                    PolynomialType::Intermediate => {
-                        let definition = &fixed_data.intermediate_definitions[&r.to_thin()];
-                        Box::new(variables_in_expression(fixed_data, definition, row_offset))
-                    }
-                },
-                Expression::PublicReference(_) | Expression::Challenge(_) => {
-                    // TODO we need to introduce a variable type for those.
-                    Box::new(std::iter::empty())
+                    _ => Box::new(std::iter::empty()),
                 }
-                _ => Box::new(std::iter::empty()),
-            }
-        })
+            },
+        )
+        .unique()
 }
 
 /// Sorts identities by row and then by ID.
