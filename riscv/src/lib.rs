6d0e004cdf41f6805673a35d038e43398fb88ddd
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2025-01-17 14:19:30 +0800

    bumped session build script cargo version from 20240801 to 20241217

--- riscv/src/lib.rs
+++ riscv/src/lib.rs
@@ -367,7 +367,7 @@ fn build_cargo_command(
 
     let mut args: Vec<&OsStr> = as_ref![
         OsStr;
-        "+nightly-2024-08-01",
+        "+nightly-2024-12-17",
         "build",
         "--release",
         "--target-dir",
