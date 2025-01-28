8026f4da0bd8e2bc0685f5d64d34dcd6e4b42b00
ShuangWu121 <47602565+ShuangWu121@users.noreply.github.com>
2025-01-28 21:56:52 +0100

    Update backend/src/stwo/circuit_builder.rs
    Co-authored-by: Georg Wiese <georgwiese@gmail.com>

--- backend/src/stwo/circuit_builder.rs
+++ backend/src/stwo/circuit_builder.rs
@@ -110,7 +110,7 @@ struct Data<'a, F> {
     witness_eval: &'a BTreeMap<PolyID, [F; 2]>,
     constant_shifted_eval: &'a BTreeMap<PolyID, F>,
     constant_eval: &'a BTreeMap<PolyID, F>,
-    // challenges for stage 0
+    // challenges for stage 1
     challenges: &'a BTreeMap<u64, F>,
 }
 
