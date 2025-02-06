ebf4612667f7fc14c4ddaf1f4ca0c64d1fb2e254
chriseth <chriseth.github@gmail.com>
2025-02-06 15:42:11 +0000

    x

--- executor/src/witgen/jit/single_step_processor.rs
+++ executor/src/witgen/jit/single_step_processor.rs
@@ -237,12 +237,12 @@ namespace M(256);
         assert_eq!(
             format_code(&code),
             "\
-VM::pc[1] = (VM::pc[0] + 1);
 call_var(1, 0, 0) = VM::pc[0];
 call_var(1, 0, 1) = VM::instr_add[0];
 call_var(1, 0, 2) = VM::instr_mul[0];
-VM::B[1] = VM::B[0];
+VM::pc[1] = (VM::pc[0] + 1);
 call_var(1, 1, 0) = VM::pc[1];
+VM::B[1] = VM::B[0];
 machine_call(1, [Known(call_var(1, 1, 0)), Unknown(call_var(1, 1, 1)), Unknown(call_var(1, 1, 2))]);
 VM::instr_add[1] = call_var(1, 1, 1);
 VM::instr_mul[1] = call_var(1, 1, 2);
