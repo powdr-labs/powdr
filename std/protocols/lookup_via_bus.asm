40839c63ad82ae0b876078eb63f2fa742929fbd5
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2025-02-11 13:14:21 +0800

    first commit; should fix but no docs yet

--- std/protocols/lookup_via_bus.asm
+++ std/protocols/lookup_via_bus.asm
@@ -1,4 +1,6 @@
 use std::protocols::bus::bus_receive;
+use std::protocols::bus::bus_multi_receive;
+use std::array;
 
 /// Given an ID, selector, and tuple, receives (ID, ...tuple) tuple from the bus
 /// with a prover-provided multiplicity if the selector is 1.
@@ -7,4 +9,14 @@ let lookup_receive: expr, expr, expr[] -> () = constr |id, selector, tuple| {
     (1 - selector) * multiplicities = 0;
     
     bus_receive(id, tuple, multiplicities, selector);
+};
+
+let lookup_multi_receive: (expr, expr, expr[])[] -> () = constr |inputs| {
+    let inputs_inner: (expr, expr[], expr, expr)[] = array::fold(inputs, [], constr |acc, input| {
+        let (id, selector, tuple) = input;
+        let multiplicity;
+        (1 - selector) * multiplicity = 0;
+        acc + [(id, tuple, multiplicity, selector)]
+    });
+    bus_multi_receive(inputs_inner);
 };
--- std/protocols/permutation_via_bus.asm
+++ std/protocols/permutation_via_bus.asm
@@ -1,7 +1,17 @@
 use std::protocols::bus::bus_receive;
+use std::protocols::bus::bus_multi_receive;
+use std::array;
 
 /// Given an ID, selector, and tuple, receives (ID, ...tuple) tuple from the bus
 /// with multiplicity 1 if the selector is 1.
 let permutation_receive: expr, expr, expr[] -> () = constr |id, selector, tuple| {
     bus_receive(id, tuple, selector, selector);
+};
+
+let permutation_multi_receive: (expr, expr, expr[])[] -> () = constr |inputs| {
+    let inputs_inner: (expr, expr[], expr, expr)[] = array::fold(inputs, [], |acc, input| {
+        let (id, selector, tuple) = input;
+        acc + [(id, tuple, selector, selector)]
+    });
+    bus_multi_receive(inputs_inner);
 };
