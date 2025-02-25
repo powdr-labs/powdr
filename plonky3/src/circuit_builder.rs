017f2f86c6fb82668990877ce19dff3a2b898e29
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2025-02-25 16:49:30 +0800

    reverted unwanted changes

--- plonky3/src/circuit_builder.rs
+++ plonky3/src/circuit_builder.rs
@@ -159,14 +159,13 @@ where
     /// For stages in which there are no public values, return an empty vector
     pub fn public_values_so_far(
         &self,
-        // witness_by_machine: &BTreeMap<String, Vec<(String, Vec<T>)>>,
-        public: &BTreeMap<String, T>,
+        witness_by_machine: &BTreeMap<String, Vec<(String, Vec<T>)>>,
     ) -> BTreeMap<String, Vec<Vec<Option<T>>>> {
-        // let witness = witness_by_machine
-        //     .values()
-        //     // this map seems redundant but it turns a reference over a tuple into a tuple of references
-        //     .flat_map(|machine_witness| machine_witness.iter().map(|(n, v)| (n, v)))
-        //     .collect::<BTreeMap<_, _>>();
+        let witness = witness_by_machine
+            .values()
+            // this map seems redundant but it turns a reference over a tuple into a tuple of references
+            .flat_map(|machine_witness| machine_witness.iter().map(|(n, v)| (n, v)))
+            .collect::<BTreeMap<_, _>>();
 
         self.split
             .iter()
@@ -177,7 +176,7 @@ where
                     .map(|publics| {
                         publics
                             .iter()
-                            .map(|(_, name, _, _)| public.get(name).cloned())
+                            .map(|(_, name, _, row)| witness.get(name).map(|column| column[*row]))
                             .collect()
                     })
                     .collect();
@@ -444,7 +443,6 @@ where
         trace_stage: u8,
         new_challenge_values: &[Plonky3Field<T>],
         witness_by_machine: &mut BTreeMap<String, Vec<(String, Vec<T>)>>,
-        public: &BTreeMap<String, T>,
     ) -> CallbackResult<Plonky3Field<T>> {
         let previous_stage_challenges: BTreeSet<&u64> = self
             .split
@@ -483,7 +481,7 @@ where
                 .collect()
         });
 
-        let public_values = self.public_values_so_far(public);
+        let public_values = self.public_values_so_far(witness_by_machine);
 
         // generate the next trace in the format p3 expects
         let air_stages = witness_by_machine
