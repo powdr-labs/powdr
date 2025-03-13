dc96fdddb0cd18ff6a18d3aa3ce7ec9cf3f5a7e1
chriseth <chris@ethereum.org>
2025-03-13 15:33:35 +0100

    Fix doc test. (#2542)

--- executor/src/witgen/jit/prover_function_heuristics.rs
+++ executor/src/witgen/jit/prover_function_heuristics.rs
@@ -122,12 +122,13 @@ fn try_decode_provide_if_unknown<T>(
 }
 
 /// Decodes functions of the form
-/// ```
-/// query |<i>| std::prover::handle_query(<Y>, <i>, match std::prover::eval(<pc>) {
+/// ```rust,ignore
+/// query |<i>| std::prover::handle_query(<Y>, <i>, match std::prover::eval(<pc>)) {
 ///   <value1> => std::prelude::Query::Output(std::convert::int::<fe>(std::prover::eval(<arg1>)), std::prover::eval(<arg2>)),
 ///   <value2> => std::prelude::Query::Input(std::convert::int::<fe>(std::prover::eval(<arg1>)), std::convert::int::<fe>(std::prover::eval(<arg2>))),
 ///   ...
 ///   _ => std::prelude::Query::None,
+/// }
 /// ```
 fn try_decode_handle_query<T>(
     index: usize,
