4529b89b0aa109247c4e36550cab180655e332fc
chriseth <chriseth.github@gmail.com>
2025-03-25 08:22:10 +0000

    Reduce interpreter code size limit.

--- executor/src/witgen/jit/function_cache.rs
+++ executor/src/witgen/jit/function_cache.rs
@@ -28,7 +28,7 @@ use super::{
 /// Inferred witness generation routines that are larger than
 /// this number of "statements" will use the interpreter instead of the compiler
 /// due to the large compilation ressources required.
-const MAX_COMPILED_CODE_SIZE: usize = 500;
+const MAX_COMPILED_CODE_SIZE: usize = 1000;
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct CacheKey<T: FieldElement> {
