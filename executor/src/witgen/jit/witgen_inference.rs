45b3aa0ac7a16159512cf8c8d16062a325f32347
chriseth <chris@ethereum.org>
2025-04-02 17:34:44 +0200

    Enable JIT for permutations (#2593)
    Enable support for permutation constraints. The main problem with
    permutation constraints is that they need a way to disable constraints
    and thus this PR adds a feature to guess values for variables that have
    not been assigned yet but are also not relevant in any of the
    identities, for example because they are multiplied by a value that is
    known to be zero.
    
    ---------
    
    Co-authored-by: Georg Wiese <georgwiese@gmail.com>

--- executor/src/witgen/jit/witgen_inference.rs
+++ executor/src/witgen/jit/witgen_inference.rs
@@ -227,6 +227,17 @@ impl<'a, T: FieldElement, FixedEval: FixedEvaluator<T>> WitgenInference<'a, T, F
         )
     }
 
+    /// Set a variable to a fixed value.
+    pub fn set_variable(&mut self, variable: Variable, value: T) -> Result<Vec<Variable>, Error> {
+        self.process_equation_on_row(
+            &Expression::Number(0.into()),
+            Some(variable),
+            -value,
+            // The row does not matter because the algebraic expression is empty
+            0,
+        )
+    }
+
     /// Processes the equality constraint `lhs = variable + offset` with row offset `row_offset`.
     /// If `variable` is `None`, it is treated as zero.
     /// If this returns an error, it means we have conflicting constraints.
@@ -500,9 +511,7 @@ impl<'a, T: FieldElement, FixedEval: FixedEvaluator<T>> WitgenInference<'a, T, F
     }
 
     pub fn try_evaluate_to_known_number(&self, expr: &Expression<T>, offset: i32) -> Option<T> {
-        self.evaluate(expr, offset)
-            .and_then(|s| s.try_to_known().map(|k| k.try_to_number()))
-            .flatten()
+        self.evaluate(expr, offset)?.try_to_known()?.try_to_number()
     }
 }
 
