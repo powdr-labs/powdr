1d30002c6028042c513d8f96dcef072a29f8b9e5
Georg Wiese <georgwiese@gmail.com>
2025-04-17 12:06:12 +0200

    Improve tests

--- executor/src/witgen/jit/solver.rs
+++ executor/src/witgen/jit/solver.rs
@@ -159,6 +159,40 @@ mod tests {
         GoldilocksField::from(value).into()
     }
 
+    fn constant_expr(value: u64) -> SymbolicExpression<GoldilocksField, &'static str> {
+        GoldilocksField::from(value).into()
+    }
+
+    fn var_expr(name: &'static str) -> SymbolicExpression<GoldilocksField, &'static str> {
+        SymbolicExpression::Symbol(name, RangeConstraint::default())
+    }
+
+    fn assert_expected_state(
+        final_state: BTreeMap<&'static str, SymbolicExpression<GoldilocksField, &'static str>>,
+        expected_final_state: BTreeMap<
+            &'static str,
+            SymbolicExpression<GoldilocksField, &'static str>,
+        >,
+    ) {
+        assert_eq!(
+            final_state.keys().collect::<Vec<_>>(),
+            expected_final_state.keys().collect::<Vec<_>>(),
+            "Different set of variables"
+        );
+
+        let mut error = false;
+        for (variable, expression) in expected_final_state {
+            // Compare string representation, so that range constraints are ignored.
+            if final_state[variable].to_string() != expression.to_string() {
+                log::error!("Mismatch for variable {variable}:");
+                log::error!("  Expected: {expression}");
+                log::error!("  Actual:   {}", final_state[variable]);
+                error = true;
+            }
+        }
+        assert!(!error, "Final state does not match expected state");
+    }
+
     fn init_logging() {
         static INIT: std::sync::Once = std::sync::Once::new();
         INIT.call_once(|| {
@@ -174,8 +208,8 @@ mod tests {
         let constraints = vec![var("x") - constant(5)];
         let final_state = Solver::new(constraints).solve().unwrap();
 
-        let expected_final_state = BTreeMap::from([("x", GoldilocksField::from(5).into())]);
-        assert_eq!(final_state, expected_final_state);
+        let expected_final_state = BTreeMap::from([("x", constant_expr(5))]);
+        assert_expected_state(final_state, expected_final_state);
     }
 
     #[test]
@@ -196,12 +230,12 @@ mod tests {
 
         let final_state = Solver::new(constraints).solve().unwrap();
         let expected_final_state = BTreeMap::from([
-            ("a", GoldilocksField::from(2).into()),
-            ("b", GoldilocksField::from(3).into()),
-            ("c", GoldilocksField::from(6).into()),
-            ("d", GoldilocksField::from(22).into()),
+            ("a", constant_expr(2)),
+            ("b", constant_expr(3)),
+            ("c", constant_expr(6)),
+            ("d", constant_expr(22)),
         ]);
-        assert_eq!(final_state, expected_final_state);
+        assert_expected_state(final_state, expected_final_state);
     }
 
     #[test]
@@ -219,12 +253,37 @@ mod tests {
         .collect();
 
         let final_state = Solver::new(constraints).solve().unwrap();
-        // let expected_final_state = BTreeMap::from([
-        //     ("a", GoldilocksField::from(2).into()),
-        //     ("b", GoldilocksField::from(3).into()),
-        //     ("c", GoldilocksField::from(6).into()),
-        //     ("d", GoldilocksField::from(22).into()),
-        // ]);
-        // assert_eq!(final_state, expected_final_state);
+        let expected_final_state = BTreeMap::from([
+            ("b", constant_expr(3)),
+            // TODO: This should simplify to `3 * a`
+            ("c", -constant_expr(3) * (-var_expr("a"))),
+            // TODO: This should simplify to `11 * a`
+            ("d", -var_expr("a") + var_expr("c") * constant_expr(4)),
+        ]);
+        assert_expected_state(final_state, expected_final_state);
+    }
+
+    #[test]
+    fn symbolically_solvable2() {
+        init_logging();
+        let constraints = [
+            var("a") - constant(2),
+            var("b") - constant(3),
+            var("c") - known("a") * var("b"),
+            var("d") - (var("c") * constant(4) - known("a")),
+        ]
+        .into_iter()
+        // Reverse to make sure several passes are necessary
+        .rev()
+        .collect();
+
+        let final_state = Solver::new(constraints).solve().unwrap();
+        let expected_final_state = BTreeMap::from([
+            ("a", constant_expr(2)),
+            ("b", constant_expr(3)),
+            ("c", constant_expr(6)),
+            ("d", constant_expr(22)),
+        ]);
+        assert_expected_state(final_state, expected_final_state);
     }
 }
