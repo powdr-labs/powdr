d3745013fcc9eac07da47cd66e667d29cfa85f52
Georg Wiese <georgwiese@gmail.com>
2025-04-17 15:49:56 +0200

    Refactor tests

--- executor/src/witgen/jit/solver/tests/mod.rs
+++ executor/src/witgen/jit/solver/tests/mod.rs
@@ -1,65 +1,47 @@
 mod test_utils;
 
-use super::*;
 use test_utils::*;
 
 #[test]
 fn single_variable() {
-    init_logging();
-    let constraints = vec![var("x") - constant(5)];
-    let final_state = Solver::new(constraints).solve().unwrap();
-
-    let expected_final_state = BTreeMap::from([("x", constant_expr(5))]);
-    assert_expected_state(final_state, expected_final_state);
+    assert_solve_result(vec![var("x") - constant(5)], vec![("x", constant_expr(5))]);
 }
 
 #[test]
 fn concretely_solvable() {
-    init_logging();
-    let constraints = [
-        var("a") - constant(2),
-        var("b") - constant(3),
-        // c = a * b = 6
-        var("c") - var("a") * var("b"),
-        // d = c * 4 - a = 22
-        var("d") - (var("c") * constant(4) - var("a")),
-    ]
-    .into_iter()
-    // Reverse to make sure several passes are necessary
-    .rev()
-    .collect();
-
-    let final_state = Solver::new(constraints).solve().unwrap();
-    let expected_final_state = BTreeMap::from([
-        ("a", constant_expr(2)),
-        ("b", constant_expr(3)),
-        ("c", constant_expr(6)),
-        ("d", constant_expr(22)),
-    ]);
-    assert_expected_state(final_state, expected_final_state);
+    assert_solve_result(
+        vec![
+            var("a") - constant(2),
+            var("b") - constant(3),
+            // c = a * b = 6
+            var("c") - var("a") * var("b"),
+            // d = c * 4 - a = 22
+            var("d") - (var("c") * constant(4) - var("a")),
+        ],
+        vec![
+            ("a", constant_expr(2)),
+            ("b", constant_expr(3)),
+            ("c", constant_expr(6)),
+            ("d", constant_expr(22)),
+        ],
+    );
 }
 
 #[test]
 fn symbolically_solvable() {
-    init_logging();
-    let constraints = [
-        // Like above, but this time `a` is only known at runtime
-        var("b") - constant(3),
-        var("c") - known("a") * var("b"),
-        var("d") - (var("c") * constant(4) - known("a")),
-    ]
-    .into_iter()
-    // Reverse to make sure several passes are necessary
-    .rev()
-    .collect();
-
-    let final_state = Solver::new(constraints).solve().unwrap();
-    let expected_final_state = BTreeMap::from([
-        ("b", constant_expr(3)),
-        // TODO: This should simplify to `3 * a`
-        ("c", -constant_expr(3) * (-var_expr("a"))),
-        // TODO: This should simplify to `11 * a`
-        ("d", -var_expr("a") + var_expr("c") * constant_expr(4)),
-    ]);
-    assert_expected_state(final_state, expected_final_state);
+    assert_solve_result(
+        vec![
+            // Like above, but this time `a` is only known at runtime
+            var("b") - constant(3),
+            var("c") - known("a") * var("b"),
+            var("d") - (var("c") * constant(4) - known("a")),
+        ],
+        vec![
+            ("b", constant_expr(3)),
+            // TODO: This should simplify to `3 * a`
+            ("c", -constant_expr(3) * (-var_expr("a"))),
+            // TODO: This should simplify to `11 * a`
+            ("d", -var_expr("a") + var_expr("c") * constant_expr(4)),
+        ],
+    );
 }
--- executor/src/witgen/jit/solver/tests/test_utils.rs
+++ executor/src/witgen/jit/solver/tests/test_utils.rs
@@ -2,15 +2,23 @@ use std::collections::BTreeMap;
 
 use powdr_number::GoldilocksField;
 
-use super::{QuadraticSymbolicExpression, RangeConstraint, SymbolicExpression};
+use crate::witgen::{
+    jit::{
+        quadratic_symbolic_expression::QuadraticSymbolicExpression, solver::Solver,
+        symbolic_expression::SymbolicExpression,
+    },
+    range_constraints::RangeConstraint,
+};
 
-pub type Qse = QuadraticSymbolicExpression<GoldilocksField, &'static str>;
+pub type Var = &'static str;
+pub type Qse = QuadraticSymbolicExpression<GoldilocksField, Var>;
+pub type Expr = SymbolicExpression<GoldilocksField, Var>;
 
-pub fn var(name: &'static str) -> Qse {
+pub fn var(name: Var) -> Qse {
     Qse::from_unknown_variable(name)
 }
 
-pub fn known(name: &'static str) -> Qse {
+pub fn known(name: Var) -> Qse {
     Qse::from_known_symbol(name, RangeConstraint::default())
 }
 
@@ -18,17 +26,37 @@ pub fn constant(value: u64) -> Qse {
     GoldilocksField::from(value).into()
 }
 
-pub fn constant_expr(value: u64) -> SymbolicExpression<GoldilocksField, &'static str> {
+pub fn constant_expr(value: u64) -> SymbolicExpression<GoldilocksField, Var> {
     GoldilocksField::from(value).into()
 }
 
-pub fn var_expr(name: &'static str) -> SymbolicExpression<GoldilocksField, &'static str> {
+pub fn var_expr(name: Var) -> SymbolicExpression<GoldilocksField, Var> {
     SymbolicExpression::Symbol(name, RangeConstraint::default())
 }
 
-pub fn assert_expected_state(
-    final_state: BTreeMap<&'static str, SymbolicExpression<GoldilocksField, &'static str>>,
-    expected_final_state: BTreeMap<&'static str, SymbolicExpression<GoldilocksField, &'static str>>,
+pub fn assert_solve_result(mut constraints: Vec<Qse>, expected_final_state: Vec<(Var, Expr)>) {
+    init_logging();
+
+    // Reverse to make sure several passes are necessary
+    constraints.reverse();
+
+    let final_state = Solver::new(constraints).solve().unwrap();
+    let expected_final_state = expected_final_state.into_iter().collect();
+    assert_expected_state(final_state, expected_final_state);
+}
+
+fn init_logging() {
+    static INIT: std::sync::Once = std::sync::Once::new();
+    INIT.call_once(|| {
+        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("trace"))
+            .is_test(true)
+            .init();
+    });
+}
+
+fn assert_expected_state(
+    final_state: BTreeMap<Var, SymbolicExpression<GoldilocksField, Var>>,
+    expected_final_state: BTreeMap<Var, SymbolicExpression<GoldilocksField, Var>>,
 ) {
     assert_eq!(
         final_state.keys().collect::<Vec<_>>(),
@@ -47,13 +75,4 @@ pub fn assert_expected_state(
         }
     }
     assert!(!error, "Final state does not match expected state");
-}
-
-pub fn init_logging() {
-    static INIT: std::sync::Once = std::sync::Once::new();
-    INIT.call_once(|| {
-        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("trace"))
-            .is_test(true)
-            .init();
-    });
 }
