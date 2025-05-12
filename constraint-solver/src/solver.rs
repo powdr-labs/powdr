06270eafdd37f75079c4aca4c1f3a5136b0b93f1
chriseth <chris@ethereum.org>
2025-05-12 17:19:18 +0200

    Opt: Detect equal columns from quadratic identities. (#2661)
    This derives equality from two constraints that are typically used in
    add-with-carry.

--- constraint-solver/src/solver.rs
+++ constraint-solver/src/solver.rs
@@ -5,6 +5,8 @@ use crate::constraint_system::{
     BusInteractionHandler, ConstraintSystem, DefaultBusInteractionHandler,
 };
 use crate::indexed_constraint_system::IndexedConstraintSystem;
+use crate::quadratic_equivalences;
+use crate::quadratic_symbolic_expression::QuadraticSymbolicExpression;
 use crate::range_constraint::RangeConstraint;
 use crate::utils::known_variables;
 
@@ -85,6 +87,8 @@ impl<T: FieldElement, V: Ord + Clone + Hash + Eq + Display + Debug> Solver<T, V>
             progress |= self.solve_in_isolation()?;
             // Try inferring new information using bus interactions.
             progress |= self.solve_bus_interactions();
+            // Try to find equivalent variables using quadratic constraints.
+            progress |= self.try_solve_quadratic_equivalences();
 
             if !progress {
                 break;
@@ -125,6 +129,22 @@ impl<T: FieldElement, V: Ord + Clone + Hash + Eq + Display + Debug> Solver<T, V>
             progress |= self.apply_effect(effect);
         }
         progress
+    }
+
+    /// Tries to find equivalent variables using quadratic constraints.
+    fn try_solve_quadratic_equivalences(&mut self) -> bool {
+        let equivalences = quadratic_equivalences::find_quadratic_equalities(
+            self.constraint_system.algebraic_constraints(),
+            &self.range_constraints,
+        );
+        for (x, y) in &equivalences {
+            // TODO can we make this work with Effects?
+            self.constraint_system.substitute_by_unknown(
+                y,
+                &QuadraticSymbolicExpression::from_unknown_variable(x.clone()),
+            );
+        }
+        !equivalences.is_empty()
     }
 
     fn apply_effect(&mut self, effect: Effect<T, V>) -> bool {
@@ -179,6 +199,14 @@ impl<T: FieldElement, V> Default for RangeConstraints<T, V> {
 
 impl<T: FieldElement, V: Clone + Hash + Eq> RangeConstraintProvider<T, V>
     for RangeConstraints<T, V>
+{
+    fn get(&self, var: &V) -> RangeConstraint<T> {
+        self.range_constraints.get(var).cloned().unwrap_or_default()
+    }
+}
+
+impl<T: FieldElement, V: Clone + Hash + Eq> RangeConstraintProvider<T, V>
+    for &RangeConstraints<T, V>
 {
     fn get(&self, var: &V) -> RangeConstraint<T> {
         self.range_constraints.get(var).cloned().unwrap_or_default()
