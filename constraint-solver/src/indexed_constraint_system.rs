ff60187b63fb60959328127e72e055af41d9d8a8
chriseth <chris@ethereum.org>
2025-05-12 15:27:40 +0200

    Substitute by unknown indexed (#2700)

--- constraint-solver/src/indexed_constraint_system.rs
+++ constraint-solver/src/indexed_constraint_system.rs
@@ -53,7 +53,7 @@ impl<T: FieldElement, V> IndexedConstraintSystem<T, V> {
 }
 
 impl<T: FieldElement, V: Clone + Hash + Ord + Eq> IndexedConstraintSystem<T, V> {
-    /// Substitutes a variable with a symbolic expression in all algebraic expressions
+    /// Substitutes a variable with a symbolic expression in the whole system
     pub fn substitute_by_known(&mut self, variable: &V, substitution: &SymbolicExpression<T, V>) {
         // Since we substitute by a known value, we do not need to update variable_occurrences.
         for item in self
@@ -62,6 +62,39 @@ impl<T: FieldElement, V: Clone + Hash + Ord + Eq> IndexedConstraintSystem<T, V> 
             .unwrap_or(&Vec::new())
         {
             substitute_by_known_in_item(&mut self.constraint_system, *item, variable, substitution);
+        }
+    }
+
+    /// Substitute an unknown variable by a QuadraticSymbolicExpression in the whole system.
+    ///
+    /// Note this does NOT work properly if the variable is used inside a
+    /// known SymbolicExpression.
+    pub fn substitute_by_unknown(
+        &mut self,
+        variable: &V,
+        substitution: &QuadraticSymbolicExpression<T, V>,
+    ) {
+        let items = self
+            .variable_occurrences
+            .get(variable)
+            .cloned()
+            .unwrap_or(Vec::new());
+        for item in &items {
+            substitute_by_unknown_in_item(
+                &mut self.constraint_system,
+                *item,
+                variable,
+                substitution,
+            );
+        }
+
+        // We just add all variables in the substitution to the items.
+        // It might be that cancellations occur, but we assume it is not worth the overhead.
+        for var in substitution.referenced_unknown_variables().unique() {
+            self.variable_occurrences
+                .entry(var.clone())
+                .or_default()
+                .extend(items.iter().cloned());
         }
     }
 }
@@ -112,5 +145,90 @@ fn substitute_by_known_in_item<T: FieldElement, V: Ord + Clone + Hash + Eq>(
                 .iter_mut()
                 .for_each(|expr| expr.substitute_by_known(variable, substitution));
         }
+    }
+}
+
+fn substitute_by_unknown_in_item<T: FieldElement, V: Ord + Clone + Hash + Eq>(
+    constraint_system: &mut ConstraintSystem<T, V>,
+    item: ConstraintSystemItem,
+    variable: &V,
+    substitution: &QuadraticSymbolicExpression<T, V>,
+) {
+    match item {
+        ConstraintSystemItem::AlgebraicConstraint(i) => {
+            constraint_system.algebraic_constraints[i]
+                .substitute_by_unknown(variable, substitution);
+        }
+        ConstraintSystemItem::BusInteraction(i) => {
+            constraint_system.bus_interactions[i]
+                .iter_mut()
+                .for_each(|expr| expr.substitute_by_unknown(variable, substitution));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use powdr_number::GoldilocksField;
+
+    use super::*;
+
+    fn format_system(s: &IndexedConstraintSystem<GoldilocksField, &'static str>) -> String {
+        format!(
+            "{}  |  {}",
+            s.algebraic_constraints().iter().format("  |  "),
+            s.bus_interactions()
+                .iter()
+                .map(
+                    |BusInteraction {
+                         bus_id,
+                         payload,
+                         multiplicity,
+                     }| format!(
+                        "{bus_id}: {multiplicity} * [{}]",
+                        payload.iter().format(", ")
+                    )
+                )
+                .format("  |  ")
+        )
+    }
+
+    #[test]
+    fn substitute_by_unknown() {
+        type Qse = QuadraticSymbolicExpression<GoldilocksField, &'static str>;
+        let x = Qse::from_unknown_variable("x");
+        let y = Qse::from_unknown_variable("y");
+        let z = Qse::from_unknown_variable("z");
+        let mut s: IndexedConstraintSystem<_, _> = ConstraintSystem {
+            algebraic_constraints: vec![
+                x.clone() + y.clone(),
+                x.clone() - z.clone(),
+                y.clone() - z.clone(),
+            ],
+            bus_interactions: vec![BusInteraction {
+                bus_id: x,
+                payload: vec![y.clone(), z],
+                multiplicity: y,
+            }],
+        }
+        .into();
+
+        s.substitute_by_unknown(&"x", &Qse::from_unknown_variable("z"));
+
+        assert_eq!(
+            format_system(&s),
+            "y + z  |  0  |  y + -z  |  z: y * [y, z]"
+        );
+
+        s.substitute_by_unknown(
+            &"z",
+            &(Qse::from_unknown_variable("x")
+                + Qse::from(SymbolicExpression::from(GoldilocksField::from(7)))),
+        );
+
+        assert_eq!(
+            format_system(&s),
+            "x + y + 7  |  0  |  -x + y + -7  |  x + 7: y * [y, x + 7]"
+        );
     }
 }
