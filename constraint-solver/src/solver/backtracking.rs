881c96548daaf0ac23153921e7c0ea467b8ac6e1
Georg Wiese <georgwiese@gmail.com>
2025-05-13 21:26:27 +0200

    Polish

--- constraint-solver/src/solver/backtracking.rs
+++ constraint-solver/src/solver/backtracking.rs
@@ -25,9 +25,47 @@ impl<'a, T: FieldElement, V> Backtracker<'a, T, V> {
 
 impl<'a, T: FieldElement, V: Ord + Clone + Hash + Eq + Display + Debug> Backtracker<'a, T, V> {
     pub fn get_unique_assignments(&self) -> Result<BTreeMap<V, T>, Error> {
-        log::debug!("Trying backtracking with maximum width {MAX_BACKTRACKING_WIDTH}");
-        let candidates = self
-            .solver
+        log::debug!("Starting backtracking with maximum width {MAX_BACKTRACKING_WIDTH}");
+        let candidates = self.get_brute_force_candidates();
+
+        log::debug!(
+            "Found {} sets of variables with few possible assignments. Checking each set...",
+            candidates.len()
+        );
+
+        let all_assignments = candidates
+            .iter()
+            .map(|assignment_candidates| self.find_unique_assignment(assignment_candidates))
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .filter_map(|assignment| assignment)
+            .collect::<Vec<_>>();
+
+        log::debug!(
+            "{} variable sets with unique assignments found",
+            all_assignments.len()
+        );
+
+        let mut result = BTreeMap::new();
+        for (variable, value) in all_assignments.iter().flatten() {
+            if let Some(old_value) = result.insert(variable.clone(), *value) {
+                if old_value != *value {
+                    // Two assignments contradict each other.
+                    return Err(Error::BacktrackingError);
+                }
+            }
+        }
+
+        log::debug!("Total assignments: {}", result.len());
+        for (variable, value) in &result {
+            log::trace!("  {variable} = {value}");
+        }
+
+        Ok(result)
+    }
+
+    fn get_brute_force_candidates(&self) -> Vec<BTreeSet<V>> {
+        self.solver
             .constraint_system
             .iter()
             .map(|expression| {
@@ -38,70 +76,61 @@ impl<'a, T: FieldElement, V: Ord + Clone + Hash + Eq + Display + Debug> Backtrac
                     .collect::<BTreeSet<_>>()
             })
             .unique()
-            .filter_map(|variables| {
-                // Consider all sets of variables that appear together in an expression.
-                let variables = variables.into_iter().collect::<Vec<_>>();
-                self.get_possible_assignments_if_few(&variables)
-                    .map(|candidates| candidates.collect::<Vec<_>>())
-            })
-            .collect_vec();
+            .filter(|variables| !variables.is_empty())
+            .filter(|variables| self.has_few_possible_assignments(variables.iter().cloned()))
+            .collect()
+    }
 
-        log::debug!(
-            "Found {} sets of variables with few possible assignments",
-            candidates.len()
-        );
-        for c in &candidates {
-            log::trace!("  {} assignments for:", c.len());
-            for v in c[0].keys() {
-                log::trace!("    {v}");
-            }
-        }
-
-        let all_assignments = candidates
+    /// Returns true if the given range constraints allow for at most `MAX_BACKTRACKING_WIDTH` possible assignments,
+    /// based on the range widths of the constraints.
+    fn has_few_possible_assignments(&self, variables: impl Iterator<Item = V>) -> bool {
+        self.range_constraints(variables)
             .iter()
-            .filter_map(|assignment_candidates| self.find_unique_assignment(assignment_candidates))
-            .flatten();
+            .map(|rc| rc.range_width().try_into_u64())
+            .collect::<Option<Vec<_>>>()
+            .and_then(|widths| widths.iter().try_fold(1u64, |acc, &x| acc.checked_mul(x)))
+            .map(|total_width| total_width < MAX_BACKTRACKING_WIDTH)
+            .unwrap_or(false)
+    }
 
-        let mut result = BTreeMap::new();
-        for (variable, value) in all_assignments {
-            if let Some(old_value) = result.insert(variable.clone(), *value) {
-                if old_value != *value {
-                    return Err(Error::BacktrackingError);
+    fn range_constraints(&self, variables: impl Iterator<Item = V>) -> Vec<RangeConstraint<T>> {
+        variables
+            .map(|v| self.solver.range_constraints.get(&v))
+            .collect()
+    }
+
+    /// Checks each assignment in the given list of candidates. If exactly one assignment satisfies the
+    /// constraint system (and all others lead to a contradiction), it returns that assignment.
+    /// If multiple assignments satisfy the constraint system, it returns `None`.
+    fn find_unique_assignment(
+        &self,
+        variables: &BTreeSet<V>,
+    ) -> Result<Option<BTreeMap<V, T>>, Error> {
+        let mut assignment = None;
+        for assignments in self.get_all_possible_assignments(variables) {
+            if self.solver.check_assignments(&assignments) {
+                if assignment.is_some() {
+                    // The assignment is not unique.
+                    return Ok(None);
                 }
+                assignment = Some(assignments);
             }
         }
-
-        log::debug!("Backtracking found {} assignments", result.len());
-        for (variable, value) in &result {
-            log::trace!("  {variable} = {value}");
+        if assignment.is_none() {
+            // No assignment satisfied the constraint system.
+            return Err(Error::BacktrackingError);
         }
-
-        Ok(result)
+        Ok(assignment)
     }
 
     /// Returns an iterator over all possible assignments for the given variables, if the number of
     /// possible assignments is nonzero and smaller than `MAX_BACKTRACKING_WIDTH`. Otherwise, returns None.
-    fn get_possible_assignments_if_few(
+    fn get_all_possible_assignments(
         &self,
-        variables: &[V],
-    ) -> Option<impl Iterator<Item = BTreeMap<V, T>>> {
-        if variables.is_empty() {
-            return None;
-        }
-        if variables.len() > MAX_BACKTRACKING_WIDTH.ilog2() as usize {
-            // Each variable can have at least 2 values (otherwise it would have a concrete solution),
-            // so the number of combinations is guaranteed to be larger than MAX_BACKTRACKING_DEPTH.
-            return None;
-        }
-        let range_constraints = variables
-            .iter()
-            .map(|v| self.solver.range_constraints.get(v))
-            .collect::<Vec<_>>();
-        if !self.has_few_possible_assignments(&range_constraints) {
-            return None;
-        }
-
-        let assignments = range_constraints
+        variables: &BTreeSet<V>,
+    ) -> impl Iterator<Item = BTreeMap<V, T>> {
+        let assignments = self
+            .range_constraints(variables.iter().cloned())
             .iter()
             .map(|rc| {
                 let (min, _) = rc.range();
@@ -114,61 +143,16 @@ impl<'a, T: FieldElement, V: Ord + Clone + Hash + Eq + Display + Debug> Backtrac
             .multi_cartesian_product()
             .collect::<Vec<_>>();
 
-        Some(
-            assignments
-                .into_iter()
-                .map(|assignment| {
-                    let mut assignments = BTreeMap::new();
-                    for (variable, value) in variables.iter().zip(assignment) {
-                        assignments.insert(variable.clone(), T::from(value));
-                    }
-                    assignments
-                })
-                .collect::<Vec<_>>()
-                .into_iter(),
-        )
-    }
-
-    /// Returns true if the given range constraints allow for at most `MAX_BACKTRACKING_WIDTH` possible assignments,
-    /// based on the range widths of the constraints.
-    fn has_few_possible_assignments(&self, range_constraints: &[RangeConstraint<T>]) -> bool {
-        let widths = range_constraints
-            .iter()
-            .map(|rc| rc.range_width().try_into_u64())
-            .collect::<Option<Vec<_>>>();
-
-        if let Some(widths) = widths {
-            widths
-                .iter()
-                .try_fold(1u64, |acc, &x| acc.checked_mul(x))
-                .map(|total_width| total_width < MAX_BACKTRACKING_WIDTH)
-                .unwrap_or(false)
-        } else {
-            false
-        }
-    }
-
-    /// Checks each assignment in the given list of candidates. If exactly one assignment satisfies the
-    /// constraint system (and all others lead to a contradiction), it returns that assignment.
-    /// If multiple assignments satisfy the constraint system, it returns `None`.
-    fn find_unique_assignment(
-        &self,
-        assignment_candidates: &'a [BTreeMap<V, T>],
-    ) -> Option<&'a BTreeMap<V, T>> {
-        let mut assignment = None;
-        for assignments in assignment_candidates {
-            if self.solver.check_assignments(assignments) {
-                match assignment {
-                    None => {
-                        assignment = Some(assignments);
-                    }
-                    Some(_) => {
-                        // The assignment is not unique.
-                        return None;
-                    }
+        assignments
+            .into_iter()
+            .map(|assignment| {
+                let mut assignments = BTreeMap::new();
+                for (variable, value) in variables.iter().zip(assignment) {
+                    assignments.insert(variable.clone(), T::from(value));
                 }
-            }
-        }
-        assignment
+                assignments
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
     }
 }
