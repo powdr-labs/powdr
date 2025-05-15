0cce511e5c36a223b15b2459b87d64c4718720b4
chriseth <chriseth.github@gmail.com>
2025-05-15 18:10:26 +0000

    test

---
+++ autoprecompiles/tests/optimizer.rs
@@ -0,0 +1,13 @@
+use itertools::Itertools;
+use powdr_autoprecompiles::SymbolicMachine;
+use powdr_number::BabyBearField;
+use powdr_pilopt::qse_opt::algebraic_to_quadratic_symbolic_expression;
+
+#[test]
+fn load_machine_cbor() {
+    let file = std::fs::File::open("tests/machine_post_opt.cbor").unwrap();
+    let reader = std::io::BufReader::new(file);
+    let machine: SymbolicMachine<BabyBearField> = serde_cbor::from_reader(reader).unwrap();
+    assert_eq!(machine.constraints.len(), 6770);
+    assert_eq!(machine.bus_interactions.len(), 3573);
+}
