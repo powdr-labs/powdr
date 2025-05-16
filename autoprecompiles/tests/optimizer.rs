52b7e639b7487f2395751d4e7bdff2a9ef042eda
chriseth <chriseth.github@gmail.com>
2025-05-16 10:20:51 +0000

    rename file.

--- autoprecompiles/tests/optimizer.rs
+++ autoprecompiles/tests/optimizer.rs
@@ -3,7 +3,7 @@ use powdr_number::BabyBearField;
 
 #[test]
 fn load_machine_cbor() {
-    let file = std::fs::File::open("tests/machine_post_opt.cbor").unwrap();
+    let file = std::fs::File::open("tests/keccak_apc_post_opt.cbor").unwrap();
     let reader = std::io::BufReader::new(file);
     let machine: SymbolicMachine<BabyBearField> = serde_cbor::from_reader(reader).unwrap();
     assert_eq!(machine.constraints.len(), 6770);
