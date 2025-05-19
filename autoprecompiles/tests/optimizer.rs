7c062e050682171599d327d9d7d561dcd32c44ac
chriseth <chriseth.github@gmail.com>
2025-05-19 15:52:15 +0000

    update after fix.

--- autoprecompiles/tests/optimizer.rs
+++ autoprecompiles/tests/optimizer.rs
@@ -20,8 +20,8 @@ fn load_machine_cbor() {
     let file = std::fs::File::open("tests/keccak_apc_post_opt.cbor").unwrap();
     let reader = std::io::BufReader::new(file);
     let machine: SymbolicMachine<BabyBearField> = serde_cbor::from_reader(reader).unwrap();
-    assert_eq!(machine.constraints.len(), 6770);
-    assert_eq!(machine.bus_interactions.len(), 3573);
+    assert_eq!(machine.constraints.len(), 506);
+    assert_eq!(machine.bus_interactions.len(), 6485);
 }
 
 #[test]
