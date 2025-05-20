bdda605e7bb3f28806ff1dcf4ffd1d9914bfeec9
chriseth <chris@ethereum.org>
2025-05-20 18:18:31 +0200

    Extract register optimizer. (#2737)
    Co-authored-by: Georg Wiese <georgwiese@gmail.com>

--- autoprecompiles/src/lib.rs
+++ autoprecompiles/src/lib.rs
@@ -2,12 +2,12 @@ use itertools::Itertools;
 use optimizer::{optimize, IsBusStateful};
 use powdr::{Column, UniqueColumns};
 use powdr_ast::analyzed::{
-    AlgebraicBinaryOperation, AlgebraicBinaryOperator, AlgebraicExpression, AlgebraicReference,
-    AlgebraicUnaryOperator,
+    AlgebraicBinaryOperation, AlgebraicExpression, AlgebraicReference, AlgebraicUnaryOperator,
 };
 use powdr_ast::analyzed::{PolyID, PolynomialType};
 use powdr_ast::parsed::visitor::Children;
 use powdr_constraint_solver::constraint_system::BusInteractionHandler;
+use register_optimizer::optimize_register_operations;
 use serde::{Deserialize, Serialize};
 use std::collections::{BTreeMap, BTreeSet};
 use std::fmt::Display;
@@ -18,6 +18,7 @@ use powdr_pilopt::simplify_expression;
 
 pub mod optimizer;
 pub mod powdr;
+pub mod register_optimizer;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct SymbolicInstructionStatement<T> {
@@ -329,7 +330,7 @@ impl<T: FieldElement> Autoprecompiles<T> {
         assert!(check_precompile(&machine));
         let machine = remove_zero_mult(machine);
 
-        let machine = optimize_precompile(machine);
+        let machine = optimize_register_operations(machine);
         assert!(check_precompile(&machine));
 
         // Fixpoint style re-attempt.
@@ -545,120 +546,6 @@ pub fn check_precompile<T: FieldElement>(machine: &SymbolicMachine<T>) -> bool {
         });
 
     count_per_addr.values().all(|&v| v % 2 == 0)
-}
-
-pub fn optimize_precompile<T: FieldElement>(mut machine: SymbolicMachine<T>) -> SymbolicMachine<T> {
-    let mut receive = true;
-    let mut local_reg_mem: BTreeMap<u32, Vec<AlgebraicExpression<T>>> = BTreeMap::new();
-    let mut new_constraints: Vec<SymbolicConstraint<T>> = Vec::new();
-    let mut to_remove: BTreeSet<usize> = Default::default();
-    let mut last_store: BTreeMap<u32, usize> = BTreeMap::new();
-    machine
-        .bus_interactions
-        .iter()
-        .enumerate()
-        .for_each(|(i, bus_int)| {
-            let mem_int: MemoryBusInteraction<T> = match bus_int.clone().try_into() {
-                Ok(mem_int) => mem_int,
-                Err(_) => {
-                    return;
-                }
-            };
-
-            if !matches!(mem_int.ty, MemoryType::Register) {
-                return;
-            }
-
-            let addr = match mem_int.try_addr_u32() {
-                None => {
-                    panic!(
-                        "Register memory access must have constant address but found {}",
-                        mem_int.addr
-                    );
-                }
-                Some(addr) => addr,
-            };
-
-            if receive {
-                match local_reg_mem.get(&addr) {
-                    Some(data) => {
-                        assert_eq!(data.len(), mem_int.data.len());
-                        mem_int
-                            .data
-                            .iter()
-                            .zip_eq(data.iter())
-                            .for_each(|(new_data, old_data)| {
-                                let eq_expr = AlgebraicExpression::new_binary(
-                                    new_data.clone(),
-                                    AlgebraicBinaryOperator::Sub,
-                                    old_data.clone(),
-                                );
-                                new_constraints.push(eq_expr.into());
-                            });
-
-                        to_remove.insert(i);
-                    }
-                    None => {
-                        local_reg_mem.insert(addr, mem_int.data.clone());
-                    }
-                }
-            } else {
-                last_store.insert(addr, i);
-                local_reg_mem.insert(addr, mem_int.data.clone());
-            }
-
-            receive = !receive;
-        });
-
-    let mut receive = true;
-    machine.bus_interactions = machine
-        .bus_interactions
-        .into_iter()
-        .enumerate()
-        .filter_map(|(i, bus_int)| {
-            let mem_int: MemoryBusInteraction<T> = match bus_int.clone().try_into() {
-                Ok(mem_int) => mem_int,
-                Err(_) => {
-                    return Some(bus_int);
-                }
-            };
-
-            if !matches!(mem_int.ty, MemoryType::Register) {
-                return Some(bus_int);
-            }
-
-            let addr = match mem_int.try_addr_u32() {
-                None => {
-                    panic!(
-                        "Register memory access must have constant address but found {}",
-                        mem_int.addr
-                    );
-                }
-                Some(addr) => addr,
-            };
-
-            let keep = if receive && !to_remove.contains(&i) {
-                Some(bus_int)
-            } else if receive && to_remove.contains(&i) {
-                None
-            } else if last_store
-                .get(&addr)
-                .is_some_and(|&last_index| last_index == i)
-            {
-                Some(bus_int)
-            } else {
-                None
-            };
-
-            receive = !receive;
-
-            keep
-        })
-        .collect();
-
-    machine.constraints.extend(new_constraints);
-
-    machine
 }
 
 pub fn optimize_pc_lookup<T: FieldElement>(
