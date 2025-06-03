968817258bbc48ae417428614d7bfdf275e7254b
Georg Wiese <georgwiese@gmail.com>
2025-06-03 20:04:34 +0200

    Add legacy_expression

---
+++ autoprecompiles/src/legacy_expression.rs
@@ -0,0 +1,73 @@
+use powdr_expression::AlgebraicExpression as ActualAlgebraicExpression;
+use serde::{Deserialize, Serialize};
+use std::hash::{Hash, Hasher};
+
+pub type AlgebraicExpression<T> = ActualAlgebraicExpression<T, AlgebraicReference>;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub enum PolynomialType {
+    Committed = 0,
+    Constant,
+    Intermediate,
+}
+
+#[derive(Debug, Copy, Clone, PartialOrd, Ord, PartialEq, Eq, Serialize, Deserialize)]
+pub struct PolyID {
+    pub id: u64,
+    pub ptype: PolynomialType,
+}
+
+impl Hash for PolyID {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        // single call to hash is faster
+        ((self.id << 2) + self.ptype as u64).hash(state);
+    }
+}
+
+#[derive(Debug, Clone, Eq, Serialize, Deserialize)]
+pub struct AlgebraicReference {
+    /// Name of the polynomial - just for informational purposes.
+    /// Comparisons are based on polynomial ID.
+    /// In case of an array element, this ends in `[i]`.
+    pub name: String,
+    /// Identifier for a polynomial reference, already contains
+    /// the element offset in case of an array element.
+    pub poly_id: PolyID,
+    pub next: bool,
+}
+
+impl AlgebraicReference {
+    #[inline]
+    pub fn is_witness(&self) -> bool {
+        self.poly_id.ptype == PolynomialType::Committed
+    }
+    #[inline]
+    pub fn is_fixed(&self) -> bool {
+        self.poly_id.ptype == PolynomialType::Constant
+    }
+}
+
+impl PartialOrd for AlgebraicReference {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for AlgebraicReference {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        (&self.poly_id, &self.next).cmp(&(&other.poly_id, &other.next))
+    }
+}
+
+impl PartialEq for AlgebraicReference {
+    fn eq(&self, other: &Self) -> bool {
+        self.poly_id == other.poly_id && self.next == other.next
+    }
+}
+
+impl Hash for AlgebraicReference {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.poly_id.hash(state);
+        self.next.hash(state);
+    }
+}
