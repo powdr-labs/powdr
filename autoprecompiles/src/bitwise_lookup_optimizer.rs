32b1552576104d1b02c0195d604d0bb4bcb56bde
chriseth <chriseth.github@gmail.com>
2025-06-03 13:17:25 +0000

    cleanup

--- autoprecompiles/src/bitwise_lookup_optimizer.rs
+++ autoprecompiles/src/bitwise_lookup_optimizer.rs
@@ -11,26 +11,23 @@ use crate::{SymbolicBusInteraction, SymbolicConstraint, SymbolicMachine, BITWISE
 pub fn optimize_bitwise_lookup<T: FieldElement>(
     mut machine: SymbolicMachine<T>,
 ) -> SymbolicMachine<T> {
-    // Expressions that we need to byte-constrain at the edn.
+    // Expressions that we need to byte-constrain at the end.
     let mut to_byte_constrain = vec![];
     // Bus interaction indices we will remove.
     let mut to_remove = vec![];
     // New constraints (mainly substitutions) we will add.
     let mut new_constraints: Vec<SymbolicConstraint<T>> = vec![];
     for (index, bus_int) in machine.bus_interactions.iter().enumerate() {
+        if bus_int.id != BITWISE_LOOKUP_BUS_ID || bus_int.mult != T::from(1).into() {
+            continue;
+        }
         // See: https://github.com/openvm-org/openvm/blob/v1.0.0/crates/circuits/primitives/src/bitwise_op_lookup/bus.rs
         // Expects (x, y, z, op), where:
         // - if op == 0, then x and y are bytes and z = 0
         // - if op == 1, then x and y are bytes and z = x ^ y
-        if bus_int.id != BITWISE_LOOKUP_BUS_ID {
-            continue;
-        }
         let [x, y, z, op] = &bus_int.args[..] else {
             panic!();
         };
-        if bus_int.mult != T::from(1).into() {
-            continue;
-        }
 
         let AlgebraicExpression::Number(op) = op else {
             continue;
@@ -48,20 +45,15 @@ pub fn optimize_bitwise_lookup<T: FieldElement>(
             }
         } else if op == &1.into() {
             // The bus interaction is equivalent to "x, y and z are bytes and z = x ^ y".
-            if x == &T::from(0).into() {
-                // y = z and both are bytes.
-                new_constraints.push((y.clone() - z.clone()).into());
-                to_byte_constrain.push(y.clone());
-                to_remove.push(index);
-            } else if y == &T::from(0).into() {
-                // x = z and both are bytes.
-                new_constraints.push((x.clone() - z.clone()).into());
-                to_byte_constrain.push(x.clone());
-                to_remove.push(index);
-            } else if z == &T::from(0).into() {
-                // x = y are both are bytes.
-                new_constraints.push((x.clone() - y.clone()).into());
-                to_byte_constrain.push(x.clone());
+
+            // If any argument is zero, the other two have to be equal.
+            let mut args = vec![x, y, z];
+            if let Some(zero_pos) = args.iter().position(|e| *e == &T::from(0).into()) {
+                args.remove(zero_pos);
+                // The two remaning expressions in args are equal and bytes.
+                let [a, b] = args.try_into().unwrap();
+                new_constraints.push((a.clone() - b.clone()).into());
+                to_byte_constrain.push(a.clone());
                 to_remove.push(index);
             }
         } else {
