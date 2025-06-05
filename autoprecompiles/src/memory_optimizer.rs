69179bc32b9d63f96bcb09c4fe67ee8bc026ac84
chriseth <chriseth.github@gmail.com>
2025-06-05 14:38:21 +0000

    cleanup

--- autoprecompiles/src/memory_optimizer.rs
+++ autoprecompiles/src/memory_optimizer.rs
@@ -14,7 +14,9 @@ use powdr_constraint_solver::quadratic_symbolic_expression::QuadraticSymbolicExp
 use powdr_constraint_solver::quadratic_symbolic_expression::RangeConstraintProvider;
 use powdr_constraint_solver::range_constraint::RangeConstraint;
 use powdr_constraint_solver::utils::possible_concrete_values;
-use powdr_number::{{FieldElement, LargeInt}, LargeInt};
+use powdr_number::{
+    LargeInt, {FieldElement, LargeInt},
+};
 
 use crate::{SymbolicConstraint, MEMORY_BUS_ID};
 
@@ -79,182 +81,6 @@ pub fn check_register_operation_consistency<T: FieldElement, V: Clone + Ord + Di
         });
 
     count_per_addr.values().all(|&v| v % 2 == 0)
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub enum MemoryType {
-    Constant,
-    Register,
-    Memory,
-    Native,
-}
-
-impl<T: FieldElement> From<AlgebraicExpression<T>> for MemoryType {
-    fn from(expr: AlgebraicExpression<T>) -> Self {
-        match expr {
-            AlgebraicExpression::Number(n) => {
-                let n_u32 = n.to_integer().try_into_u32().unwrap();
-                match n_u32 {
-                    0 => MemoryType::Constant,
-                    1 => MemoryType::Register,
-                    2 => MemoryType::Memory,
-                    3 => MemoryType::Native,
-                    _ => unreachable!("Expected 0, 1, 2 or 3 but got {n}"),
-                }
-            }
-            _ => unreachable!("Expected number"),
-        }
-    }
-}
-
-impl<T: FieldElement> From<MemoryType> for AlgebraicExpression<T> {
-    fn from(ty: MemoryType) -> Self {
-        match ty {
-            MemoryType::Constant => AlgebraicExpression::Number(T::from(0u32)),
-            MemoryType::Register => AlgebraicExpression::Number(T::from(1u32)),
-            MemoryType::Memory => AlgebraicExpression::Number(T::from(2u32)),
-            MemoryType::Native => AlgebraicExpression::Number(T::from(3u32)),
-        }
-    }
-}
-
-/// Returns the word size of a particularly memory type.
-/// Word size `k` means that an address `x` and an address `x + k` are guaranteed to be
-/// non-overlapping, it is not necessarily related to what is stored, rather
-/// how memory is addressed.
-fn word_size_by_memory(ty: MemoryType) -> Option<u32> {
-    match ty {
-        MemoryType::Register | MemoryType::Memory => Some(4),
-        MemoryType::Constant | MemoryType::Native => None, // Let's not optimize this.
-    }
-}
-
-#[derive(Clone, Debug)]
-enum MemoryOp {
-    Send,
-    Receive,
-}
-
-#[derive(Clone, Debug)]
-struct MemoryBusInteraction<T> {
-    ty: MemoryType,
-    op: MemoryOp,
-    addr: AlgebraicExpression<T>,
-    data: Vec<AlgebraicExpression<T>>,
-}
-
-impl<T: FieldElement> MemoryBusInteraction<T> {
-    fn try_addr_u32(&self) -> Option<u32> {
-        match self.addr {
-            AlgebraicExpression::Number(n) => n.to_integer().try_into_u32(),
-            _ => None,
-        }
-    }
-}
-
-impl<T: FieldElement> MemoryBusInteraction<T> {
-    /// Tries to convert a `SymbolicBusInteraction` to a `MemoryBusInteraction`.
-    ///
-    /// Returns `Ok(None)` if we know that the bus interaction is not a memory bus interaction.
-    /// Returns `Err(_)` if the bus interaction is a memory bus interaction but could not be converted properly
-    /// (usually because the multiplicity is not -1 or 1).
-    /// Otherwise returns `Ok(Some(memory_bus_interaction))`
-    fn try_from_symbolic_bus_interaction(
-        bus_interaction: &SymbolicBusInteraction<T>,
-    ) -> Result<Option<Self>, ()> {
-        if bus_interaction.id != MEMORY_BUS_ID {
-            return Ok(None);
-        }
-        // TODO: Timestamp is ignored, we could use it to assert that the bus interactions
-        // are in the right order.
-        let ty = bus_interaction.args[0].clone().into();
-        let op = match bus_interaction.try_multiplicity_to_number() {
-            Some(n) if n == 1.into() => MemoryOp::Send,
-            Some(n) if n == (-1).into() => MemoryOp::Receive,
-            _ => return Err(()),
-        };
-        let addr = bus_interaction.args[1].clone();
-        let data = bus_interaction.args[2..bus_interaction.args.len() - 1].to_vec();
-        Ok(Some(MemoryBusInteraction { ty, op, addr, data }))
-    }
-}
-
-/// Tries to find indices of bus interactions that can be removed in the given machine
-/// and also returns a set of new constraints to be added.
-fn redundant_memory_interactions_indices<T: FieldElement, V: Hash + Eq + Clone + Ord + Display>(
-    system: &ConstraintSystem<T, V>,
-    range_constraints: impl RangeConstraintProvider<T, V> + Clone,
-) -> (Vec<usize>, Vec<QuadraticSymbolicExpression<T, V>>) {
-    let address_comparator = MemoryAddressComparator::new(system);
-    let mut new_constraints: Vec<QuadraticSymbolicExpression<T, V>> = Vec::new();
-
-    // Address across all memory types.
-    type GlobalAddress<T, V> = (MemoryType, QuadraticSymbolicExpression<T, V>);
-    // Track memory contents by memory type while we go through bus interactions.
-    // This maps an address to the index of the previous send on that address and the
-    // data currently stored there.
-    let mut memory_contents: HashMap<
-        GlobalAddress<T, V>,
-        (usize, Vec<QuadraticSymbolicExpression<_, _>>),
-    > = Default::default();
-    let mut to_remove: Vec<usize> = Default::default();
-
-    // TODO we assume that memory interactions are sorted by timestamp.
-    for (index, bus_int) in system.bus_interactions.iter().enumerate() {
-        let mem_int = match MemoryBusInteraction::try_from_bus_interaction(bus_int) {
-            Ok(Some(mem_int)) => mem_int,
-            Ok(None) => continue,
-            Err(_) => {
-                // This interaction might be going to memory, but we do not know
-                // the multiplicity. Delete all knowledge.
-                // TODO If we can still clearly determine the memory type, we could
-                // only clear the knowledge for that memory type.
-                memory_contents.clear();
-                continue;
-            }
-        };
-        let Some(word_size) = word_size_by_memory(mem_int.ty) else {
-            continue;
-        };
-
-        let addr = (mem_int.ty, mem_int.addr.clone());
-
-        match mem_int.op {
-            MemoryOp::Receive => {
-                // If there is an unconsumed send to this address, consume it.
-                // In that case, we can replace both bus interactions with equality constraints
-                // between the data that would have been sent and received.
-                if let Some((previous_send, existing_values)) = memory_contents.remove(&addr) {
-                    for (existing, new) in existing_values.iter().zip_eq(mem_int.data.iter()) {
-                        new_constraints.push((existing.clone() - new.clone()));
-                    }
-                    to_remove.extend([index, previous_send]);
-                }
-            }
-            MemoryOp::Send => {
-                // We can only retain knowledge about addresses where we can prove
-                // that this send operation does not interfere with it, i.e.
-                // if we can prove that the two addresses differ by at least a word size.
-                memory_contents.retain(|other_addr, _| {
-                    address_comparator.are_addrs_known_to_be_different_by_word(
-                        &addr,
-                        other_addr,
-                        word_size,
-                        range_constraints.clone(),
-                    )
-                });
-                memory_contents.insert(addr.clone(), (index, mem_int.data.clone()));
-            }
-        }
-    }
-
-    log::debug!(
-        "Removing {} memory interactions and adding {} new constraints",
-        to_remove.len(),
-        new_constraints.len()
-    );
-
-    (to_remove, new_constraints)
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
@@ -337,6 +163,84 @@ impl<T: FieldElement, V: Ord + Clone + Eq> MemoryBusInteraction<T, V> {
         let data = bus_interaction.payload[2..].to_vec();
         Ok(Some(MemoryBusInteraction { ty, op, addr, data }))
     }
+}
+
+/// Tries to find indices of bus interactions that can be removed in the given machine
+/// and also returns a set of new constraints to be added.
+fn redundant_memory_interactions_indices<T: FieldElement, V: Hash + Eq + Clone + Ord + Display>(
+    system: &ConstraintSystem<T, V>,
+    range_constraints: impl RangeConstraintProvider<T, V> + Clone,
+) -> (Vec<usize>, Vec<QuadraticSymbolicExpression<T, V>>) {
+    let address_comparator = MemoryAddressComparator::new(system);
+    let mut new_constraints: Vec<QuadraticSymbolicExpression<T, V>> = Vec::new();
+
+    // Address across all memory types.
+    type GlobalAddress<T, V> = (MemoryType, QuadraticSymbolicExpression<T, V>);
+    // Track memory contents by memory type while we go through bus interactions.
+    // This maps an address to the index of the previous send on that address and the
+    // data currently stored there.
+    let mut memory_contents: HashMap<
+        GlobalAddress<T, V>,
+        (usize, Vec<QuadraticSymbolicExpression<_, _>>),
+    > = Default::default();
+    let mut to_remove: Vec<usize> = Default::default();
+
+    // TODO we assume that memory interactions are sorted by timestamp.
+    for (index, bus_int) in system.bus_interactions.iter().enumerate() {
+        let mem_int = match MemoryBusInteraction::try_from_bus_interaction(bus_int) {
+            Ok(Some(mem_int)) => mem_int,
+            Ok(None) => continue,
+            Err(_) => {
+                // This interaction might be going to memory, but we do not know
+                // the multiplicity. Delete all knowledge.
+                // TODO If we can still clearly determine the memory type, we could
+                // only clear the knowledge for that memory type.
+                memory_contents.clear();
+                continue;
+            }
+        };
+        let Some(word_size) = word_size_by_memory(mem_int.ty) else {
+            continue;
+        };
+
+        let addr = (mem_int.ty, mem_int.addr.clone());
+
+        match mem_int.op {
+            MemoryOp::Receive => {
+                // If there is an unconsumed send to this address, consume it.
+                // In that case, we can replace both bus interactions with equality constraints
+                // between the data that would have been sent and received.
+                if let Some((previous_send, existing_values)) = memory_contents.remove(&addr) {
+                    for (existing, new) in existing_values.iter().zip_eq(mem_int.data.iter()) {
+                        new_constraints.push((existing.clone() - new.clone()));
+                    }
+                    to_remove.extend([index, previous_send]);
+                }
+            }
+            MemoryOp::Send => {
+                // We can only retain knowledge about addresses where we can prove
+                // that this send operation does not interfere with it, i.e.
+                // if we can prove that the two addresses differ by at least a word size.
+                memory_contents.retain(|other_addr, _| {
+                    address_comparator.are_addrs_known_to_be_different_by_word(
+                        &addr,
+                        other_addr,
+                        word_size,
+                        range_constraints.clone(),
+                    )
+                });
+                memory_contents.insert(addr.clone(), (index, mem_int.data.clone()));
+            }
+        }
+    }
+
+    log::debug!(
+        "Removing {} memory interactions and adding {} new constraints",
+        to_remove.len(),
+        new_constraints.len()
+    );
+
+    (to_remove, new_constraints)
 }
 
 struct MemoryAddressComparator<T: FieldElement, V> {
