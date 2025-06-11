77e114fd05e49b8a2b0e3cc72798d301d9f9c2f3
chriseth <chriseth.github@gmail.com>
2025-06-11 14:15:32 +0000

    Improve formatting.

--- constraint-solver/src/quadratic_symbolic_expression.rs
+++ constraint-solver/src/quadratic_symbolic_expression.rs
@@ -924,34 +924,64 @@ impl<T: FieldElement, V: Clone + Ord + Hash + Eq> Mul for QuadraticSymbolicExpre
 
 impl<T: FieldElement, V: Clone + Ord + Display> Display for QuadraticSymbolicExpression<T, V> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let formatted = self
-            .quadratic
+        let (sign, s) = self.to_signed_string();
+        if sign {
+            write!(f, "-({s})")
+        } else {
+            write!(f, "{s}")
+        }
+    }
+}
+
+impl<T: FieldElement, V: Clone + Ord + Display> QuadraticSymbolicExpression<T, V> {
+    fn to_signed_string(&self) -> (bool, String) {
+        self.quadratic
             .iter()
-            .map(|(a, b)| format!("({a}) * ({b})"))
+            .map(|(a, b)| {
+                let (a_sign, a) = a.to_signed_string();
+                let (b_sign, b) = b.to_signed_string();
+                (a_sign ^ b_sign, format!("({a}) * ({b})"))
+            })
             .chain(
                 self.linear
                     .iter()
                     .map(|(var, coeff)| match coeff.try_to_number() {
-                        Some(k) if k == 1.into() => format!("{var}"),
-                        Some(k) if k == (-1).into() => format!("-{var}"),
-                        _ => format!("{coeff} * {var}"),
+                        Some(k) if k == 1.into() => (false, format!("{var}")),
+                        Some(k) if k == (-1).into() => (true, format!("{var}")),
+                        _ => {
+                            let (sign, coeff) = Self::symbolic_expression_to_signed_string(coeff);
+                            (sign, format!("{coeff} * {var}"))
+                        }
                     }),
             )
             .chain(match self.constant.try_to_number() {
                 Some(k) if k == T::zero() => None,
-                _ => Some(format!("{}", self.constant)),
+                _ => Some(Self::symbolic_expression_to_signed_string(&self.constant)),
             })
-            .format(" + ")
-            .to_string();
-        write!(
-            f,
-            "{}",
-            if formatted.is_empty() {
-                "0".to_string()
-            } else {
-                formatted
+            .reduce(|(n1, p1), (n2, p2)| {
+                (
+                    n1,
+                    if n1 == n2 {
+                        format!("{p1} + {p2}")
+                    } else {
+                        format!("{p1} - {p2}")
+                    },
+                )
+            })
+            .unwrap_or((false, "0".to_string()))
+    }
+
+    fn symbolic_expression_to_signed_string(value: &SymbolicExpression<T, V>) -> (bool, String) {
+        match value.try_to_number() {
+            Some(k) => {
+                if k.is_in_lower_half() {
+                    (false, format!("{k}"))
+                } else {
+                    (true, format!("{}", -k))
+                }
             }
-        )
+            _ => (false, value.to_string()),
+        }
     }
 }
 
