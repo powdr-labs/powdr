4b829812d57da6a92be881548c4b3ae05682b4ce
ShuangWu121 <47602565+ShuangWu121@users.noreply.github.com>
2025-06-18 08:58:43 +0200

    Add copy constraints for Plonk circuit (#2871)
    This PR implements copy constraints in Plonk circuit.
    
    Copy constraints are implemented as bus interactions. 
    
    Given an example:
    $w_0 \times w_1 = temp_0$
    $temp_0 \times w_0 = temp_2$
    $w_0 + w_2 = temp_3$
    $temp_0 \times temp_3 = temp_4$
    
    In this example, there are 6 extra columns needed to implement the copy
    constraint:
    | a_id | b_id | c_id | a_perm | b_perm | c_perm |
    
    |-------------|---------------------------|------------|---------------------|---------------------|---------------------|
    | 0         | 1         | 2     |  4        | 1         | 3    |
    | 3       | 4       | 5   |9       | 6      | 5   |
    | 6      |7  |8     | 0      |7  |10     |
    | 9  | 10     |11|2  | 8     |11|
    
    For example, the variable $w_0$ appears in [a_id,b_id,c_id] with
    position [0, 4, 6] (I call it permutation set). These positions are
    shifted cyclically (by one) and placed in the same place of the
    corresponding permutation column, as shown below
    
    
    | a_id | b_id | c_id | a_perm | b_perm | c_perm |
    
    |-------------|---------------------------|------------|---------------------|---------------------|---------------------|
    | 0         | -         | -     |  4        | -        | -    |
    | -      | 4       | -   |-       | 6      | -   |
    | 6      |-  |-     | 0      |-  |-     |
    | -  | -     |-|-  | -     |-|
    
    
    
    The bus interaction are 
    receive: ([a, a_id],1), ([b,b_id],1), ([c, c_id],1)
    send: ([a, a_perm],-1), ([b,b_perm],-1), ([c, c_perm],-1)
    
    This PR creates permutation sets for only the
    Witness(AlgebraicReference) cells, and then creates their corresponding
    permutation columns as well.
    
    Next PR will do the same for  Temp(id) cells.

--- openvm/src/plonk/mod.rs
+++ openvm/src/plonk/mod.rs
@@ -7,8 +7,10 @@ use powdr_autoprecompiles::bus_map::BusType;
 pub mod air_to_plonkish;
 pub mod bus_interaction_handler;
 
+pub const NUMBER_OF_WITNESS_COLS: u64 = 5;
+
 /// A variable in a PlonK gate.
-#[derive(Clone, Copy, Debug, PartialEq, Default)]
+#[derive(Clone, Copy, Debug, PartialEq, Default, Eq, Hash)]
 pub enum Variable<V> {
     /// A variable from the input constraint system.
     /// At run-time, we can get the concrete values from the APC witness generation.
