6b3e62c4d055201d40cecc7642abbafb976d36cd
Georg Wiese <georgwiese@gmail.com>
2025-06-24 20:04:52 +0200

    Add documentation

--- constraint-solver/src/runtime_constant.rs
+++ constraint-solver/src/runtime_constant.rs
@@ -31,31 +31,49 @@ pub trait RuntimeConstant<V>:
 
     /// Creates a run-time constant from a variable.
     fn from_symbol(symbol: V, rc: RangeConstraint<Self::FieldType>) -> Self;
+
+    /// Tries to convert the constant to a single number. This works for compile-time constants.
     fn try_to_number(&self) -> Option<Self::FieldType>;
+
+    /// Returns the range constraint for this constant. For compile-time constants,
+    /// this will be a single value range constraint.
     fn range_constraint(&self) -> RangeConstraint<Self::FieldType>;
+
+    /// Substitutes a variable with another constant.
     fn substitute(&mut self, variable: &V, substitution: &Self);
+
+    /// Returns an iterator over all referenced symbols in this constant.
     fn referenced_symbols<'a>(&'a self) -> impl Iterator<Item = &'a V> + 'a
     where
         V: 'a;
+
+    /// Divides this constant by another constant, returning a new constant.
     fn field_div(&self, other: &Self) -> Self;
 
+    /// Converts a u64 to a run-time constant.
     fn from_u64(k: u64) -> Self {
         Self::from(Self::FieldType::from(k))
     }
 
+    /// Returns whether this constant is known to be zero at compile time.
     fn is_known_zero(&self) -> bool {
         self.try_to_number().is_some_and(|n| n.is_zero())
     }
 
+    /// Returns whether this constant is known to be one at compile time.
     fn is_known_one(&self) -> bool {
         self.try_to_number().is_some_and(|n| n.is_one())
     }
 
+    /// Returns whether this constant is known to be -1 at compile time.
     fn is_known_minus_one(&self) -> bool {
         self.try_to_number()
             .is_some_and(|n| n == -Self::FieldType::from(1))
     }
 
+    /// Returns whether this constant is known to be non-zero at compile time.
+    /// Note that this could return true even if the constant is not known fully
+    /// at compile time, but it is guaranteed that the constant is not zero.
     fn is_known_nonzero(&self) -> bool {
         // Only checking range constraint is enough since if this is a known
         // fixed value, we will get a range constraint with just a single value.
