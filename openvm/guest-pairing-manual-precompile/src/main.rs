b7f852d48d6421501328c01bede3f644ac2b6def
Leandro Pacheco <contact@leandropacheco.com>
2025-06-30 13:19:29 -0300

    fix

--- openvm/guest-pairing-manual-precompile/src/main.rs
+++ openvm/guest-pairing-manual-precompile/src/main.rs
@@ -1,6 +1,5 @@
 use openvm_algebra_guest::IntMod;
 use openvm_ecc_guest::AffinePoint;
-#[allow(unused_imports)]
 use {
     openvm_pairing::bn254::{Bn254, Bn254G1Affine, Fp, Fp2},
     openvm_pairing::PairingCheck,
