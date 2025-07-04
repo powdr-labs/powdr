621d0cf0106f5e23bf6ed829a219968e8780cd12
Thibaut Schaeffer <schaeffer.thibaut@gmail.com>
2025-07-04 15:59:11 +0200

    Avoid collecting into pc vector (#2989)
    When we generate the PGO data, we first collect all visited pcs in
    execution order which is linear in the execution length.
    Then we reduce this to a map of unique PC to number of occurrences,
    which is linear in the program size.
    
    This PR removes the first collection, generating the map directly.
    
    Update: since for reth we do ~300 million additions to the hashmap, this
    PR also switches to using a vector to keep track of the count as opposed
    to a hashmap. Not to be confused: before this PR the vector is the
    sequence of all pcs visited. After this PR, it is the number of times a
    pc was visited, by pc.
    
    Other update:
    copilot made a good point about switching away from
    `Arc<Mutex<Vec<u32>>>` to `Arc<Vec<AtomicU32>>`. Done.

--- openvm/src/lib.rs
+++ openvm/src/lib.rs
@@ -9,6 +9,7 @@ use openvm_circuit::arch::{
 };
 use openvm_circuit::{circuit_derive::Chip, derive::AnyEnum};
 use openvm_circuit_primitives_derive::ChipUsageGetter;
+use openvm_instructions::program::Program;
 use openvm_sdk::{
     config::{AggStarkConfig, AppConfig, SdkVmConfig, SdkVmConfigExecutor, SdkVmConfigPeriphery},
     keygen::AggStarkProvingKey,
@@ -28,10 +29,11 @@ use serde::{Deserialize, Serialize};
 use std::cmp::Reverse;
 use std::fs::File;
 use std::io::BufWriter;
+use std::sync::atomic::{AtomicU32, Ordering};
 use std::{
     collections::HashMap,
     path::{Path, PathBuf},
-    sync::{Arc, Mutex},
+    sync::Arc,
 };
 use strum::{Display, EnumString};
 
@@ -668,13 +670,10 @@ pub fn execution_profile(program: OriginalCompiledProgram, inputs: StdIn) -> Has
     let OriginalCompiledProgram { exe, sdk_vm_config } = program;
 
     // in memory collector storage
-    let collected = Arc::new(Mutex::new(Vec::new()));
-    let collector_layer = PgoCollector {
-        pc: collected.clone(),
-    };
+    let collector = PgoCollector::new(&exe.program);
 
     // build subscriber
-    let subscriber = Registry::default().with(collector_layer);
+    let subscriber = Registry::default().with(collector.clone());
 
     // prepare for execute
     let sdk = Sdk::default();
@@ -686,26 +685,15 @@ pub fn execution_profile(program: OriginalCompiledProgram, inputs: StdIn) -> Has
             .unwrap();
     });
 
-    // collect the pc's during execution
-    let pc = collected.lock().unwrap();
+    // Extract the collected data
+    let pc_index_count = collector.into_hashmap();
 
-    // create pc_index map to times executed, where pc_index = (pc - pc_base) / step
-    let pc_base = exe.program.pc_base;
-    let step = exe.program.step;
-    let pc_index_count = pc
-        .iter()
-        .fold(std::collections::HashMap::new(), |mut acc, pc| {
-            let pc_index = (*pc as u32 - pc_base) / step;
-            *acc.entry(pc_index).or_insert(0u32) += 1;
-            acc
-        });
-
-    // the smallest pc is the same as the base_pc if there's no stdin
-    let pc_min = pc.iter().min().unwrap();
-    tracing::debug!("pc_min: {}; pc_base: {}", pc_min, pc_base);
+    // the smallest pc is the same as the pc_base if there's no stdin
+    let pc_min = pc_index_count.keys().min().unwrap();
+    tracing::debug!("pc_min: {}; pc_base: {}", pc_min, exe.program.pc_base);
 
     // print the total and by pc counts
-    tracing::debug!("Pgo captured {} pc's", pc.len());
+    tracing::debug!("Pgo captured {} pc's", pc_index_count.len());
 
     if tracing::enabled!(Level::DEBUG) {
         // print pc_index map in descending order of pc_index count
@@ -741,7 +729,44 @@ impl tracing::field::Visit for PgoData {
 // A Layer that collects data we are interested in using for the pgo from the trace fields.
 #[derive(Clone)]
 struct PgoCollector {
-    pc: Arc<Mutex<Vec<usize>>>,
+    step: usize,
+    pc_base: usize,
+    pc_index_map: Arc<Vec<AtomicU32>>,
+}
+
+impl PgoCollector {
+    fn new<F>(program: &Program<F>) -> Self {
+        let max_pc_index = program.instructions_and_debug_infos.len();
+        // create a map with max_pc entries initialized to 0
+        let pc_index_map = Arc::new((0..max_pc_index).map(|_| AtomicU32::new(0)).collect());
+        Self {
+            pc_index_map,
+            step: program.step as usize,
+            pc_base: program.pc_base as usize,
+        }
+    }
+
+    fn into_hashmap(self) -> HashMap<u32, u32> {
+        // Turn the map into a HashMap of (pc_index, count)
+        self.pc_index_map
+            .iter()
+            .enumerate()
+            .filter_map(|(pc_index, count)| {
+                let count = count.load(Ordering::Relaxed);
+
+                // if the count is zero, we skip it
+                if count == 0 {
+                    return None;
+                }
+
+                Some((pc_index as u32, count))
+            })
+            .collect()
+    }
+
+    fn increment(&self, pc: usize) {
+        self.pc_index_map[(pc - self.pc_base) / self.step].fetch_add(1, Ordering::Relaxed);
+    }
 }
 
 impl<S> Layer<S> for PgoCollector
@@ -756,7 +781,7 @@ where
         // because our subscriber is at the trace level, for trace print outs that don't match PgoData,
         // the visitor can't parse them, and these cases are filtered out automatically
         if let Some(pc) = visitor.pc {
-            self.pc.lock().unwrap().push(pc);
+            self.increment(pc);
         }
     }
 }
