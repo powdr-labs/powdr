29ddd18108938db24ff4638b79841c7feb5ffa1b
ShuangWu121 <shuang.wu@posteo.net>
2025-07-14 21:40:41 +0200

    add ecc add guest

---
+++ openvm/guest-ecc-add-hint/src/add.rs
@@ -0,0 +1,31 @@
+use k256::FieldElement;
+
+
+pub fn affine_add(
+    p1_x: &FieldElement,
+    p1_y: &FieldElement,
+    p2_x: &FieldElement,
+    p2_y: &FieldElement,
+) -> Option<(FieldElement, FieldElement)> {
+        
+    let dx = (*p2_x - *p1_x).normalize();
+
+    if dx.is_zero().into() {
+        return None;
+    }
+
+    //let invert = FieldElement::from_u64(1).normalize();
+    let invert = dx.invert().unwrap().normalize();
+
+    let dy = (*p2_y - p1_y).normalize();
+    let lambda = (dy * invert).normalize();
+
+    if FieldElement::from_u64(1).normalize()!=invert.normalize()*dx{
+        panic!("wrong invert");
+    };
+    let x3 = (lambda.square() - p1_x - p2_x).normalize();
+    let y3 = (lambda * (*p1_x - x3) - *p1_y).normalize();
+
+
+    Some((x3, y3))
+}
---
+++ openvm/guest-ecc-add-hint/src/main.rs
@@ -0,0 +1,48 @@
+#![cfg_attr(not(feature = "std"), no_main)]
+#![cfg_attr(not(feature = "std"), no_std)]
+use hex_literal::hex;
+use k256::{FieldBytes, FieldElement};
+mod add;
+use add::affine_add;
+
+
+openvm::entry!(main);
+
+pub fn main() {
+    let x1: FieldElement = FieldElement::from_bytes(FieldBytes::from_slice(&{
+        let mut bytes = [0u8; 32];
+        bytes[31] = 1;
+        bytes
+    })).unwrap();
+    let y1: FieldElement = FieldElement::from_bytes(FieldBytes::from_slice(&hex!(
+        "4218F20AE6C646B363DB68605822FB14264CA8D2587FDD6FBC750D587E76A7EE"
+    ))).unwrap().normalize();
+
+    let x2: FieldElement = FieldElement::from_bytes(FieldBytes::from_slice(&{
+        let mut bytes = [0u8; 32];
+        bytes[31] = 2; // big-endian encoding of 1
+        bytes
+    })).unwrap().normalize();
+    let y2: FieldElement = FieldElement::from_bytes(FieldBytes::from_slice(&hex!(
+        "990418D84D45F61F60A56728F5A10317BDB3A05BDA4425E3AEE079F8A847A8D1"
+    ))).unwrap().normalize();
+
+    // This is the sum of (x1, y1) and (x2, y2).
+    let x3: FieldElement= FieldElement::from_bytes(FieldBytes::from_slice(&hex!(
+        "F23A2D865C24C99CC9E7B99BD907FB93EBD6CCCE106BCCCB0082ACF8315E67BE"
+    ))).unwrap().normalize();
+    let y3:FieldElement = FieldElement::from_bytes(FieldBytes::from_slice(&hex!(
+        "791DFC78B49C9B5882867776F18BA7883ED0BAE1C0A856D26D41D38FB47345B4"
+    ))).unwrap().normalize();
+
+
+    // Generic add can handle equal or unequal points.
+    let sum_p1_p2 = affine_add(&x1, &y1, &x2, &y2).unwrap();
+   // let sum_p2_p2 = affine_add(&x2, &y2, &x2, &y2).unwrap();
+ 
+    if x3 != sum_p1_p2.0 {
+        panic!();
+    }
+
+
+}
