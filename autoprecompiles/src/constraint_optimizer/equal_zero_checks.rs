233be8a94cb6858f9d40c555a8787f05aa440dc3
chriseth <chriseth.github@gmail.com>
2025-07-22 14:26:09 +0000

    Remove heuristic.

--- autoprecompiles/src/constraint_optimizer/equal_zero_checks.rs
+++ autoprecompiles/src/constraint_optimizer/equal_zero_checks.rs
@@ -2,7 +2,6 @@ use std::collections::{BTreeSet, HashSet};
 use std::fmt::Display;
 use std::hash::Hash;
 
-use itertools::Itertools;
 use num_traits::One;
 use powdr_constraint_solver::constraint_system::{BusInteractionHandler, ConstraintSystem};
 use powdr_constraint_solver::grouped_expression::{GroupedExpression, RangeConstraintProvider};
@@ -85,21 +84,44 @@ fn try_replace_equal_zero_check<T: FieldElement, V: Clone + Ord + Hash + Display
         return;
     }
 
-    // Some of the inputs are redundant, so try to reduce the size of the set.
-    // TODO this is not how we find the best set of vars. Most are mutually redundant.
-    // We should sort them by distance to a stateful bus interaction and start removing
-    // those with a higher distance. or maybe distance to the output?
-    let inputs = determine_and_remove_redundant_inputs(
-        constraint_system.indexed_system(),
-        bus_interaction_handler.clone(),
-        inputs.iter().rev().cloned(), // TODO using rev as a bad heuristic.
-        output.clone(),
-        value,
+    // Ok, we can replace the constraints by a potentially more efficient version.
+    // Let's find out which are the constraints that we need to replace.
+    // We do a reachability search starting from stateful bus interactions, but we stop
+    // the search as soon as we reach an input or the output variable.
+    // The variables that are not reachable in this sense can be removed, because they are only
+    // connected to a stateful bus interaction via the input or output.
+    let blocking_variables = inputs.iter().chain([&output]).cloned();
+    let outside_variables = reachable_variables_except_blocked(
+        variables_in_stateful_bus_interactions(
+            constraint_system.system(),
+            bus_interaction_handler.clone(),
+        )
+        .cloned(),
+        blocking_variables,
+        constraint_system.system(),
     );
 
-    // println!("\n\nFound equal zero check for variable {output}:\n{output} = {value} if and only if all of {} are zero.",
-    //     inputs.iter().format(", ")
-    // );
+    // If some of the inputs are not reachable via stateful bus interactions, they
+    // are most likely redundant inputs that by chance share the property.
+    let inputs = inputs
+        .into_iter()
+        .filter(|v| outside_variables.contains(v))
+        .collect::<BTreeSet<_>>();
+
+    // Let's verify the condition again.
+    if solve_with_assignments(
+        constraint_system.indexed_system(),
+        bus_interaction_handler.clone(),
+        inputs
+            .iter()
+            .map(|v| (v.clone(), T::from(0)))
+            .chain([(output.clone(), T::from(1) - value)]),
+    )
+    .is_ok()
+    {
+        // Something is wrong here, but rather do nothing.
+        return;
+    }
 
     // Check that each input is non-negative and that the sum of inputs cannot wrap.
     let min_input = inputs
@@ -121,27 +143,11 @@ fn try_replace_equal_zero_check<T: FieldElement, V: Clone + Ord + Hash + Display
         return;
     }
 
-    // Ok, we can replace the constraints by a potentially more efficient version.
-    // Let's find out which are the constraints that we need to replace.
-    // We do a reachability search starting from stateful bus interactions, but we stop
-    // the search as soon as we reach an input or the output variable.
-    // The variables that are not reachable in this sense can be removed, because they are only
-    // connected to a stateful bus interaction via the input or output.
-    let blocking_variables = inputs.iter().chain([&output]).cloned();
-    let outside_variables = reachable_variables_except_blocked(
-        variables_in_stateful_bus_interactions(
-            constraint_system.system(),
-            bus_interaction_handler.clone(),
-        )
-        .cloned(),
-        blocking_variables,
-        constraint_system.system(),
-    );
-
+    // Now that we have a reduce set of inputs, we can focus again on finding the
+    // inner variables to remove.
     // We could still have constraints that are only connected to the inputs but not the output, which constrain
     // the inputs.
     // Those we should keep, so we again do a reachability search from the output.
-
     let output_reachable_variables = reachable_variables_except_blocked(
         std::iter::once(output.clone()),
         inputs.iter().cloned(),
@@ -152,21 +158,18 @@ fn try_replace_equal_zero_check<T: FieldElement, V: Clone + Ord + Hash + Display
         .difference(&outside_variables)
         .collect::<HashSet<_>>();
     variables_to_remove.remove(&output);
-
-    if variables_to_remove.len() <= 2 {
-        // println!(
-        //     "Not enough variables to remove ({}), keeping the system as it is.",
-        //     variables_to_remove.len()
-        // );
-        return;
-    } else {
-        // println!(
-        //     "Removing {} variables: {}",
-        //     variables_to_remove.len(),
-        //     variables_to_remove.iter().format(", ")
-        // );
+    for input in &inputs {
+        variables_to_remove.remove(input);
     }
 
+    if variables_to_remove.len() <= 2 {
+        return;
+    }
+
+    // We should be able to remove all constraints that contain
+    // at least one variable in `variables_to_remove`.
+    // All other variables in such a constraint should either be inputs,
+    // the output or other variables we can remove.
     for constr in constraint_system.system().iter() {
         if constr
             .referenced_variables()
@@ -175,7 +178,6 @@ fn try_replace_equal_zero_check<T: FieldElement, V: Clone + Ord + Hash + Display
             assert!(constr.referenced_variables().all(|var| {
                 inputs.contains(var) || var == &output || variables_to_remove.contains(var)
             }));
-            // println!("Can replace constraint: {constr}",);
         }
     }
     constraint_system.retain_algebraic_constraints(|constr| {
@@ -189,6 +191,7 @@ fn try_replace_equal_zero_check<T: FieldElement, V: Clone + Ord + Hash + Display
             .any(|var| variables_to_remove.contains(var))
     });
 
+    // New we build the more efficient version of the function.
     let output_expr = GroupedExpression::from_unknown_variable(output.clone());
     let output_expr = if value == T::from(1) {
         output_expr
@@ -234,43 +237,6 @@ fn try_replace_equal_zero_check<T: FieldElement, V: Clone + Ord + Hash + Display
             .chain([(output.clone(), T::from(1) - value)]),
     )
     .is_err());
-}
-
-fn determine_and_remove_redundant_inputs<T: FieldElement, V: Clone + Ord + Hash + Display>(
-    constraint_system: &IndexedConstraintSystem<T, V>,
-    bus_interaction_handler: impl BusInteractionHandler<T> + IsBusStateful<T> + Clone,
-    inputs: impl IntoIterator<Item = V>,
-    output: V,
-    value: T,
-) -> Vec<V> {
-    let inputs = inputs.into_iter().collect_vec();
-    // An input 'i' is redundant if setting `var = 1 - value` and `x = 0` for all `x` in `inputs \ {i}` is still inconsistent.
-    let mut redundant_input_indices = vec![];
-    for (i, _var) in inputs.iter().enumerate() {
-        let assignments = inputs
-            .iter()
-            .enumerate()
-            .filter(|&(j, _)| j != i && !redundant_input_indices.contains(&j))
-            .map(|(_, var)| (var.clone(), T::from(0)))
-            .chain([(output.clone(), T::from(1) - value)]);
-        if solve_with_assignments(
-            constraint_system,
-            bus_interaction_handler.clone(),
-            assignments,
-        )
-        .is_err()
-        {
-            // TODO println!("Input {var} is redundant, removing it from the set of inputs.");
-            redundant_input_indices.push(i);
-        }
-    }
-
-    inputs
-        .iter()
-        .enumerate()
-        .filter(|(i, _)| !redundant_input_indices.contains(i))
-        .map(|(_, v)| v.clone())
-        .collect()
 }
 
 /// Runs the solver given a list of variable assignments.
