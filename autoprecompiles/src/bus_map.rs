54ee9b3ac4f1b5ad8463c3e0605006c574bfc5f6
Georg Wiese <georgwiese@gmail.com>
2025-07-23 14:48:15 +0200

    Rename generic parameter

--- autoprecompiles/src/bus_map.rs
+++ autoprecompiles/src/bus_map.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::{collections::BTreeMap, fmt::Display};
 
 #[derive(Copy, Clone, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord)]
-pub enum BusType<T> {
+pub enum BusType<C> {
     /// In a no-CPU architecture, instruction AIRs receive the current state and send the next state.
     /// Typically the state would include the current time stamp and program counter, but powdr does
     /// not make any assumptions about the state.
@@ -15,10 +15,10 @@ pub enum BusType<T> {
     // TODO: Generalize this to a more generic bus type.
     OpenVmBitwiseLookup,
     /// Other types, specific to the VM integration. Powdr largely ignores these.
-    Other(T),
+    Other(C),
 }
 
-impl<T: Display> std::fmt::Display for BusType<T> {
+impl<C: Display> std::fmt::Display for BusType<C> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let name = match self {
             BusType::ExecutionBridge => "EXECUTION_BRIDGE",
@@ -32,13 +32,13 @@ impl<T: Display> std::fmt::Display for BusType<T> {
 }
 
 #[derive(Clone, Deserialize, Serialize)]
-pub struct BusMap<T> {
-    bus_ids: BTreeMap<u64, BusType<T>>,
+pub struct BusMap<C> {
+    bus_ids: BTreeMap<u64, BusType<C>>,
 }
 
-impl<T: PartialEq + Eq + Clone + Display> BusMap<T> {
+impl<C: PartialEq + Eq + Clone + Display> BusMap<C> {
     /// Construct a new `BusMap`, ensuring the same id is not used for different `BusType`s
-    pub fn from_id_type_pairs(pairs: impl IntoIterator<Item = (u64, BusType<T>)>) -> Self {
+    pub fn from_id_type_pairs(pairs: impl IntoIterator<Item = (u64, BusType<C>)>) -> Self {
         let mut bus_ids = BTreeMap::new();
         for (k, v) in pairs.into_iter() {
             bus_ids.entry(k).and_modify(|existing| {
@@ -52,19 +52,19 @@ impl<T: PartialEq + Eq + Clone + Display> BusMap<T> {
     }
 
     /// Lookup the `BusType` for a given ID.
-    pub fn bus_type(&self, bus_id: u64) -> BusType<T> {
+    pub fn bus_type(&self, bus_id: u64) -> BusType<C> {
         self.bus_ids.get(&bus_id).cloned().unwrap_or_else(|| {
             panic!("No bus type found for ID: {bus_id}");
         })
     }
 
     /// View the entire map.
-    pub fn all_types_by_id(&self) -> &BTreeMap<u64, BusType<T>> {
+    pub fn all_types_by_id(&self) -> &BTreeMap<u64, BusType<C>> {
         &self.bus_ids
     }
 
     /// Find the ID for a given `BusType` (if any).
-    pub fn get_bus_id(&self, bus_type: &BusType<T>) -> Option<u64> {
+    pub fn get_bus_id(&self, bus_type: &BusType<C>) -> Option<u64> {
         self.bus_ids
             .iter()
             .find_map(|(id, bus)| if bus == bus_type { Some(*id) } else { None })
