e682b57f3efe10064cec8b1c46492ecab04cb07f
ShuangWu121 <shuang.wu@posteo.net>
2025-07-30 14:22:25 +0200

    using diffirent metod to read data

---
+++ openvm/guest-ecc-powdr-affine-hint-10-26-field/src/main.rs
@@ -0,0 +1,124 @@
+#![cfg_attr(not(feature = "std"), no_main)]
+#![cfg_attr(not(feature = "std"), no_std)]
+use hex_literal::hex;
+use k256::PowdrAffinePoint;
+use k256::{FieldBytes, FieldElement, Scalar};
+use k256::elliptic_curve::PrimeField;
+
+openvm::entry!(main);
+
+pub fn main() {
+    let x1: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[{
+            let mut bytes = [0u8; 32];
+            bytes[31] = 1;
+            bytes
+        }])[0],
+    )
+    .unwrap();
+    let y1: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[<[_; 32]>::try_from(hex!(
+            "4218F20AE6C646B363DB68605822FB14264CA8D2587FDD6FBC750D587E76A7EE"
+        ))
+        .unwrap()])[0],
+    )
+    .unwrap()
+    .normalize();
+
+    let x2: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[{
+            let mut bytes = [0u8; 32];
+            bytes[31] = 2;
+            bytes
+        }])[0],
+    )
+    .unwrap();
+
+    let y2: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[<[_; 32]>::try_from(hex!(
+            "990418D84D45F61F60A56728F5A10317BDB3A05BDA4425E3AEE079F8A847A8D1"
+        ))
+        .unwrap()])[0],
+    )
+    .unwrap()
+    .normalize();
+
+    let x3: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[<[_; 32]>::try_from(hex!(
+            "F23A2D865C24C99CC9E7B99BD907FB93EBD6CCCE106BCCCB0082ACF8315E67BE"
+        ))
+        .unwrap()])[0],
+    )
+    .unwrap()
+    .normalize();
+
+    let y3: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[<[_; 32]>::try_from(hex!(
+            "791DFC78B49C9B5882867776F18BA7883ED0BAE1C0A856D26D41D38FB47345B4"
+        ))
+        .unwrap()])[0],
+    )
+    .unwrap()
+    .normalize();
+
+    let x4: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[<[_; 32]>::try_from(hex!(
+            "33333333333333333333333333333333333333333333333333333332FFFFFF3B"
+        ))
+        .unwrap()])[0],
+    )
+    .unwrap()
+    .normalize();
+
+    let y4: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[<[_; 32]>::try_from(hex!(
+            "3916485F2C3D80C62048C6FD8ACBF71EED11987A55CC10ABDC4E4A25C4EC54AC"
+        ))
+        .unwrap()])[0],
+    )
+    .unwrap()
+    .normalize();
+
+    let point1 = PowdrAffinePoint {
+        x: x1,
+        y: y1,
+        infinity: 0,
+    };
+    let point2 = PowdrAffinePoint {
+        x: x2,
+        y: y2,
+        infinity: 0,
+    };
+
+    let addition = point1 + point2.clone();
+    let double = point2.double();
+    assert_eq!(addition.x, x3);
+    assert_eq!(addition.y, y3);
+
+    assert_eq!(double.x, x4.normalize());
+    assert_eq!(double.y, y4.normalize());
+
+    let x5: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[<[_; 32]>::try_from(hex!(
+            "6D6D216817A448DC312FEE586FA306D189CB404A9CAF72D90308797F38934A19"
+        ))
+        .unwrap()])[0],
+    )
+    .unwrap()
+    .normalize();
+
+    let y5: FieldElement = FieldElement::from_bytes(
+        &FieldBytes::cast_slice_from_core(&[<[_; 32]>::try_from(hex!(
+            "2C9BB19372B2E1B830B5F4D92ADBAFEAAEB612026122E571D1BEA76D742F279E"
+        ))
+        .unwrap()])[0],
+    )
+    .unwrap()
+    .normalize();
+
+    // Ec Mul
+    let scalar = Scalar::from_u128(12345678);
+    let multiplication = point1 * scalar;
+    assert_eq!(multiplication.x, x5);
+    assert_eq!(multiplication.y, y5);
+}
