e1f6633024d39ee1e9fc29eca4a783f46f0cd68c
chriseth <chriseth.github@gmail.com>
2025-07-31 13:17:12 +0000

    Remove last_var.

--- constraint-solver/src/solver.rs
+++ constraint-solver/src/solver.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use powdr_number::{ExpressionConvertible, FieldElement};
 
 use crate::boolean_extractor::try_extract_boolean;
@@ -157,12 +156,10 @@ impl BooleanVarDispenser {
         self.next_boolean_id += 1;
         Variable::Boolean(id)
     }
-
-    fn latest_var<V>(&self) -> Option<Variable<V>> {
-        (self.next_boolean_id > 0).then(|| Variable::Boolean(self.next_boolean_id - 1))
-    }
 }
 
+/// An implementation of `Solver` that tries to introduce new boolean variables
+/// for certain quadratic constraints to make them affine.
 struct BooleanExtractedSolver<T, V, S> {
     solver: S,
     boolean_var_dispenser: BooleanVarDispenser,
@@ -229,23 +226,22 @@ where
         &mut self,
         constraints: impl IntoIterator<Item = GroupedExpression<T, V>>,
     ) {
-        let constraints = constraints
-            .into_iter()
-            .flat_map(|constr| {
+        let mut new_boolean_vars = vec![];
+        self.solver
+            .add_algebraic_constraints(constraints.into_iter().flat_map(|constr| {
                 let constr = constr.transform_var_type(&mut |v| v.clone().into());
-                let extracted =
-                    try_extract_boolean(&constr, &mut || self.boolean_var_dispenser.next_var())
-                        .inspect(|_| {
-                            // Make sure to range-constrain the variable
-                            self.solver.add_range_constraint(
-                                &self.boolean_var_dispenser.latest_var().unwrap(),
-                                RangeConstraint::from_mask(1),
-                            );
-                        });
+                let extracted = try_extract_boolean(&constr, &mut || {
+                    let v = self.boolean_var_dispenser.next_var();
+                    new_boolean_vars.push(v.clone());
+                    v
+                });
                 std::iter::once(constr).chain(extracted)
-            })
-            .collect_vec();
-        self.solver.add_algebraic_constraints(constraints);
+            }));
+        // We need to manually add the boolean range constraints for the new variables.
+        for v in new_boolean_vars {
+            self.solver
+                .add_range_constraint(&v, RangeConstraint::from_mask(1));
+        }
     }
 
     fn add_bus_interactions(
