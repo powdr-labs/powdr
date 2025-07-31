1111cd20b8b58168a510224300a62927a9f9c162
ShuangWu121 <shuang.wu@posteo.net>
2025-07-31 12:15:08 +0200

    update the projective test, in case for testing with newer guests using hint

--- openvm/guest-ecc-operations/src/main.rs
+++ openvm/guest-ecc-operations/src/main.rs
@@ -8,83 +8,58 @@ use k256::{AffinePoint, EncodedPoint, FieldBytes, ProjectivePoint, Scalar};
 openvm::entry!(main);
 
 pub fn main() {
-    let x1: FieldBytes = *FieldBytes::from_slice(&{
-        let mut bytes = [0u8; 32];
-        bytes[31] = 1;
-        bytes
-    });
-    let y1: FieldBytes = *FieldBytes::from_slice(&hex!(
-        "4218F20AE6C646B363DB68605822FB14264CA8D2587FDD6FBC750D587E76A7EE"
+    let x1 = FieldBytes::from_slice(&[
+        177, 205, 72, 85, 29, 179, 168, 198, 125, 68, 123, 98, 49, 165, 115, 23, 117, 100, 184, 12,
+        125, 99, 103, 18, 245, 130, 15, 91, 76, 105, 85, 20,
+    ]);
+
+    let y1 = FieldBytes::from_slice(&[
+        219, 130, 184, 163, 86, 144, 60, 160, 181, 38, 124, 67, 141, 79, 174, 63, 60, 188, 208,
+        206, 139, 94, 72, 251, 222, 58, 13, 159, 189, 75, 97, 12,
+    ]);
+
+    let x2 = FieldBytes::from_slice(&[
+        146, 161, 155, 83, 76, 248, 129, 31, 87, 66, 55, 228, 112, 251, 3, 121, 113, 60, 97, 168,
+        52, 94, 83, 10, 224, 229, 14, 231, 182, 207, 33, 28,
+    ]);
+
+    let y2 = FieldBytes::from_slice(&[
+        163, 84, 112, 69, 78, 54, 106, 228, 95, 24, 73, 7, 216, 178, 14, 141, 200, 150, 92, 72, 29,
+        246, 91, 179, 165, 11, 29, 36, 68, 96, 135, 19,
+    ]);
+
+    let point1 =
+        AffinePoint::from_encoded_point(&EncodedPoint::from_affine_coordinates(&x1, &y1, false))
+            .expect("AffinePoint should be valid");
+    let point2 =
+        AffinePoint::from_encoded_point(&EncodedPoint::from_affine_coordinates(&x2, &y2, false))
+            .expect("AffinePoint should be valid");
+
+    let x = ProjectivePoint::from(point1);
+    let y = ProjectivePoint::from(point2);
+
+    let scalar_1 = Scalar::from_repr(*FieldBytes::from_slice(
+        hex!("BFD5D7FA526B6954945C980C6C804E0E19840F2DA009C8B0C9A511189FB466BF").as_ref(),
     ));
 
-    let x2: FieldBytes = *FieldBytes::from_slice(&{
-        let mut bytes = [0u8; 32];
-        bytes[31] = 2; // big-endian encoding of 1
-        bytes
-    });
-    let y2: FieldBytes = *FieldBytes::from_slice(&hex!(
-        "990418D84D45F61F60A56728F5A10317BDB3A05BDA4425E3AEE079F8A847A8D1"
+    let scalar_2 = Scalar::from_repr(*FieldBytes::from_slice(
+        hex!("369E07A2FC32462DD74AB67CE7D7595EC91FC11CC90A3C15A94B57A21E878614").as_ref(),
     ));
 
-    // This is the sum of (x1, y1) and (x2, y2).
-    let x3 = *FieldBytes::from_slice(&hex!(
-        "F23A2D865C24C99CC9E7B99BD907FB93EBD6CCCE106BCCCB0082ACF8315E67BE"
-    ));
-    let y3 = *FieldBytes::from_slice(&hex!(
-        "791DFC78B49C9B5882867776F18BA7883ED0BAE1C0A856D26D41D38FB47345B4"
-    ));
+    let rusult_x = FieldBytes::from_slice(&[
+        112, 170, 75, 207, 229, 212, 237, 2, 131, 65, 143, 232, 168, 46, 48, 240, 56, 164, 245,
+        167, 23, 29, 43, 132, 130, 181, 145, 207, 3, 49, 25, 48,
+    ]);
+    let rusult_y = FieldBytes::from_slice(&[
+        225, 222, 233, 182, 14, 157, 47, 22, 177, 249, 107, 145, 57, 77, 133, 68, 6, 102, 101, 78,
+        5, 249, 10, 81, 202, 112, 204, 76, 117, 7, 231, 160,
+    ]);
 
-    // This is the double of (x2, y2).
+    let result_point = AffinePoint::from_encoded_point(&EncodedPoint::from_affine_coordinates(
+        &*rusult_x, &*rusult_y, false,
+    ))
+    .expect("AffinePoint should be valid");
 
-    let x4 = *FieldBytes::from_slice(&hex!(
-        "33333333333333333333333333333333333333333333333333333332FFFFFF3B"
-    ));
-
-    let y4 = *FieldBytes::from_slice(&hex!(
-        "3916485F2C3D80C62048C6FD8ACBF71EED11987A55CC10ABDC4E4A25C4EC54AC"
-    ));
-
-    let p1 = EncodedPoint::from_affine_coordinates(&x1, &y1, false);
-    let p2 = EncodedPoint::from_affine_coordinates(&x2, &y2, false);
-    let p3 = EncodedPoint::from_affine_coordinates(&x3, &y3, false);
-    let p4 = EncodedPoint::from_affine_coordinates(&x4, &y4, false);
-
-    let p1_affine = AffinePoint::from_encoded_point(&p1).expect("invalid point 1");
-    let p2_affine = AffinePoint::from_encoded_point(&p2).expect("invalid point 2");
-    let p3_affine = AffinePoint::from_encoded_point(&p3).expect("invalid point 3");
-    let p4_affine = AffinePoint::from_encoded_point(&p4).expect("invalid point 4");
-
-    let mut p1_projective = ProjectivePoint::from(p1_affine);
-    let mut p2_projective = ProjectivePoint::from(p2_affine);
-
-    // Generic add can handle equal or unequal points.
-    let sum_projective = p1_projective + p2_projective;
-    let sum_affine = sum_projective.to_affine();
-    assert_eq!(sum_affine, p3_affine);
-    
-
-    // let p2_add_p2_projective = p2_projective + p2_projective;
-    // let p2_add_p2_affine = p2_add_p2_projective.to_affine();
-    // assert_eq!(p4_affine, p2_add_p2_affine);
-
-    // Add assign and double assign
-    // p1_projective += p2_projective;
-    // assert_eq!(p1_projective.to_affine(), p3_affine);
-
-    p2_projective = p2_projective.double();
-    assert_eq!(p2_projective.to_affine(), p4_affine);
-
-    // Ec Mul
-    let p1_projective = ProjectivePoint::from(p1_affine);
-    let scalar = Scalar::from_u128(12345678);
-
-    let x5 = *FieldBytes::from_slice(&hex!(
-        "6D6D216817A448DC312FEE586FA306D189CB404A9CAF72D90308797F38934A19"
-    ));
-    let y5 = *FieldBytes::from_slice(&hex!(
-        "2C9BB19372B2E1B830B5F4D92ADBAFEAAEB612026122E571D1BEA76D742F279E"
-    ));
-    let result = p1_projective * scalar;
-    assert_eq!(result.to_affine(),  AffinePoint::from_encoded_point(&EncodedPoint::from_affine_coordinates(&x5, &y5, false))
-            .expect("invalid point 5"));
+    let result = x * scalar_1.unwrap() + y * scalar_2.unwrap();
+    assert_eq!(result.to_affine(), result_point);
 }
