a4862ba5b6ba231e49b2b4145528f6d14b31c34b
ShuangWu121 <shuang.wu@posteo.net>
2025-08-15 15:21:56 +0200

    remove sha

--- openvm/guest-ecrecover-manual/src/main.rs
+++ openvm/guest-ecrecover-manual/src/main.rs
@@ -9,7 +9,6 @@ use openvm_k256::ecdsa::{Signature, VerifyingKey};
 // clippy thinks this is unused, but it's used in the init! macro
 #[allow(unused)]
 use openvm_k256::Secp256k1Point;
-use openvm_sha2::sha256;
 
 openvm::init!();
 
@@ -18,7 +17,6 @@ openvm::entry!(main);
 /// Signature recovery test vectors
 struct RecoveryTestVector {
     pk: [u8; 33],
-    msg: &'static [u8],
     sig: [u8; 64],
     recid: RecoveryId,
 }
@@ -27,7 +25,6 @@ const RECOVERY_TEST_VECTORS: &[RecoveryTestVector] = &[
     // Recovery ID 0
     RecoveryTestVector {
         pk: hex!("021a7a569e91dbf60581509c7fc946d1003b60c7dee85299538db6353538d59574"),
-        msg: b"example message",
         sig: hex!(
             "ce53abb3721bafc561408ce8ff99c909f7f0b18a2f788649d6470162ab1aa032
                  3971edc523a6d6453f3fb6128d318d9db1a5ff3386feb1047d9816e780039d52"
@@ -37,7 +34,6 @@ const RECOVERY_TEST_VECTORS: &[RecoveryTestVector] = &[
     // Recovery ID 1
     RecoveryTestVector {
         pk: hex!("036d6caac248af96f6afa7f904f550253a0f3ef3f5aa2fe6838a95b216691468e2"),
-        msg: b"example message",
         sig: hex!(
             "46c05b6368a44b8810d79859441d819b8e7cdc8bfd371e35c53196f4bcacdb51
                  35c7facce2a97b95eacba8a586d87b7958aaf8368ab29cee481f76e871dbd9cb"
@@ -49,7 +45,10 @@ const RECOVERY_TEST_VECTORS: &[RecoveryTestVector] = &[
 // Test public key recovery
 fn main() {
     for vector in RECOVERY_TEST_VECTORS {
-        let digest = sha256(vector.msg);
+        let digest = [
+            173, 132, 205, 11, 16, 252, 2, 135, 56, 151, 27, 7, 129, 36, 174, 194, 160, 231, 198,
+            217, 134, 163, 129, 190, 11, 56, 111, 50, 190, 232, 135, 175,
+        ];
         let sig = Signature::try_from(vector.sig.as_slice()).unwrap();
         let recid = vector.recid;
         let pk = VerifyingKey::recover_from_prehash(digest.as_slice(), &sig, recid).unwrap();
--- openvm/guest-ecrecover/src/main.rs
+++ openvm/guest-ecrecover/src/main.rs
@@ -1,18 +1,14 @@
-#![cfg_attr(not(feature = "std"), no_main)]
-#![cfg_attr(not(feature = "std"), no_std)]
+//#![cfg_attr(not(feature = "std"), no_main)]
+//#![cfg_attr(not(feature = "std"), no_std)]
 openvm::entry!(main);
 
+use ecdsa::{PowdrVerifyingKey, RecoveryId, Signature};
 use hex_literal::hex;
-use k256::{
-    ecdsa::{K256VerifyExt, RecoveryId, Signature, VerifyingKey},
-    EncodedPoint, Secp256k1,
-};
-use sha2::{Digest, Sha256};
+use k256::EncodedPoint;
 
 // Signature recovery test vectors
 struct RecoveryTestVector {
     pk: [u8; 33],
-    msg: &'static [u8],
     sig: [u8; 64],
     recid: RecoveryId,
 }
@@ -21,7 +17,6 @@ const RECOVERY_TEST_VECTORS: &[RecoveryTestVector] = &[
     // Recovery ID 0
     RecoveryTestVector {
         pk: hex!("021a7a569e91dbf60581509c7fc946d1003b60c7dee85299538db6353538d59574"),
-        msg: b"example message",
         sig: hex!(
             "ce53abb3721bafc561408ce8ff99c909f7f0b18a2f788649d6470162ab1aa032
                  3971edc523a6d6453f3fb6128d318d9db1a5ff3386feb1047d9816e780039d52"
@@ -31,7 +26,6 @@ const RECOVERY_TEST_VECTORS: &[RecoveryTestVector] = &[
     // Recovery ID 1
     RecoveryTestVector {
         pk: hex!("036d6caac248af96f6afa7f904f550253a0f3ef3f5aa2fe6838a95b216691468e2"),
-        msg: b"example message",
         sig: hex!(
             "46c05b6368a44b8810d79859441d819b8e7cdc8bfd371e35c53196f4bcacdb51
                  35c7facce2a97b95eacba8a586d87b7958aaf8368ab29cee481f76e871dbd9cb"
@@ -43,15 +37,13 @@ const RECOVERY_TEST_VECTORS: &[RecoveryTestVector] = &[
 //Test public key recovery
 pub fn main() {
     for vector in RECOVERY_TEST_VECTORS {
-        let digest = Sha256::digest(vector.msg);
+        let digest = [
+            173, 132, 205, 11, 16, 252, 2, 135, 56, 151, 27, 7, 129, 36, 174, 194, 160, 231, 198,
+            217, 134, 163, 129, 190, 11, 56, 111, 50, 190, 232, 135, 175,
+        ];
         let sig = Signature::try_from(vector.sig.as_slice()).unwrap();
         let recid = vector.recid;
-        let pk = <VerifyingKey as K256VerifyExt>::recover_from_prehash::<Secp256k1>(
-            digest.as_slice(),
-            &sig,
-            recid,
-        )
-        .unwrap();
-        assert_eq!(&vector.pk[..], EncodedPoint::from(&pk).as_bytes());
+        let pk = PowdrVerifyingKey::recover_from_prehash(digest.as_slice(), &sig, recid).unwrap();
+        assert_eq!(&vector.pk[..], EncodedPoint::from(&pk.0).as_bytes());
     }
 }
