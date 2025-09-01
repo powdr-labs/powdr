0b9017b05d7dd758f0e51f5dac1db0cca5bb7f20
schaeff <thibaut@powdrlabs.com>
2025-09-01 10:30:11 +0200

    fix can_always_be_satisfied_via_free_variable

--- autoprecompiles/src/constraint_optimizer.rs
+++ autoprecompiles/src/constraint_optimizer.rs
@@ -183,7 +183,7 @@ fn remove_free_variables<T: FieldElement, V: Clone + Ord + Eq + Hash + Display>(
         .filter(|(variable, constraint)| match constraint {
             // Remove the algebraic constraint if we can solve for the variable.
             ConstraintRef::AlgebraicConstraint(constr) => {
-                can_always_be_zero_via_free_variable(constr, variable)
+                can_always_be_satisfied_via_free_variable(constr.clone(), variable)
             }
             ConstraintRef::BusInteraction(bus_interaction) => {
                 let bus_id = bus_interaction.bus_id.try_to_number().unwrap();
@@ -239,18 +239,18 @@ fn remove_free_variables<T: FieldElement, V: Clone + Ord + Eq + Hash + Display>(
     constraint_system
 }
 
-/// Returns true if the given expression can always be made to evaluate to 0 by setting the
+/// Returns true if the given constraint can always be made to be satisfied by setting the
 /// free variable, regardless of the values of other variables.
-fn can_always_be_zero_via_free_variable<T: FieldElement, V: Clone + Hash + Eq + Ord + Display>(
-    expression: &GroupedExpression<T, V>,
+fn can_always_be_satisfied_via_free_variable<T: FieldElement, V: Clone + Hash + Eq + Ord + Display>(
+    constraint: AlgebraicConstraint<&GroupedExpression<T, V>>,
     free_variable: &V,
 ) -> bool {
-    if expression.try_solve_for(free_variable).is_some() {
+    if constraint.try_solve_for(free_variable).is_some() {
         true
-    } else if let Some((left, right)) = expression.try_as_single_product() {
+    } else if let Some((left, right)) = constraint.expression.try_as_single_product() {
         // If either `left` or `right` can be set to 0, the constraint is satisfied.
-        can_always_be_zero_via_free_variable(left, free_variable)
-            || can_always_be_zero_via_free_variable(right, free_variable)
+        can_always_be_satisfied_via_free_variable(AlgebraicConstraint::from(left), free_variable)
+            || can_always_be_satisfied_via_free_variable(AlgebraicConstraint::from(right), free_variable)
     } else {
         false
     }
