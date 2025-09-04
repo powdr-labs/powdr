54d016882274b626753db6ce7b4c5ab964176353
chriseth <chriseth.github@gmail.com>
2025-09-04 17:22:12 +0000

    Move all bounds to where clause.

--- constraint-solver/src/reachability.rs
+++ constraint-solver/src/reachability.rs
@@ -10,12 +10,13 @@ use crate::runtime_constant::{ReferencedSymbols, RuntimeConstant};
 /// Returns the set of all variables reachable from an initial set via shared constraints
 /// (algebraic constraints and bus interactions).
 /// The returned set also contains the initial variables.
-pub fn reachable_variables<T, V: Clone + Ord + Hash + Display>(
+pub fn reachable_variables<T, V>(
     initial_variables: impl IntoIterator<Item = V>,
     constraint_system: &IndexedConstraintSystem<T, V>,
 ) -> HashSet<V>
 where
     T: RuntimeConstant + ReferencedSymbols<V>,
+    V: Clone + Ord + Hash + Display,
 {
     reachable_variables_except_blocked(initial_variables, std::iter::empty(), constraint_system)
 }
@@ -25,13 +26,14 @@ where
 /// The set of blocking variables is a barrier that stops the reachability search.
 /// The returned set does not contain the blocking variables but it does contain
 /// the initial variables.
-pub fn reachable_variables_except_blocked<T, V: Clone + Ord + Hash + Display>(
+pub fn reachable_variables_except_blocked<T, V>(
     initial_variables: impl IntoIterator<Item = V>,
     blocking_variables: impl IntoIterator<Item = V>,
     constraint_system: &IndexedConstraintSystem<T, V>,
 ) -> HashSet<V>
 where
     T: RuntimeConstant + ReferencedSymbols<V>,
+    V: Clone + Ord + Hash + Display,
 {
     let mut reachable_variables = initial_variables.into_iter().collect::<HashSet<_>>();
     let blocking_variables = blocking_variables.into_iter().collect::<HashSet<_>>();
