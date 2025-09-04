b519bfc738f92981026d26cecf97baf33b6b82a1
chriseth <chriseth.github@gmail.com>
2025-09-04 16:33:51 +0000

    Use RuntimeConstant.

--- constraint-solver/src/reachability.rs
+++ constraint-solver/src/reachability.rs
@@ -2,17 +2,19 @@ use std::collections::HashSet;
 use std::fmt::Display;
 use std::hash::Hash;
 
-use powdr_number::FieldElement;
-
 use crate::constraint_system::ConstraintSystem;
+use crate::runtime_constant::{ReferencedSymbols, RuntimeConstant};
 
 /// Returns the set of all variables reachable from an initial set via shared constraints
 /// (algebraic constraints and bus interactions).
 /// The returned set also contains the initial variables.
-pub fn reachable_variables<T: FieldElement, V: Clone + Ord + Hash + Display>(
+pub fn reachable_variables<T, V: Clone + Ord + Hash + Display>(
     initial_variables: impl IntoIterator<Item = V>,
     constraint_system: &ConstraintSystem<T, V>,
-) -> HashSet<V> {
+) -> HashSet<V>
+where
+    T: RuntimeConstant + ReferencedSymbols<V>,
+{
     reachable_variables_except_blocked(initial_variables, std::iter::empty(), constraint_system)
 }
 
@@ -21,11 +23,14 @@ pub fn reachable_variables<T: FieldElement, V: Clone + Ord + Hash + Display>(
 /// The set of blocking variables is a barrier that stops the reachability search.
 /// The returned set does not contain the blocking variables but it does contain
 /// the initial variables.
-pub fn reachable_variables_except_blocked<T: FieldElement, V: Clone + Ord + Hash + Display>(
+pub fn reachable_variables_except_blocked<T, V: Clone + Ord + Hash + Display>(
     initial_variables: impl IntoIterator<Item = V>,
     blocking_variables: impl IntoIterator<Item = V>,
     constraint_system: &ConstraintSystem<T, V>,
-) -> HashSet<V> {
+) -> HashSet<V>
+where
+    T: RuntimeConstant + ReferencedSymbols<V>,
+{
     let mut reachable_variables = initial_variables.into_iter().collect::<HashSet<_>>();
     let blocking_variables = blocking_variables.into_iter().collect::<HashSet<_>>();
     // We just remove variables, order does not matter.
