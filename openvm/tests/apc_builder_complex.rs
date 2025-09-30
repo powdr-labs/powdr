e7c77609104a6fd80dac8ed91e42ee5069f9c47b
ShuangWu121 <shuang.wu@posteo.net>
2025-09-30 11:12:18 +0200

    add test

--- openvm/tests/apc_builder_complex.rs
+++ openvm/tests/apc_builder_complex.rs
@@ -13,6 +13,7 @@ fn guest_top_block() {
     // Top block from `guest` with `--pgo cell`, with 4 instructions:
     // Instruction { opcode: 512, args: [8, 8, 16777200, 1, 0, 0, 0] }
     // Instruction { opcode: 531, args: [4, 8, 12, 1, 2, 1, 0] }
+ 
     // Instruction { opcode: 576, args: [4, 0, 0, 1, 0, 0, 0] }
     // Instruction { opcode: 565, args: [4, 4, 1780, 1, 0, 1, 0] }
 
@@ -71,4 +72,37 @@ fn load_two_bytes_compare() {
         bne(52, 56, 28),
     ];
     assert_machine_output(program.to_vec(), "load_two_bytes_compare");
+}
+
+#[test]
+fn memory_copy_block_from_ecc(){
+    // Block 0x20a3b0 of the ecc affine hint apc=100
+    // => Executed 595.8K times. Effectiveness: 3.39.
+//       LOADW rd_rs2_ptr = 60, rs1_ptr = 56, imm = 0, mem_as = 2, needs_write = 1, imm_sign = 0
+//   LOADW rd_rs2_ptr = 64, rs1_ptr = 56, imm = 4, mem_as = 2, needs_write = 1, imm_sign = 0
+//   LOADW rd_rs2_ptr = 68, rs1_ptr = 56, imm = 8, mem_as = 2, needs_write = 1, imm_sign = 0
+//   LOADW rd_rs2_ptr = 20, rs1_ptr = 56, imm = 12, mem_as = 2, needs_write = 1, imm_sign = 0
+//   STOREW rd_rs2_ptr = 60, rs1_ptr = 52, imm = 0, mem_as = 2, needs_write = 1, imm_sign = 0
+//   STOREW rd_rs2_ptr = 64, rs1_ptr = 52, imm = 4, mem_as = 2, needs_write = 1, imm_sign = 0
+//   STOREW rd_rs2_ptr = 68, rs1_ptr = 52, imm = 8, mem_as = 2, needs_write = 1, imm_sign = 0
+//   STOREW rd_rs2_ptr = 20, rs1_ptr = 52, imm = 12, mem_as = 2, needs_write = 1, imm_sign = 0
+//   ADD rd_ptr = 56, rs1_ptr = 56, rs2 = 16, rs2_as = 0
+//   ADD rd_ptr = 48, rs1_ptr = 48, rs2 = 16777200, rs2_as = 0
+//   ADD rd_ptr = 52, rs1_ptr = 52, rs2 = 16, rs2_as = 0
+
+    let program =[
+        loadw(60, 56, 0, 2, 1, 0),
+        loadw(64, 56, 4, 2, 1, 0),
+        loadw(68, 56, 8, 2, 1, 0),
+        loadw(20, 56, 12, 2, 1, 0),
+        storew(60, 52, 0, 2, 1, 0),
+        storew(64, 52, 4, 2, 1, 0),
+        storew(68, 52, 8, 2, 1, 0),
+        storew(20, 52, 12, 2, 1, 0),
+        add(56, 56, 16, 0),
+        add(48, 48, 16777200, 0),
+        add(52, 52, 16, 0),
+    ];
+    assert_machine_output(program.to_vec(), "memory_copy_block_from_ecc");
+
 }
