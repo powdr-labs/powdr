94dc32566a3544bb38b944760ebf7ba2c5333d1e
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2025-10-07 16:43:31 +0800

    ignore keccak plonk test due to debug assertion bug

--- openvm/src/lib.rs
+++ openvm/src/lib.rs
@@ -1198,6 +1198,7 @@ mod tests {
 
     // All gate constraints should be satisfied, but bus interactions are not implemented yet.
     #[test]
+    #[ignore = "Pending bug fix"]
     fn keccak_plonk_small_prove_mock() {
         let mut stdin = StdIn::default();
         stdin.write(&GUEST_KECCAK_ITER_SMALL);
