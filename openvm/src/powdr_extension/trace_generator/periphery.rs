646c2bc541d1407f9d580662636409a27e2c85fb
qwang98 <qian.wang.wg24@wharton.upenn.edu>
2025-10-15 09:39:21 +0200

    fixed periphery bug with comments

--- openvm/src/powdr_extension/trace_generator/periphery.rs
+++ openvm/src/powdr_extension/trace_generator/periphery.rs
@@ -8,7 +8,10 @@ use openvm_circuit_primitives::{
     bitwise_op_lookup::{
         BitwiseOperationLookupAir, BitwiseOperationLookupChip, SharedBitwiseOperationLookupChip,
     },
-    range_tuple::{RangeTupleCheckerAir, RangeTupleCheckerChip, SharedRangeTupleCheckerChip},
+    range_tuple::{
+        RangeTupleCheckerAir, RangeTupleCheckerBus, RangeTupleCheckerChip,
+        SharedRangeTupleCheckerChip,
+    },
     var_range::{
         SharedVariableRangeCheckerChip, VariableRangeCheckerAir, VariableRangeCheckerChip,
     },
@@ -74,11 +77,28 @@ impl PowdrPeripheryInstances {
                 tuple_range_checker: tuple_range_checker.clone(),
             },
             dummy: SharedPeripheryChips {
-                bitwise_lookup_8: bitwise_8
-                    .map(|bitwise_8| Arc::new(BitwiseOperationLookupChipGPU::new())),
+                // BitwiseLookupChipGPU is always initialized via `hybrid()` with a CPU chip in all available extensions of `SdkVmGpuBuilder::create_chip_complex()`.
+                // In case this changes in the future, `cpu_chip.unwrap()` will panic, and we can fix the code.
+                bitwise_lookup_8: bitwise_8.map(|bitwise_8| {
+                    Arc::new(BitwiseOperationLookupChipGPU::hybrid(Arc::new(
+                        BitwiseOperationLookupChip::new(
+                            bitwise_8.as_ref().cpu_chip.as_ref().unwrap().bus(),
+                        ),
+                    )))
+                }),
                 range_checker: range_checker.clone(),
+                // RangeTupleCheckerGPU is always initialized via `new()` without a CPU chip in all available extensions of `SdkVmGpuBuilder::create_chip_complex()`.
+                // In case this changes in the future the `Some` matching arm below will catch it.
                 tuple_range_checker: tuple_range_checker.map(|tuple_range_checker| {
-                    Arc::new(RangeTupleCheckerChipGPU::new(tuple_range_checker.sizes))
+                    Arc::new({
+                        match tuple_range_checker.cpu_chip.as_ref() {
+                            // None is the expected case
+                            None => RangeTupleCheckerChipGPU::new(tuple_range_checker.sizes),
+                            Some(cpu_chip) => RangeTupleCheckerChipGPU::hybrid(Arc::new(
+                                RangeTupleCheckerChip::new(cpu_chip.bus()),
+                            )),
+                        }
+                    })
                 }),
             },
         }
@@ -149,9 +169,16 @@ impl<SC: StarkGenericConfig> VmCircuitExtension<SC> for SharedPeripheryChips {
                 .find_air::<RangeTupleCheckerAir<2>>()
                 .next()
                 .is_none());
-            inventory.add_air(RangeTupleCheckerAir::<2> {
-                bus: *tuple_range_checker.cpu_chip.as_ref().unwrap().bus(),
-            });
+            // RangeTupleCheckerGPU is always initialized via `new()` without a CPU chip in all available extensions of `SdkVmGpuBuilder::create_chip_complex()`.
+            // Therefore we create a new bus index, following a similar scenario in `Rv32M::extend_circuit`.
+            let bus = match tuple_range_checker.cpu_chip.as_ref() {
+                // None is the expected case
+                None => {
+                    RangeTupleCheckerBus::new(inventory.new_bus_idx(), tuple_range_checker.sizes)
+                }
+                Some(cpu_chip) => *cpu_chip.bus(),
+            };
+            inventory.add_air(RangeTupleCheckerAir::<2> { bus });
         }
 
         // The range checker is already present in the builder because it's is used by the system, so we don't add it again.
