cb3c6eb0dcd60d6d49df0ac45369bce00d52114f
chriseth <chriseth.github@gmail.com>
2025-10-24 08:58:13 +0000

    Improve system splitter speed.

--- constraint-solver/src/system_splitter.rs
+++ constraint-solver/src/system_splitter.rs
@@ -11,9 +11,19 @@ use crate::{
 /// Splits the constraint system into independent subsets.
 /// Each variable occurs in exactly one subset and all constraints referencing a
 /// certain variable have to be in the same subsystem.
+/// Note that this ignores derived variables, i.e. the returned systems all have
+/// no derived variables.
 pub fn split_system<T: RuntimeConstant, V: Clone + Ord + Hash + Display>(
-    mut constraint_system: IndexedConstraintSystem<T, V>,
+    constraint_system: IndexedConstraintSystem<T, V>,
 ) -> Vec<ConstraintSystem<T, V>> {
+    // We cleanup and re-index the constraint system, otherwise we get too many
+    // empty systems due to variables that have already been substituted.\
+    let mut constraint_system: ConstraintSystem<T, V> = constraint_system.into();
+    constraint_system
+        .algebraic_constraints
+        .retain(|constr| !constr.is_redundant());
+    let constraint_system: IndexedConstraintSystem<T, V> = constraint_system.into();
+
     let mut systems = Vec::new();
     let mut remaining_variables: BTreeSet<_> = constraint_system.variables().cloned().collect();
 
@@ -33,22 +43,17 @@ pub fn split_system<T: RuntimeConstant, V: Clone + Ord + Hash + Display>(
                 }
             }
         }
-        constraint_system.retain_algebraic_constraints(|constr| {
-            !constr
-                .referenced_unknown_variables()
-                .any(|var| variables_to_extract.contains(var))
-        });
-        constraint_system.retain_bus_interactions(|constr| {
-            !constr
-                .referenced_unknown_variables()
-                .any(|var| variables_to_extract.contains(var))
-        });
         systems.push(ConstraintSystem {
             algebraic_constraints,
             bus_interactions,
-            derived_variables: Vec::new(), // TODO As long as we re-compine the system later on it does not matter much where we put them.
+            derived_variables: Vec::new(),
         });
-        remaining_variables.retain(|var| !variables_to_extract.contains(var));
+        // Fine to iterate over a hash set here since the order in which we remove
+        // is not relevant.
+        #[allow(clippy::iter_over_hash_type)]
+        for v in variables_to_extract {
+            remaining_variables.remove(&v);
+        }
     }
     systems
 }
