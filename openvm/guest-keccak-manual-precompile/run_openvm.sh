3451375d542077b168b291511c1921adbae0e3c4
Georg Wiese <georgwiese@gmail.com>
2025-10-31 12:09:46 +0100

    Don't comment out code

--- openvm/guest-keccak-manual-precompile/run_openvm.sh
+++ openvm/guest-keccak-manual-precompile/run_openvm.sh
@@ -1,5 +1,5 @@
-#cargo openvm build
-#cargo openvm keygen
+cargo openvm build
+cargo openvm keygen
 # Start with 0x01, then little-endian encoding of the number of hashes.
 # hex(10000) = 0x2710 -> little-endian 0x1027
 RUST_LOG=debug cargo openvm prove app --input "0x010102700000000000" > output.txt
