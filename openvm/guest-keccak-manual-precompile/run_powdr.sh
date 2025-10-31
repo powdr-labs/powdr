be34ec50ec3c6773398d3f164698c0397aa72ee9
Georg Wiese <georgwiese@gmail.com>
2025-10-31 14:08:43 +0100

    Upload logs

--- openvm/guest-keccak-manual-precompile/run_powdr.sh
+++ openvm/guest-keccak-manual-precompile/run_powdr.sh
@@ -1,4 +1,4 @@
 
 mkdir -p $1
-RUST_LOG=debug ../../target/release/powdr_openvm prove --input 256 $(pwd) > $1/output.txt
+RUST_LOG=debug ../../target/release/powdr_openvm prove --input 10000 $(pwd) > $1/output.txt
 ./parse_logs.sh $1
