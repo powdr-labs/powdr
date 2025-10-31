dc0f2b622406746895dcd99c0eb90d45dbc20440
Georg Wiese <georgwiese@gmail.com>
2025-10-31 14:49:24 +0100

    Add runs

--- openvm/guest-keccak/run_openvm.sh
+++ openvm/guest-keccak/run_openvm.sh
@@ -4,8 +4,8 @@ cargo openvm keygen
 mkdir -p $1
 # Start with 0x01, then little-endian encoding of the number of hashes.
 # hex(256) = 0x0100 -> little-endian 0x0001
-RUST_LOG=debug cargo openvm prove app --input "0x010001000000000000" > $1/output.txt
+#RUST_LOG=debug cargo openvm prove app --input "0x010001000000000000" > $1/output.txt
 # hex(10000) = 0x2710 -> little-endian 0x1027
-#RUST_LOG=debug cargo openvm prove app --input "0x011027000000000000" > $1/output.txt
+RUST_LOG=debug cargo openvm prove app --input "0x011027000000000000" > $1/output.txt
 
 ./parse_logs.sh $1
