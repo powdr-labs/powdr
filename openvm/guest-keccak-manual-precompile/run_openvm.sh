26c986374a3094fcae9bed4b52878b5fac87baa9
Georg Wiese <georgwiese@gmail.com>
2025-10-31 12:24:01 +0100

    Separate files

--- openvm/guest-keccak-manual-precompile/run_openvm.sh
+++ openvm/guest-keccak-manual-precompile/run_openvm.sh
@@ -3,10 +3,11 @@ cargo openvm keygen
 # Start with 0x01, then little-endian encoding of the number of hashes.
 # hex(10000) = 0x2710 -> little-endian 0x1027
 # hex(256) = 0x0100 -> little-endian 0x0001
-RUST_LOG=debug cargo openvm prove app --input "0x010001000000000000" > $1.txt
+mkdir -p $1
+RUST_LOG=debug cargo openvm prove app --input "0x010001000000000000" > $1/output.txt
 
 echo "\n\nTrace cells:"
-grep "Cells =" $1.txt
+grep "Cells =" $1/output.txt > $1/cells.txt
 
 echo "\n\nSegment proving times:"
-grep "prove_segment" $1.txt
+grep "prove_segment" $1/output.txt > $1/prove_times.txt
