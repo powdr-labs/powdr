02d6a4883c1779721cb81d4b71c10cd24034620d
Georg Wiese <georgwiese@gmail.com>
2025-10-31 12:31:01 +0100

    Change back to 10k & openvm sdk

--- openvm/guest-keccak-manual-precompile/run_openvm.sh
+++ openvm/guest-keccak-manual-precompile/run_openvm.sh
@@ -1,10 +1,12 @@
 cargo openvm build
 cargo openvm keygen
-# Start with 0x01, then little-endian encoding of the number of hashes.
-# hex(10000) = 0x2710 -> little-endian 0x1027
-# hex(256) = 0x0100 -> little-endian 0x0001
+
 mkdir -p $1
-RUST_LOG=debug cargo openvm prove app --input "0x010001000000000000" > $1/output.txt
+# Start with 0x01, then little-endian encoding of the number of hashes.
+# hex(256) = 0x0100 -> little-endian 0x0001
+#RUST_LOG=debug cargo openvm prove app --input "0x010001000000000000" > $1/output.txt
+# hex(10000) = 0x2710 -> little-endian 0x1027
+RUST_LOG=debug cargo openvm prove app --input "0x011027000000000000" > $1/output.txt
 
 grep "Cells =" $1/output.txt > $1/cells.txt
 grep "prove_segment" $1/output.txt > $1/prove_times.txt
