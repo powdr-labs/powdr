98bbe07bd1067c2b40a1eb754aa895a66f251884
chriseth <chriseth.github@gmail.com>
2025-11-17 20:13:22 +0000

    Try out egglog.

--- autoprecompiles/src/constraint_optimizer.rs
+++ autoprecompiles/src/constraint_optimizer.rs
@@ -23,6 +23,7 @@ use crate::{
     low_degree_bus_interaction_optimizer::LowDegreeBusInteractionOptimizer,
     memory_optimizer::{optimize_memory, MemoryBusInteraction},
     range_constraint_optimizer::RangeConstraintHandler,
+    rule_based_optimizer::rule_based_optimization,
     stats_logger::StatsLogger,
 };
 
@@ -60,6 +61,10 @@ pub fn optimize_constraints<
 ) -> Result<ConstraintSystem<P, V>, Error> {
     // Index the constraint system for the first time
     let constraint_system = IndexedConstraintSystem::from(constraint_system);
+
+    let constraint_system =
+        rule_based_optimization(constraint_system, bus_interaction_handler.clone());
+    stats_logger.log("rule-based optimization", &constraint_system);
 
     let constraint_system = solver_based_optimization(constraint_system, solver)?;
     stats_logger.log("solver-based optimization", &constraint_system);
