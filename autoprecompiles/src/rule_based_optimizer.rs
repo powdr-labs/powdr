dc32b16ad95494f03b8833fa5df7da0addcfabbc
chriseth <chriseth.github@gmail.com>
2025-11-27 00:50:43 +0000

    Rename system to environment.

--- autoprecompiles/src/rule_based_optimizer.rs
+++ autoprecompiles/src/rule_based_optimizer.rs
@@ -84,9 +84,7 @@ impl<E: Clone + Hash + Eq> ExpressionDB<E> {
     }
 }
 
-// TODO rename this "Environment"
-
-struct System<T: FieldElement> {
+struct Environment<T: FieldElement> {
     expressions: RefCell<ExpressionDB<GroupedExpression<T, Var>>>,
     var_to_string: HashMap<Var, String>,
 
@@ -97,7 +95,7 @@ struct System<T: FieldElement> {
     new_var_generator: RefCell<NewVarGenerator<T>>,
 }
 
-impl<T: FieldElement> System<T> {
+impl<T: FieldElement> Environment<T> {
     fn new(
         expressions: ExpressionDB<GroupedExpression<T, Var>>,
         var_to_string: HashMap<Var, String>,
@@ -122,36 +120,36 @@ impl<T: FieldElement> System<T> {
     }
 }
 
-impl<T: FieldElement> PartialEq for System<T> {
+impl<T: FieldElement> PartialEq for Environment<T> {
     fn eq(&self, _other: &Self) -> bool {
-        // TODO change this as soon as we have different systems
+        // TODO change this as soon as we have different environments
         true
     }
 }
 
-impl<T: FieldElement> Eq for System<T> {}
+impl<T: FieldElement> Eq for Environment<T> {}
 
-impl<T: FieldElement> PartialOrd for System<T> {
+impl<T: FieldElement> PartialOrd for Environment<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<T: FieldElement> Ord for System<T> {
+impl<T: FieldElement> Ord for Environment<T> {
     fn cmp(&self, _other: &Self) -> std::cmp::Ordering {
-        // TODO change this as soon as we have different systems
+        // TODO change this as soon as we have different environments
         std::cmp::Ordering::Equal
     }
 }
 
-impl<T: FieldElement> Hash for System<T> {
+impl<T: FieldElement> Hash for Environment<T> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        // TODO change this as soon as we have different systems
+        // TODO change this as soon as we have different environments
         0.hash(state);
     }
 }
 
-impl<T: FieldElement> System<T> {
+impl<T: FieldElement> Environment<T> {
     pub fn insert(&self, expr: &GroupedExpression<T, Var>) -> Expr {
         self.expressions.borrow_mut().insert(expr)
     }
@@ -359,7 +357,7 @@ impl<T> NewVarGenerator<T> {
     }
 }
 
-impl<T: FieldElement> RangeConstraintProvider<T, Var> for System<T> {
+impl<T: FieldElement> RangeConstraintProvider<T, Var> for Environment<T> {
     fn get(&self, var: &Var) -> RangeConstraint<T> {
         self.range_constraints_on_vars
             .get(var)
@@ -380,7 +378,7 @@ enum Action<T> {
 
 crepe! {
     @input
-    struct S<'a, T: FieldElement>(&'a System<T>);
+    struct Env<'a, T: FieldElement>(&'a Environment<T>);
 
     @input
     struct InitialAlgebraicConstraint(Expr);
@@ -398,25 +396,25 @@ crepe! {
     Expression(e) <- RangeConstraintOnExpression(e, _);
 
     RangeConstraintOnExpression(e, rc) <-
-      S(sys),
+      Env(env),
       Expression(e),
-      let rc = sys.range_constraint_on_expr(e);
+      let rc = env.range_constraint_on_expr(e);
 
 
     struct ContainsVariable(Expr, Var);
     ContainsVariable(e, v) <-
       Expression(e),
-      S(sys),
-      for v in sys.referenced_variables(e);
+      Env(env),
+      for v in env.referenced_variables(e);
 
     struct AffineExpression<T: FieldElement>(Expr, T, Var, T);
     AffineExpression(e, coeff, var, offset) <-
       Expression(e),
-      S(sys),
-      let Some((coeff, var, offset)) = sys.try_to_affine(e);
+      Env(env),
+      let Some((coeff, var, offset)) = env.try_to_affine(e);
 
     struct RangeConstraintOnVar<T: FieldElement>(Var, RangeConstraint<T>);
-    RangeConstraintOnVar(v, rc) <- S(sys), ContainsVariable(_, v), let rc = sys.get(&v);
+    RangeConstraintOnVar(v, rc) <- Env(env), ContainsVariable(_, v), let rc = env.get(&v);
     // RC(coeff * var + offset) = rc <=>
     // coeff * RC(var) + offset = rc <=>
     // RC(var) = (rc - offset) / coeff
@@ -432,25 +430,25 @@ crepe! {
     struct Product(Expr, Expr, Expr);
     Product(e, l, r) <-
       Expression(e),
-      S(sys),
-      let Some((l, r)) = sys.try_as_single_product(e);
+      Env(env),
+      let Some((l, r)) = env.try_as_single_product(e);
     Product(e, r, l) <- Product(e, l, r);
 
     // (E, expr, offset) <-> E = (expr) * (expr + offset) is a constraint
     struct QuadraticEquivalenceCandidate<T: FieldElement>(Expr, Expr, T);
     QuadraticEquivalenceCandidate(e, r, offset) <-
        AlgebraicConstraint(e),
-       S(sys),
+       Env(env),
        Product(e, l, r),
-       ({sys.affine_var_count(l).unwrap_or(0) > 1 && sys.affine_var_count(r).unwrap_or(0) > 1}),
-       let Some(offset) = sys.constant_difference(l, r);
+       ({env.affine_var_count(l).unwrap_or(0) > 1 && env.affine_var_count(r).unwrap_or(0) > 1}),
+       let Some(offset) = env.constant_difference(l, r);
 
     struct QuadraticEquivalence(Var, Var);
     QuadraticEquivalence(v1, v2) <-
       QuadraticEquivalenceCandidate(_, expr1, offset),
       QuadraticEquivalenceCandidate(_, expr2, offset),
-      S(sys),
-      let Some((v1, v2, coeff)) = sys.differ_in_exactly_one_variable(expr1, expr2),
+      Env(env),
+      let Some((v1, v2, coeff)) = env.differ_in_exactly_one_variable(expr1, expr2),
       RangeConstraintOnVar(v1, rc),
       RangeConstraintOnVar(v2, rc),
       (rc.is_disjoint(&rc.combine_sum(&RangeConstraint::from_value(offset / coeff))));
@@ -474,31 +472,31 @@ crepe! {
     // if that value is still small enough.
     struct SingleOccurrenceVariable(Expr, Var);
     SingleOccurrenceVariable(e, v) <-
-      S(sys),
-      for v in sys.single_occurrence_variables().cloned(),
+      Env(env),
+      for v in env.single_occurrence_variables().cloned(),
       AlgebraicConstraint(e),
       ContainsVariable(e, v2),
       (v == v2);
 
     struct LargestSingleOccurrenceVariablePairInExpr(Expr, Var, Var);
     LargestSingleOccurrenceVariablePairInExpr(e, v1, v2) <-
-      S(sys),
+      Env(env),
       SingleOccurrenceVariable(e, v1),
       SingleOccurrenceVariable(e, v2),
       (v1 < v2),
-      (sys
+      (env
         .single_occurrence_variables()
-        .filter(|v3| sys.referenced_variables(e).contains(v3))
+        .filter(|v3| env.referenced_variables(e).contains(v3))
         .all(|&v3| v3 == v1 || v3 == v2 || v3 < v1));
 
     struct HasProductSummand(Expr, Expr, Expr);
     HasProductSummand(e, l, r) <-
-      S(sys),
+      Env(env),
       Expression(e),
-      (!sys.on_expr(e, (), |e, _| e.is_affine())),
-      for (l, r) in sys.extract(e).into_summands().filter_map(|s| {
+      (!env.on_expr(e, (), |e, _| e.is_affine())),
+      for (l, r) in env.extract(e).into_summands().filter_map(|s| {
           if let GroupedExpressionComponent::Quadratic(l, r) = s {
-              Some((sys.insert_owned(l), sys.insert_owned(r)))
+              Some((env.insert_owned(l), env.insert_owned(r)))
           } else {
               None
           }
@@ -532,7 +530,7 @@ crepe! {
     struct PotentiallyReplaceAlgebraicConstraintBy(Expr, Expr);
 
     PotentiallyReplaceAlgebraicConstraintBy(e, replacement) <-
-      S(sys),
+      Env(env),
       FreeVariableCombinationCandidate(e, coeff1, v1, x1, coeff2, v2, x2),
       // Here, we have e = coeff1 * v1 * x1 + coeff2 * v2 * x2 + ...
       RangeConstraintOnExpression(x1, rc1),
@@ -557,14 +555,14 @@ crepe! {
         if !(sum_rc.range().0.is_zero() && sum_rc.range().1 < T::from(-1)) {
             return None;
         }
-        let e = sys.extract(e);
-        let x1 = sys.extract(x1);
+        let e = env.extract(e);
+        let x1 = env.extract(x1);
         let x1 = if x1_needs_squaring {
             x1.clone() * x1
         } else {
             x1
         };
-        let x2 = sys.extract(x2);
+        let x2 = env.extract(x2);
         let x2 = if x2_needs_squaring {
             x2.clone() * x2
         } else {
@@ -580,9 +578,9 @@ crepe! {
             true
         }).map(GroupedExpression::from).sum::<GroupedExpression<T, Var>>();
         let factor = x1 * coeff1 + x2 * coeff2;
-        let combined_var = sys.new_var("free_var", ComputationMethod::QuotientOrZero(-r.clone(), factor.clone()));
+        let combined_var = env.new_var("free_var", ComputationMethod::QuotientOrZero(-r.clone(), factor.clone()));
         let replacement = r + GroupedExpression::from_unknown_variable(combined_var) * factor;
-        Some(sys.insert_owned(replacement))
+        Some(env.insert_owned(replacement))
       })();
 
     // TODO wait a second. We can craete range constraints on expressions for all
@@ -610,13 +608,13 @@ crepe! {
 
     // Do not do this because it is rather expensive.
 
-    ReplaceAlgebraicConstraintBy(e, sys.substitute_by_known(e, v, val)) <-
-      S(sys),
+    ReplaceAlgebraicConstraintBy(e, env.substitute_by_known(e, v, val)) <-
+      Env(env),
       AlgebraicConstraint(e),
       ContainsVariable(e, v),
       Assignment(v, val);
-    ReplaceAlgebraicConstraintBy(e, sys.substitute_by_var(e, v, v2)) <-
-       S(sys),
+    ReplaceAlgebraicConstraintBy(e, env.substitute_by_var(e, v, v2)) <-
+       Env(env),
        AlgebraicConstraint(e),
        ContainsVariable(e, v),
        Equivalence(v, v2);
@@ -666,7 +664,7 @@ pub fn rule_based_optimization<T: FieldElement, V: Hash + Eq + Ord + Clone + Dis
         let (algebraic_constraints, _bus_interactions) =
             transform_constraint_system(&system, &var_mapper, db.as_mut().unwrap());
 
-        let sys = System::<T>::new(
+        let env = Environment::<T>::new(
             db.take().unwrap(),
             var_mapper.all_names(),
             system
@@ -688,10 +686,10 @@ pub fn rule_based_optimization<T: FieldElement, V: Hash + Eq + Ord + Clone + Dis
                 .copied()
                 .map(InitialAlgebraicConstraint),
         );
-        rt.extend(std::iter::once(S(&sys)));
+        rt.extend(std::iter::once(Env(&env)));
 
         let (actions,) = rt.run();
-        let (expr_db, new_var_generator) = sys.terminate();
+        let (expr_db, new_var_generator) = env.terminate();
         db = Some(expr_db);
 
         // TODO we do not need all of those variables.
