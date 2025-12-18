48b23122abba36dd5b2e6fdd4c4637c295d93b01
chriseth <chriseth.github@gmail.com>
2025-12-18 16:51:41 +0000

    update expectations.

--- openvm/tests/optimizer.rs
+++ openvm/tests/optimizer.rs
@@ -92,11 +92,11 @@ fn test_ecrecover() {
     // This cbor file above has the `is_valid` column removed, this is why the number below
     // might be one less than in other tests.
     expect![[r#"
-        2854
+        2852
     "#]]
     .assert_debug_eq(&machine.main_columns().count());
     expect![[r#"
-        1621
+        1619
     "#]]
     .assert_debug_eq(&machine.bus_interactions.len());
     expect![[r#"
