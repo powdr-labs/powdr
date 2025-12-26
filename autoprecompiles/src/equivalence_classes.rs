0ed2ae05e63a62a87f78ca8d91b899b643485b20
Leo Alt <leo@powdrlabs.com>
2025-12-27 00:20:03 +0100

    Make partitions disjoint by construction

--- autoprecompiles/src/equivalence_classes.rs
+++ autoprecompiles/src/equivalence_classes.rs
@@ -5,17 +5,11 @@ use serde::{Deserialize, Deserializer, Serialize};
 /// An equivalence class, i.e, a set of values of type `T` which are considered equivalent
 pub type EquivalenceClass<T> = BTreeSet<T>;
 
-/// A collection of equivalence classes where all classes are guaranteed to have at least one element
-/// and no element can appear in more than one class.
+/// A collection of equivalence classes where all classes are non-empty
+/// and each element appears in exactly one class.
 #[derive(Serialize, Debug, PartialEq, Eq, Clone)]
 pub struct Partition<T> {
     inner: BTreeSet<EquivalenceClass<T>>,
-}
-
-impl<T: Ord> FromIterator<EquivalenceClass<T>> for Partition<T> {
-    fn from_iter<I: IntoIterator<Item = EquivalenceClass<T>>>(iter: I) -> Self {
-        Self::from_classes(iter)
-    }
 }
 
 impl<T> Partition<T> {
@@ -29,31 +23,36 @@ impl<T: Ord> Partition<T> {
     where
         I: IntoIterator<Item = EquivalenceClass<T>>,
     {
-        let mut merged: Vec<EquivalenceClass<T>> = Vec::new();
+        let mut uf = UnionFind::default();
+        let mut index = BTreeMap::<T, usize>::new();
 
-        for mut class in classes {
-            if class.is_empty() {
+        for class in classes {
+            let mut iter = class.into_iter();
+            let Some(first) = iter.next() else {
                 continue;
+            };
+            let first_id = intern(first, &mut uf, &mut index);
+            for item in iter {
+                let id = intern(item, &mut uf, &mut index);
+                uf.union(first_id, id);
             }
+        }
 
-            let mut i = 0;
-            while i < merged.len() {
-                if class.is_disjoint(&merged[i]) {
-                    i += 1;
-                    continue;
-                }
-
-                let mut existing = std::mem::take(&mut merged[i]);
-                class.append(&mut existing);
-                merged.swap_remove(i);
-            }
-
-            merged.push(class);
+        let mut grouped: BTreeMap<usize, EquivalenceClass<T>> = BTreeMap::new();
+        for (item, id) in index {
+            let root = uf.find(id);
+            grouped.entry(root).or_default().insert(item);
         }
 
         Self {
-            inner: merged.into_iter().collect(),
+            inner: grouped.into_values().collect(),
         }
+    }
+}
+
+impl<T: Ord> FromIterator<EquivalenceClass<T>> for Partition<T> {
+    fn from_iter<I: IntoIterator<Item = EquivalenceClass<T>>>(iter: I) -> Self {
+        Self::from_classes(iter)
     }
 }
 
@@ -65,12 +64,22 @@ where
     where
         D: Deserializer<'de>,
     {
-        let classes = BTreeSet::<EquivalenceClass<T>>::deserialize(deserializer)?;
+        let classes = Vec::<EquivalenceClass<T>>::deserialize(deserializer)?;
         Ok(Self::from_classes(classes))
     }
 }
 
 impl<T: Ord + Clone> Partition<T> {
+    fn class_index(&self) -> BTreeMap<T, usize> {
+        let mut map = BTreeMap::new();
+        for (class_idx, class) in self.inner.iter().enumerate() {
+            for item in class {
+                map.insert(item.clone(), class_idx);
+            }
+        }
+        map
+    }
+
     /// Intersects multiple partitions of the same universe into a single partition.
     /// In other words, two elements are in the same equivalence class in the resulting partition
     /// if and only if they are in the same equivalence class in all input partitions.
@@ -84,19 +93,8 @@ impl<T: Ord + Clone> Partition<T> {
             return partitions[0].clone();
         }
 
-        let class_ids: Vec<BTreeMap<&T, usize>> = partitions
-            .iter()
-            .map(|partition| {
-                let mut map = BTreeMap::new();
-                for (class_idx, class) in partition.iter().enumerate() {
-                    for item in class {
-                        let previous = map.insert(item, class_idx);
-                        debug_assert!(previous.is_none(), "Element appears in multiple classes");
-                    }
-                }
-                map
-            })
-            .collect();
+        let class_ids: Vec<BTreeMap<T, usize>> =
+            partitions.iter().map(|partition| partition.class_index()).collect();
 
         let base = class_ids
             .iter()
@@ -105,7 +103,7 @@ impl<T: Ord + Clone> Partition<T> {
 
         let mut grouped: BTreeMap<Vec<usize>, EquivalenceClass<T>> = BTreeMap::new();
 
-        for item in base.keys().copied() {
+        for item in base.keys() {
             let mut signature = Vec::with_capacity(class_ids.len());
             let mut in_all = true;
             for map in &class_ids {
@@ -120,13 +118,52 @@ impl<T: Ord + Clone> Partition<T> {
                 continue;
             }
 
-            grouped
-                .entry(signature)
-                .or_default()
-                .insert(item.clone());
+            grouped.entry(signature).or_default().insert(item.clone());
         }
 
         Self::from_classes(grouped.into_values())
+    }
+}
+
+fn intern<T: Ord>(value: T, uf: &mut UnionFind, index: &mut BTreeMap<T, usize>) -> usize {
+    if let Some(&id) = index.get(&value) {
+        return id;
+    }
+
+    let id = uf.make_set();
+    index.insert(value, id);
+    id
+}
+
+#[derive(Default)]
+struct UnionFind {
+    parent: Vec<usize>,
+}
+
+impl UnionFind {
+    fn make_set(&mut self) -> usize {
+        let id = self.parent.len();
+        self.parent.push(id);
+        id
+    }
+
+    fn find(&mut self, id: usize) -> usize {
+        let parent = self.parent[id];
+        if parent == id {
+            id
+        } else {
+            let root = self.find(parent);
+            self.parent[id] = root;
+            root
+        }
+    }
+
+    fn union(&mut self, a: usize, b: usize) {
+        let root_a = self.find(a);
+        let root_b = self.find(b);
+        if root_a != root_b {
+            self.parent[root_b] = root_a;
+        }
     }
 }
 
