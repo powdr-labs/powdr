4f00b019654c8e4257931d56d1ffad8a99441349
chriseth <chriseth.github@gmail.com>
2026-01-05 23:13:15 +0000

    detect more

--- constraint-solver/src/rule_based_optimizer/rules.rs
+++ constraint-solver/src/rule_based_optimizer/rules.rs
@@ -363,17 +363,25 @@ crepe! {
     //   and diff_marker_expr is of the form `1 - diff_marker1 - diff_marker2 - ...`
     //   such that we have n variables and there is another
     //   NegatedDiffMarkerConstraint with n-1 variables used to derive this one.
-    struct NegatedDiffMarkerConstraintFinal(Expr, Var, Expr, Var, u32);
-    NegatedDiffMarkerConstraintFinal(e, diff_marker, l, result, n + 1) <-
+    struct NegatedDiffMarkerConstraintFinal(Expr, Var, Var, u32);
+    NegatedDiffMarkerConstraintFinal(e, diff_marker, result, n + 1) <-
       ProductConstraint(e, l, r),
         NegatedDiffMarkerConstraint(_, _, diff_marker_expr2, _, result, n),
         SplitLinearSummand(l, diff_marker_expr2, T::from(-1), diff_marker),
       LinearExpression(r, result, T::from(1));
 
+    struct NegatedDiffMarkerConstraintFinalNegated(Expr, Var, Var, Var, u32);
+    NegatedDiffMarkerConstraintFinalNegated(e, diff_marker, v, result, n + 1) <-
+      ProductConstraint(e, l, r),
+        NegatedDiffMarkerConstraint(_, _, diff_marker_expr2, _, result, n),
+        SplitLinearSummand(l, diff_marker_expr2, T::from(-1), diff_marker),
+      PlusMinusResult(r, r2, result),
+      AffineExpression(r2, T::from(-1), v, T::from(1));
+
     // EqualZeroCheck(constrs, result, vars) =>
     //   constrsexprs can be equivalently replaced by a constraint that models
     //   result = 1 if all vars are zero, and result = 0 otherwise.
-    struct EqualZeroCheck([Expr; 8], Var, [Var; 4]);
+    struct EqualZeroCheck([Expr; 9], Var, [Var; 4]);
     EqualZeroCheck(constrs, result, vars) <-
       // (1 - diff_marker__3_0) * (a__3_0 * (2 * cmp_result_0 - 1)) = 0
       NegatedDiffMarkerConstraint(constr_0, diff_marker_3, _, a_3, result, 0),
@@ -382,28 +390,30 @@ crepe! {
       // (1 - (diff_marker__1_0 + diff_marker__2_0 + diff_marker__3_0)) * (a__1_0 * (2 * cmp_result_0 - 1)) = 0
       NegatedDiffMarkerConstraint(constr_2, diff_marker_1, _, a_1, result, 2),
       // (1 - (diff_marker__0_0 + diff_marker__1_0 + diff_marker__2_0 + diff_marker__3_0)) * cmp_result_0 = 0
-      NegatedDiffMarkerConstraintFinal(constr_3, diff_marker_0, _, result, 3),
+      NegatedDiffMarkerConstraintFinal(constr_3, diff_marker_0, result, 3),
+      // (1 - (diff_marker__0_0 + diff_marker__1_0 + diff_marker__2_0 + diff_marker__3_0)) * ((1 - a__0_0) * (2 * cmp_result_0 - 1)) = 0
+      NegatedDiffMarkerConstraintFinalNegated(constr_4, diff_marker_0, a_0, result, 3),
       // diff_marker__0_0 * ((a__0_0 - 1) * (2 * cmp_result_0 - 1) + diff_val_0) = 0
-      DiffMarkerConstraint(constr_4, diff_marker_0, a_0_e, result, diff_val),
+      DiffMarkerConstraint(constr_5, diff_marker_0, a_0_e, result, diff_val),
         AffineExpression(a_0_e, a_0_e_coeff, a_0, a_0_e_offset), (a_0_e_coeff == T::from(1)), (a_0_e_offset == T::from(-1)),
       // diff_marker__1_0 * (a__1_0 * (2 * cmp_result_0 - 1) + diff_val_0) = 0
-      DiffMarkerConstraint(constr_5, diff_marker_1, a_1_e, result, diff_val),
+      DiffMarkerConstraint(constr_6, diff_marker_1, a_1_e, result, diff_val),
         LinearExpression(a_1_e, a_1, a_coeff), (a_coeff == T::from(1)),
       // diff_marker__2_0 * (a__2_0 * (2 * cmp_result_0 - 1) + diff_val_0) = 0
-      DiffMarkerConstraint(constr_6, diff_marker_2, a_2_e, result, diff_val),
+      DiffMarkerConstraint(constr_7, diff_marker_2, a_2_e, result, diff_val),
         LinearExpression(a_2_e, a_2, a_coeff2), (a_coeff2 == T::from(1)),
       // diff_marker__3_0 * (a__3_0 * (2 * cmp_result_0 - 1) + diff_val_0) = 0
-      DiffMarkerConstraint(constr_7, diff_marker_3, a_3_e, result, diff_val),
+      DiffMarkerConstraint(constr_8, diff_marker_3, a_3_e, result, diff_val),
         LinearExpression(a_3_e, a_3, a_coeff3), (a_coeff3 == T::from(1)),
       BooleanVar(result),
       BooleanVar(diff_marker_0),
       BooleanVar(diff_marker_1),
       BooleanVar(diff_marker_2),
       BooleanVar(diff_marker_3),
-      // (1 * is_valid - (diff_marker__0_0 + diff_marker__1_0 + diff_marker__2_0 + diff_marker__3_0)) * ((1 - a__0_0) * (2 * cmp_result_0 - 1)) = 0
+      // TODO detect these:
       // (diff_marker__0_0 + diff_marker__1_0 + diff_marker__2_0 + diff_marker__3_0) * (diff_marker__0_0 + diff_marker__1_0 + diff_marker__2_0 + diff_marker__3_0 - 1) = 0
       // (1 - is_valid) * (diff_marker__0_0 + diff_marker__1_0 + diff_marker__2_0 + diff_marker__3_0) = 0
-      let constrs = [constr_0, constr_1, constr_2, constr_3, constr_4, constr_5, constr_6, constr_7],
+      let constrs = [constr_0, constr_1, constr_2, constr_3, constr_4, constr_5, constr_6, constr_7, constr_8],
       let vars = [a_0, a_1, a_2, a_3],
       Env(env),
       ({println!("Found EqualZeroCheck on {} with result {:?}", vars.iter().map(|v| env.format_var(*v)).format(", "), result); true})
