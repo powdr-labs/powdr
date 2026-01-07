fca5b14586c0de29eb0af0eed423e4ffc8c31eaa
ShuangWu121 <shuang.wu@posteo.net>
2026-01-07 15:39:21 +0100

    add comment

--- constraint-solver/src/rule_based_optimizer/rules.rs
+++ constraint-solver/src/rule_based_optimizer/rules.rs
@@ -180,6 +180,7 @@ crepe! {
       (coeff != T::zero());
 
     //////////////////// RANGE PROPERTIES OF EXPRESSIONS //////////////////////
+    // NonNegativeExpression(e) => range of e is [0, a] for some a < P - 1
     struct NonNegativeExpression(Expr);
     NonNegativeExpression(e) <-
       RangeConstraintOnExpression(e, e_rc),
