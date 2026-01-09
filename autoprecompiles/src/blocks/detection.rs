20fa39ad552dfa7c098db92523e992b86e96226a
Georg Wiese <georgwiese@gmail.com>
2026-01-09 10:29:11 -0500

    Add BasicBlock::pc_step

--- autoprecompiles/src/blocks/detection.rs
+++ autoprecompiles/src/blocks/detection.rs
@@ -13,8 +13,10 @@ pub fn collect_basic_blocks<A: Adapter>(
     instruction_handler: &A::InstructionHandler,
 ) -> Vec<BasicBlock<A::Instruction>> {
     let mut blocks = Vec::new();
+    let pc_step = program.pc_step();
     let mut curr_block = BasicBlock {
         start_pc: program.instruction_index_to_pc(0),
+        pc_step,
         statements: Vec::new(),
     };
     for (i, instr) in program.instructions().enumerate() {
@@ -31,11 +33,13 @@ pub fn collect_basic_blocks<A: Adapter>(
             // Push the instruction itself
             blocks.push(BasicBlock {
                 start_pc: program.instruction_index_to_pc(i),
+                pc_step,
                 statements: vec![instr.clone()],
             });
             // Skip the instruction and start a new block from the next instruction.
             curr_block = BasicBlock {
                 start_pc: program.instruction_index_to_pc(i + 1),
+                pc_step,
                 statements: Vec::new(),
             };
         } else {
@@ -47,6 +51,7 @@ pub fn collect_basic_blocks<A: Adapter>(
                 }
                 curr_block = BasicBlock {
                     start_pc: program.instruction_index_to_pc(i),
+                    pc_step,
                     statements: Vec::new(),
                 };
             }
@@ -57,6 +62,7 @@ pub fn collect_basic_blocks<A: Adapter>(
                 blocks.push(curr_block); // guaranteed to be non-empty because an instruction was just pushed
                 curr_block = BasicBlock {
                     start_pc: program.instruction_index_to_pc(i + 1),
+                    pc_step,
                     statements: Vec::new(),
                 };
             }
