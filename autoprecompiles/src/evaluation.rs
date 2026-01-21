ef0eee772987a074d6a83f29e5f0c57c3154c879
Thibaut Schaeffer <schaeffer.thibaut@gmail.com>
2026-01-21 13:57:25 +0100

    Make stats non-optional (#3570)
    Changes in this PR:
    - Use `DegreeBound` everywhere possible instead of `max_degree: usize`
    where it's unclear what is meant
    - Make stats non optional: before this PR, we only compute the stats in
    `Cell` mode, because they are used for the apc selection. After this PR,
    we also compute the stats in other PGO modes, it simply happens after
    selection.
    - Simplify some of the APIs
    - Add a trait method to `Adapter` to generate the stats. This is because
    stats are client specific.
    - Generalize usage of `ApcWithStats` to simplify function signatures.
    Basically, inside the apc engine, we pass around the apc with its stats.
    - Make the `InstructionHandler` specific to the degree bound. This
    avoids passing around the degree bound, it can instead be recovered from
    the instruction handler.

--- autoprecompiles/src/evaluation.rs
+++ autoprecompiles/src/evaluation.rs
@@ -1,6 +1,9 @@
-use std::{fmt::Display, iter::Sum, ops::Add};
+use std::{fmt::Display, iter::Sum, ops::Add, sync::Arc};
 
-use crate::{blocks::Instruction, InstructionHandler, SymbolicMachine};
+use crate::{
+    adapter::{Adapter, AdapterApc, AdapterApcWithStats, AdapterBasicBlock},
+    InstructionHandler, SymbolicMachine,
+};
 
 use serde::{Deserialize, Serialize};
 
@@ -18,7 +21,7 @@ pub struct AirStats {
 }
 
 impl AirStats {
-    pub fn new<F: Clone + Ord + std::fmt::Display>(machine: &SymbolicMachine<F>) -> Self {
+    pub fn new<F>(machine: &SymbolicMachine<F>) -> Self {
         Self {
             main_columns: machine.main_columns().count(),
             constraints: machine.constraints.len(),
@@ -57,22 +60,24 @@ pub struct EvaluationResult {
 
 /// Evaluate an APC by comparing its cost to the cost of executing the
 /// basic block in software.
-pub fn evaluate_apc<IH>(
-    basic_block: &[IH::Instruction],
-    instruction_handler: &IH,
-    machine: &SymbolicMachine<impl Clone + Ord + std::fmt::Display>,
-) -> EvaluationResult
-where
-    IH: InstructionHandler,
-    IH::Field: Clone + Ord + std::fmt::Display,
-    IH::Instruction: Instruction<IH::Field>,
-{
+/// This is used by different pgo strategies in different stages. For example, for cell PGO, this is done before selection, and for instruction PGO, it is done after.
+pub fn evaluate_apc<A: Adapter>(
+    basic_block: AdapterBasicBlock<A>,
+    instruction_handler: &A::InstructionHandler,
+    apc: AdapterApc<A>,
+) -> AdapterApcWithStats<A> {
     let before = basic_block
+        .statements
         .iter()
         .map(|instruction| instruction_handler.get_instruction_air_stats(instruction))
         .sum();
-    let after = AirStats::new(machine);
-    EvaluationResult { before, after }
+    let after = AirStats::new(apc.machine());
+    let evaluation_result = EvaluationResult { before, after };
+
+    let apc = Arc::new(apc);
+    let apc_stats = A::apc_stats(apc.clone(), instruction_handler);
+
+    AdapterApcWithStats::<A>::new(apc, apc_stats, evaluation_result)
 }
 
 impl Display for EvaluationResult {
