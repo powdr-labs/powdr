b683ffdff015a98bd7cda9dca7050df33d9ef3c2
chriseth <chriseth.github@gmail.com>
2026-02-23 14:38:50 +0000

    Re-create new variables in a deterministic order.

--- constraint-solver/src/rule_based_optimizer/new_var_generator.rs
+++ constraint-solver/src/rule_based_optimizer/new_var_generator.rs
@@ -5,10 +5,15 @@ use crate::{
     rule_based_optimizer::types::Var,
 };
 
+pub struct NewVarRequest<T> {
+    pub final_id: Option<Var>,
+    pub prefix: String,
+    pub computation_method: ComputationMethod<T, GroupedExpression<T, Var>>,
+}
+
 pub struct NewVarGenerator<T> {
     counter: usize,
-    requests: Vec<(Var, String)>,
-    computation_methods: HashMap<Var, ComputationMethod<T, GroupedExpression<T, Var>>>,
+    requests: HashMap<Var, NewVarRequest<T>>,
 }
 
 impl<T> NewVarGenerator<T> {
@@ -16,7 +21,6 @@ impl<T> NewVarGenerator<T> {
         Self {
             counter: initial_counter,
             requests: Default::default(),
-            computation_methods: Default::default(),
         }
     }
 
@@ -26,20 +30,19 @@ impl<T> NewVarGenerator<T> {
         computation_method: ComputationMethod<T, GroupedExpression<T, Var>>,
     ) -> Var {
         let var = Var::from(self.counter);
-        self.requests.push((var, prefix.to_string()));
-        self.computation_methods.insert(var, computation_method);
+        self.requests.insert(
+            var,
+            NewVarRequest {
+                final_id: None,
+                prefix: prefix.to_string(),
+                computation_method,
+            },
+        );
         self.counter += 1;
         var
     }
 
-    pub fn requests(&self) -> &Vec<(Var, String)> {
-        &self.requests
-    }
-
-    pub fn computation_method(
-        &self,
-        var: &Var,
-    ) -> &ComputationMethod<T, GroupedExpression<T, Var>> {
-        self.computation_methods.get(var).unwrap()
+    pub fn requests(self) -> HashMap<Var, NewVarRequest<T>> {
+        self.requests
     }
 }
