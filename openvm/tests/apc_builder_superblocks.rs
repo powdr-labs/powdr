1c362359938366453f9b67caba06627a621140d6
Leandro Pacheco <contact@leandropacheco.com>
2026-03-02 08:50:32 -0300

    apc test with 3 basic blocks

--- openvm/tests/apc_builder_superblocks.rs
+++ openvm/tests/apc_builder_superblocks.rs
@@ -70,3 +70,30 @@ fn beq_jump() {
 
     assert_machine_output(program.to_vec(), "beq_jump");
 }
+
+#[test]
+fn many_blocks() {
+    // Superblock with 3 basic blocks.
+    // Constraints should propagate accross the jump instructions:
+    // x10 = 10
+    // x11 = x10
+    // x12 = x11 + 5 = 15
+    // x8 = x12 = 15
+    let program = [
+        bb(0, vec![
+            add(10, 0, 10, 0), // x10 = 10
+            bne(10, 11, 100),
+        ]),
+        // PC=8, BNE fallthrough (x10 = x11)
+        bb(8, vec![
+            add(12, 11, 5, 0), // x12 = x11 + 5 = 15 (known after propagation)
+            beq(8, 12, 60),    // PC=12, BEQ jump to PC+60=72
+        ]),
+        // PC=72, BEQ jump (x8 = x12 = 15)
+        bb(72, vec![
+            add(9, 9, 1, 0),
+        ]),
+    ];
+
+    assert_machine_output(program.to_vec(), "many_blocks");
+}
