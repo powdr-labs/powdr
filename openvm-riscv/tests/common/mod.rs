c31a30f135a62c5f7e4dc10bc6a40c34da860bb4
Leandro Pacheco <contact@leandropacheco.com>
2026-03-09 12:40:14 -0300

    enumerate instructions by PC in apc test outputs (#3632)

--- openvm-riscv/tests/common/mod.rs
+++ openvm-riscv/tests/common/mod.rs
@@ -54,11 +54,19 @@ pub mod apc_builder_utils {
             bus_map: bus_map.clone(),
         };
 
+        let superblock = superblock.map_instructions(Instr::<BabyBear, RiscvISA>::from);
+        // for aligning the output
+        let max_pc_digits = superblock.pcs().max().unwrap().max(1).ilog10() as usize + 1;
         let superblock_str = superblock
             .instructions()
-            .map(|inst| format!("  {}", openvm_instruction_formatter(inst)))
+            .zip(superblock.pcs())
+            .map(|(inst, pc)| {
+                format!(
+                    "  {pc:>max_pc_digits$}: {}",
+                    openvm_instruction_formatter(&inst.inner)
+                )
+            })
             .join("\n");
-        let superblock = superblock.map_instructions(Instr::<BabyBear, RiscvISA>::from);
 
         // Use this env var to output serialized APCs for tests as well.
         let export_path = std::env::var("APC_EXPORT_PATH").ok();
