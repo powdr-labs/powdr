6736da97f4fa7c027294d0bac081aeea48370513
Georg Wiese <georgwiese@gmail.com>
2026-06-02 14:43:38 +0200

    Bring back short circuit

--- autoprecompiles/src/pgo/instruction.rs
+++ autoprecompiles/src/pgo/instruction.rs
@@ -39,6 +39,10 @@ impl<A: Adapter> PgoAdapter for InstructionPgo<A> {
             exec_blocks.blocks.len()
         );
 
+        if config.autoprecompiles == 0 {
+            return vec![];
+        }
+
         let blocks = exec_blocks
             .blocks
             .into_iter()
